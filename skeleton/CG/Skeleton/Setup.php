545152d858f4e2de3867401a9fa47c65a0190940
Richard Heelin <richardh@channelgrabber.com>
2013-10-01 12:39:47 +0100

    CGIV-336: Skeleton setup will now check for project base path

--- skeleton/CG/Skeleton/Setup.php
+++ skeleton/CG/Skeleton/Setup.php
@@ -6,6 +6,8 @@ use Zend\Config\Config;
 
 class Setup
 {
+    const PROJECT_BASE_PATH = 'PROJECT_BASE_PATH';
+
     protected $commands;
     protected $arguments;
     protected $config;
@@ -48,25 +50,63 @@ class Setup
         return $this->config;
     }
 
+    public function getProjectBasePath()
+    {
+        $projectBasePath = $this->getConfig()->get(static::PROJECT_BASE_PATH);
+        if ($projectBasePath) {
+            return $projectBasePath;
+        }
+
+        if (isset($_SERVER[static::PROJECT_BASE_PATH])) {
+            return $_SERVER[static::PROJECT_BASE_PATH];
+        }
+
+        return '';
+    }
+
     public function run()
     {
         fwrite(STDOUT, 'Welcome to the Skeleton Application Setup' . PHP_EOL);
+        $this->setupEnvironment();
         $this->commandList();
+    }
+
+    protected function setupEnvironment()
+    {
+        fwrite(STDOUT, PHP_EOL);
+        $this->setupProjectBasePath();
+        fwrite(STDOUT, PHP_EOL);
+    }
+
+    protected function setupProjectBasePath()
+    {
+        $valid = true;
+        $projectBasePath = $this->getProjectBasePath();
+        $defaultProjectBasePath = dirname(getcwd());
+
+        while (!is_dir($projectBasePath)) {
+            $valid = false;
+            fwrite(STDOUT, ' - ' . static::PROJECT_BASE_PATH . ' is not set or is not a valid directory' . PHP_EOL);
+            fwrite(STDOUT, '   Please enter new path [' . $defaultProjectBasePath . ']: ');
+            $projectBasePath = trim(fgets(STDIN)) ?: $defaultProjectBasePath;
+        }
+
+        fwrite(STDOUT, ' + ' . static::PROJECT_BASE_PATH . ' set as \'' . $projectBasePath . '\'' . PHP_EOL);
+        if (!$valid) {
+            $this->getConfig()->offsetSet(static::PROJECT_BASE_PATH, $projectBasePath);
+        }
     }
 
     protected function commandList()
     {
         fwrite(STDOUT, 'The following commands are available' . PHP_EOL . PHP_EOL);
 
-        fwrite(STDOUT, str_repeat(' ', 3) . '0: Exit' . PHP_EOL);
-
         $commands = $this->getCommands();
         $commands->rewind();
         for ($i = 1; $commands->valid(); $i++, $commands->next()) {
             fwrite(STROUT, str_repeat(' ', 3) . $i . ': ' . $commands->current()->getName() . PHP_EOL);
         }
-
-        fwrite(STDOUT, PHP_EOL);
+        fwrite(STDOUT, str_repeat(' ', 3) . '0: Exit' . PHP_EOL . PHP_EOL);
 
         $command = null;
         while (!is_numeric($command) || $command < 0 || $command > $commands->count()) {
