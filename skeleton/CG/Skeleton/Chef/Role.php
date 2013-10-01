83d8d6d3dfd84ffec0ac414d621877fc74172dc6
Richard Heelin <richardh@channelgrabber.com>
2013-10-01 17:35:20 +0100

    CGIV-336: Will now create/update the role file for the application

---
+++ skeleton/CG/Skeleton/Chef/Role.php
@@ -0,0 +1,62 @@
+<?php
+namespace CG\Skeleton\Chef;
+
+class Role
+{
+    protected $path;
+    protected $data;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+        $this->data = array(
+            'name' => basename($path, '.json'),
+            'chef_type' => 'role',
+            'json_class' => 'Chef::Role',
+            'description' => 'Role for ' . basename($path, '.json'),
+            'run_list' => array()
+        );
+        $this->load();
+    }
+
+    protected function load()
+    {
+        if (!is_file($this->path)) {
+            return;
+        }
+
+        $jsonData = json_decode($this->path, true);
+        if (!is_array($jsonData)) {
+            return;
+        }
+
+        $this->data = array_merge($this->data, $jsonData);
+    }
+
+    public function setName($name)
+    {
+        $this->data['name'] = $name;
+        return $this;
+    }
+
+    public function setDescription($description)
+    {
+        $this->data['description'] = $description;
+        return $this;
+    }
+
+    public function addToRunList($run)
+    {
+        if (isset(array_flip($this->data['run_list'])[$run])) {
+            return $this;
+        }
+
+        $this->data['run_list'][] = $run;
+        return $this;
+    }
+
+    public function save()
+    {
+        file_put_contents($this->path, json_encode($this->data, JSON_PRETTY_PRINT));
+    }
+}
--- skeleton/CG/Skeleton/Command/Vagrant/SaveNode.php
+++ skeleton/CG/Skeleton/Command/Vagrant/SaveNode.php
@@ -4,16 +4,61 @@ namespace CG\Skeleton\Command\Vagrant;
 use CG\Skeleton\Command;
 use CG\Skeleton\Arguments;
 use CG\Skeleton\Config;
+use Zend\Config\Config as ZendConfig;
+use CG\Skeleton\Chef\Role;
 
 class SaveNode implements Command
 {
+    const ROLE = 'role';
+    const ROLES = 'tools/chef/roles/';
+
+    protected $defaults;
+
+    public function __construct()
+    {
+        $this->defaults = array();
+    }
+
     public function getName()
     {
-        return 'Create / Update node data';
+        return 'Save Node Data';
     }
 
     public function run(Arguments $arguments, Config $config)
     {
+        $cwd = getcwd();
+        chdir($config->getInfrastructurePath());
+        exec('git checkout ' . $config->getBranch());
+
+        $chefConfig = $config->get('Chef', new ZendConfig($this->defaults, true));
+        $this->saveRole($config, $chefConfig);
+        $config->offsetSet('Chef', $chefConfig);
+
+        chdir($cwd);
         return true;
+    }
+
+    protected function saveRole(Config $config, ZendConfig $chefConfig)
+    {
+        $roleName = $chefConfig->get(static::ROLE);
+        if (!$roleName) {
+            $roleName = $config->getAppName();
+            $chefConfig->offsetSet(static::ROLE, $roleName);
+        }
+
+        $roleFile = static::ROLES . $roleName . '.json';
+        $role = new Role($roleFile);
+
+        $role->addToRunList('role[apt]')
+             ->addToRunList('role[cg]')
+             ->addToRunList('role[percona]')
+             ->addToRunList('role[web]');
+
+        $role->save();
+
+        exec(
+            'git add ' . $roleFile . ';'
+            . ' git commit -m "SKELETON: Updated role ' . $roleName . '" --only -- ' . $roleFile
+        );
     }
 }
