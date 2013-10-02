07cff334230ae9e09c1afe6f991a31189dd18dac
Richard Heelin <richardh@channelgrabber.com>
2013-10-02 10:45:05 +0100

    CGIV-336: Added an abstract command for vagrant commands to use

---
+++ skeleton/CG/Skeleton/Vagrant/Command/AbstractCommand.php
@@ -0,0 +1,23 @@
+<?php
+namespace CG\Skeleton\Vagrant\Command;
+
+use CG\Skeleton\Command;
+use CG\Skeleton\Arguments;
+use CG\Skeleton\Config;
+
+abstract class AbstractCommand implements Command
+{
+    const VAGRANT_PATH = '/tools/vagrant';
+
+    public function run(Arguments $arguments, Config $config)
+    {
+        $cwd = getcwd();
+        chdir($config->getInfrastructurePath() . static::VAGRANT_PATH);
+        exec('git checkout ' . $config->getBranch() . ' > /dev/null;');
+        $this->runVagrantCommand($arguments, $config);
+        chdir($cwd);
+        return true;
+    }
+
+    abstract protected function runVagrantCommand(Arguments $arguments, Config $config);
+}
--- skeleton/CG/Skeleton/Vagrant/Command/Up.php
+++ skeleton/CG/Skeleton/Vagrant/Command/Up.php
@@ -1,25 +1,18 @@
 <?php
 namespace CG\Skeleton\Vagrant\Command;
 
-use CG\Skeleton\Command;
 use CG\Skeleton\Arguments;
 use CG\Skeleton\Config;
 
-class Up implements Command
+class Up extends AbstractCommand
 {
-    const VAGRANT_PATH = '/tools/vagrant';
-
     public function getName()
     {
         return 'Start Virtual Machine';
     }
 
-    public function run(Arguments $arguments, Config $config)
+    protected function runVagrantCommand(Arguments $arguments, Config $config)
     {
-        $cwd = getcwd();
-        chdir($config->getInfrastructurePath() . static::VAGRANT_PATH);
-        exec('git checkout ' . $config->getBranch() . ' > /dev/null; vagrant up ' . $config->getNode());
-        chdir($cwd);
-        return true;
+        exec(' vagrant up ' . $config->getNode());
     }
 }
