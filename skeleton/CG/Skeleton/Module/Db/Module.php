a28f592b9ffaf3857609dc5fcb041d8ea2129580
Richard Heelin <richardh@channelgrabber.com>
2013-10-03 09:49:22 +0100

    CGIV-336: Added basic enable implementation to Db module

--- skeleton/CG/Skeleton/Module/Db/Module.php
+++ skeleton/CG/Skeleton/Module/Db/Module.php
@@ -2,11 +2,20 @@
 namespace CG\Skeleton\Module\Db;
 
 use CG\Skeleton\Module\AbstractModule;
+use CG\Skeleton\Module\EnableInterface;
+use CG\Skeleton\Arguments;
+use CG\Skeleton\Config;
+use CG\Skeleton\Module\BaseConfig;
 
-class Module extends AbstractModule
+class Module extends AbstractModule implements EnableInterface
 {
     public function getConfigClass()
     {
         return 'CG\Skeleton\Module\BaseConfig';
+    }
+
+    public function enable(Arguments $arguments, Config $config, BaseConfig $moduleConfig)
+    {
+        $moduleConfig->setEnabled(true);
     }
 }
--- skeleton/CG/Skeleton/Module/EnableInterface.php
+++ skeleton/CG/Skeleton/Module/EnableInterface.php
@@ -2,10 +2,10 @@
 namespace CG\Skeleton\Module;
 
 use CG\Skeleton\Arguments;
-use CG\Skeleton\Config;
+use CG\Skeleton\Config as SkeletonConfig;
 use CG\Skeleton\Module\BaseConfig;
 
 interface EnableInterface
 {
-    public function enable(Arguments $arguments, Config $config, BaseConfig $moduleConfig);
+    public function enable(Arguments $arguments, SkeletonConfig $config, BaseConfig $moduleConfig);
 }
