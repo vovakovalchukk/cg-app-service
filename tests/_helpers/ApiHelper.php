6afafb61342fb9b757b140d66f347f737039a9f5
Richard Heelin <richardh@channelgrabber.com>
2013-10-04 11:32:03 +0100

    CGIV-353: Added codeception tests structure

---
+++ tests/_helpers/ApiHelper.php
@@ -0,0 +1,9 @@
+<?php
+namespace Codeception\Module;
+
+use CG\Codeception\ApiHelper as CGApiHelper;
+// here you can define custom functions for ApiGuy 
+
+class ApiHelper extends CGApiHelper
+{
+}
