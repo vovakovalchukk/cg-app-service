6af09fb7470858e39c341fe59a918814c5cdd5b1
M King <matthew.king@channelgrabber.com>
2013-11-11 21:10:19 +0000

    CGIV-710 Add validation to middleware

--- public/index.php
+++ public/index.php
@@ -6,7 +6,9 @@ use CG\Slim\ContentTypes;
 use CG\Slim\Rest\Options;
 use CG\Slim\Rest\UnusedMethods;
 use CG\Slim\Renderer;
+use CG\Slim\Validator;
 use CG\Slim\VndError\VndError;
+use Nocarrier\Hal;
 
 require_once 'application/bootstrap.php';
 require_once 'config/routing.php';
@@ -14,16 +16,16 @@ require_once 'config/routing.php';
 $di = $serviceManager->get('Di');
 $app = $serviceManager->get(Slim::class);
 
-$app->add($di->get(ContentTypes::class));
-$app->add($di->get(VndError::class));
-$app->add($di->get(Renderer::class));
-
 $options = $di->get(Options::class, array('app' => $app));
 
 $unusedMethods = $di->get(UnusedMethods::class, array('app' => $app));
 $app->any('.+', $unusedMethods);
 
 foreach ($routes as $route => $request) {
+    if (isset($request["validation"])) {
+        $app->add($di->get(Validator::class, array("rules" => $request["validation"]["rules"],
+            "flatten" => $request["validation"]["flatten"] )));
+    }
     $route = $app->map(
         $route,
         $options,
@@ -34,5 +36,11 @@ foreach ($routes as $route => $request) {
     call_user_func_array(array($route, 'via'), $request['via']);
 }
 
+
+$app->add($di->get(ContentTypes::class));
+$app->add($di->get(VndError::class));
+$app->add($di->get(Renderer::class));
+
 $di->instanceManager()->addSharedInstance($app, Slim::class);
 $app->run();
+
