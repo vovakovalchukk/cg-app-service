cd09accf3b4de968cff37c343e9e9853da39a8dd
M King <matthew.king@channelgrabber.com>
2013-11-12 14:27:58 +0000

    CGIV-710 Code review changes

--- public/index.php
+++ public/index.php
@@ -21,13 +21,11 @@ $options = $di->get(Options::class, array('app' => $app));
 $unusedMethods = $di->get(UnusedMethods::class, array('app' => $app));
 $app->any('.+', $unusedMethods);
 
+$validator = $di->get(Validator::class);
 foreach ($routes as $route => $request) {
     if (isset($request["validation"])) {
-        $app->add($di->get(Validator::class, array("filterRules" => $request["validation"]["filterRules"],
-            "dataRules" => $request["validation"]["dataRules"],
-            "flatten" => $request["validation"]["flatten"],
-            "methods" =>  $request['via'],
-            "route" => $route)));
+        $validator->attach($request["validation"]["flatten"], $route, $request["validation"]["dataRules"],
+            $request["validation"]["filterRules"]);
     }
     $route = $app->map(
         $route,
@@ -38,8 +36,7 @@ foreach ($routes as $route => $request) {
     }
     call_user_func_array(array($route, 'via'), $request['via']);
 }
-
-
+$app->add($di->get(Validator::class));
 $app->add($di->get(ContentTypes::class));
 $app->add($di->get(VndError::class));
 $app->add($di->get(Renderer::class));
