bd0cff893eef6fa886649ba6eb0e1b56c011ba94
Adam <adamb@channelgrabber.com>
2013-11-18 14:54:45 +0000

    CGIV-780 added adapter config

--- skeleton/CG/Skeleton/Module/Redis/Module.php
+++ skeleton/CG/Skeleton/Module/Redis/Module.php
@@ -47,6 +47,63 @@ class Module extends AbstractModule implements EnableInterface, ConfigureInterfa
         $this->configureModule($arguments, $config, $moduleConfig, true);
     }
 
+    public function configureModule(Arguments $arguments, SkeletonConfig $config, Config $moduleConfig, $reconfigure = false)
+    {
+        $this->configureRedisAdapters($arguments, $config, $moduleConfig, $reconfigure);
+    }
+
+    public function configureRedisAdapters(Arguments $arguments, SkeletonConfig $config, Config $moduleConfig, $reconfigure = false)
+    {
+        if (!$moduleConfig->isEnabled()) {
+            return;
+        }
+        $cwd = getcwd();
+        chdir($config->getInfrastructurePath() . '/tools/chef');
+
+        ob_start();
+        passthru('knife solo data bag show redis local -F json 2>/dev/null');
+        $redisInstancesJson = json_decode(ob_get_clean(), true) ?: array();
+
+        $redisInstances = array();
+        if (isset($redisInstancesJson['instance'])) {
+            foreach (array_keys($redisInstancesJson['instance']) as $redisInstance) {
+                $redisInstances[$redisInstance] = $redisInstance;
+            }
+        }
+
+        if (empty($redisInstances)) {
+            $moduleConfig->setEnabled(false);
+            $this->getConsole()->writelnErr(
+                'No Redis Instances configured in redis data bag for local environment  - ' . $this->getModuleName() . ' Disabled'
+            );
+            chdir($cwd);
+            return;
+        }
+
+        $configuredAdapters = $moduleConfig->getRedisAdapters();
+        while (true) {
+            if (!$reconfigure && !empty($configuredAdapters)) {
+                break;
+            }
+
+            $this->getConsole()->writeln('Available Redis Adapters:');
+            foreach ($redisInstances as $instance) {
+                $this->getConsole()->writeln('   * ' . $instance);
+            }
+
+            $adaptersString = $this->getConsole()->ask('Please choose the adapters you require (separated by spaces)');
+
+            foreach (explode(" ", $adaptersString) as $adapter) {
+                $configuredAdapters[$adapter] = true;
+            }
+
+            $reconfigure = false;
+        };
+
+        $moduleConfig->setRedisAdapters($configuredAdapters);
+        chdir($cwd);
+    }
+
     protected function updateNode(Arguments $arguments, SkeletonConfig $config, BaseConfig $moduleConfig)
     {
         $nodeFile = Chef::NODES . $config->getNode() . '.json';
