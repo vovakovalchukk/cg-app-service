e3db32895a5fd6fa8c6340bf0d19c3860186d8e0
Dangerous Dan <dangerousdan@channelgrabber.com>
2013-11-27 11:00:17 +0000

    CGIV-933 updated index to use newrelic in unknown methods

--- public/index.php
+++ public/index.php
@@ -35,7 +35,7 @@ foreach ($routes as $route => $request) {
     }
     call_user_func_array([$route, 'via'], $request['via']);
 }
-$app->any('.+', $unusedMethods);
+$app->any('.+', $newRelic, $unusedMethods);
 
 $app->add($di->get(ContentTypes::class));
 $app->add($di->get(VndError::class));
