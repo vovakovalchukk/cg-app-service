86bb04789ed151aa46613930dd449e9440449929
Dangerous Dan <dangerousdan@channelgrabber.com>
2013-11-27 10:56:25 +0000

    CGIV-933 updated index to implement new validation

--- public/index.php
+++ public/index.php
@@ -16,19 +16,18 @@ require_once 'config/routing.php';
 $di = $serviceManager->get('Di');
 $app = $serviceManager->get(Slim::class);
 
-$newRelic = $di->get(NewRelic::class, compact($app));
-$options = $di->get(Options::class, compact($app));
-$unusedMethods = $di->get(UnusedMethods::class, compact($app));
+$newRelic = $di->get(NewRelic::class, compact('app'));
+$options = $di->get(Options::class, compact('app'));
+$unusedMethods = $di->get(UnusedMethods::class, compact('app'));
+$validator = $di->get(Validator::class, compact('app', 'di'));
 
-$validator = $di->get(Validator::class);
 foreach ($routes as $route => $request) {
-    if (isset($request["validation"])) {
-        $validator->attach($request["validation"]["flatten"], $route, $request["validation"]["dataRules"],
-            $request["validation"]["filterRules"]);
-    }
+    $validator->setValidators($request);
+
     $route = $app->map(
         $route,
         $newRelic,
+        $validator,
         $options,
         $request["controllers"])->name($request["name"]);
     if (!is_array($request['via'])) {
@@ -36,9 +35,8 @@ foreach ($routes as $route => $request) {
     }
     call_user_func_array([$route, 'via'], $request['via']);
 }
-$app->any('.+', $newRelic, $unusedMethods);
+$app->any('.+', $unusedMethods);
 
-$app->add($validator);
 $app->add($di->get(ContentTypes::class));
 $app->add($di->get(VndError::class));
 $app->add($di->get(Renderer::class));
