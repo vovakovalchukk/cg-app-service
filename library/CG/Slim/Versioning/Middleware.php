4216b6a5b2aaaa05fc2918639952b6d0a69e0b23
Richard Heelin <richardh@channelgrabber.com>
2013-11-28 17:07:13 +0000

    CGIV-962: Requests that ask for an unsupported version will now be met with a 400 Bad Request

--- library/CG/Slim/Versioning/Middleware.php
+++ library/CG/Slim/Versioning/Middleware.php
@@ -3,11 +3,16 @@ namespace CG\Slim\Versioning;
 
 use Slim\Middleware as SlimMiddleware;
 use Slim\Slim;
+use CG\Http\Exception\Exception4xx\BadRequest;
 
 class Middleware extends SlimMiddleware
 {
+    const VERSION_HEADER = 'VERSION';
+
     protected $route;
     protected $versions = array();
+    protected $version;
+    protected $requested;
 
     public function __construct(Slim $app)
     {
@@ -34,6 +39,26 @@ class Middleware extends SlimMiddleware
 
     public function call()
     {
+        $this->parseRequest();
         $this->next->call();
+    }
+
+    protected function parseRequest()
+    {
+        $route = $this->getRoute();
+        if (!$route || !isset($this->versions[$route])) {
+            return;
+        }
+        $headers = $this->getApplication()->request()->headers;
+
+        $this->version = $this->versions[$route];
+        $this->requested =
+            isset($headers[static::VERSION_HEADER])
+                ? $headers[static::VERSION_HEADER]
+                : $this->version->getMin();
+
+        if (!$this->version->allowedVersion($this->requested)) {
+            throw new BadRequest();
+        }
     }
 }
