224a2ef14cff1d7b9dc871b8fc53543e55c5e988
Adam <adamb@channelgrabber.com>
2013-12-09 17:03:37 +0000

    CGIV-1056 added composer to di

--- skeleton/config/di.php
+++ skeleton/config/di.php
@@ -55,6 +55,12 @@ return array(
             ),
             'shared' => true
         ),
+        'CG\Skeleton\Composer\Composer' => array(
+            'parameters' => array(
+                'path' => 'composer.json'
+            ),
+            'shared' => true
+        ),
         'CG\Skeleton\Module\StartupCommand' => array(
             'injections' => $modules
         ),
