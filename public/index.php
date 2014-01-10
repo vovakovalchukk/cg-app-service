474a69124cf0c33e1d6f8076ede1eb9c7f139c2f
Aaron Baker <aaronb@channelgrabber.com>
2014-01-10 17:04:01 +0000

    CGIV-1065: added log event listening

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 chdir(dirname(__DIR__));
 
 use Slim\Slim;
+use CG\Log\Logger;
 use CG\Slim\ContentTypes;
+use CG\Slim\Module\Logging as LoggingModule;
 use CG\Slim\NewRelic;
 use CG\Slim\Rest\Options;
 use CG\Slim\Rest\UnusedMethods;
@@ -14,6 +16,16 @@ use CG\Slim\HeadRequest\Middleware as HeadRequest;
 
 require_once 'application/bootstrap.php';
 $routes = require_once 'config/routing.php';
+
+$app->hook('slim.before.dispatch', function() use ($app, $di) {
+    if (isset($app->slimApp)) {
+        return;
+    }
+    $app->slimApp = SlimApp::create($app);
+    $app->module = $di->get(LoggingModule::class, ['application' => $app]);
+    $app->module->listenForLog();
+    $app->logger = $di->get(Logger::class);
+});
 
 $newRelic = $di->get(NewRelic::class, compact('app'));
 $options = $di->get(Options::class, compact('app'));
