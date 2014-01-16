583fe7d54ee37fbc0df37dc4a824d3e86cfa17ad
Adam <adamb@channelgrabber.com>
2014-01-16 11:39:48 +0000

    CGIV-642 fixed vendor_components.php conflict

--- config/di/vendor_components.php
+++ config/di/vendor_components.php
@@ -3,7 +3,6 @@
  * This file is generated but should still be committed
  */
 return array(
-<<<<<<< HEAD
     'alexandresalome_php-selenium_src_Selenium',
     'behat_mink-browserkit-driver_src_Behat_Mink_Driver',
     'behat_mink-goutte-driver_src_Behat_Mink_Driver',
@@ -12,62 +11,27 @@ return array(
     'behat_mink-zombie-driver_src_Behat_Mink_Driver',
     'behat_mink_src_Behat_Mink',
 //    'channelgrabber_codeception_CG_Codeception',
-=======
-    'behat_mink-browserkit-driver_src_Behat_Mink_Driver',
-    'behat_mink-goutte-driver_src_Behat_Mink_Driver',
-    'behat_mink-selenium2-driver_src_Behat_Mink_Driver',
-    'behat_mink_src_Behat_Mink',
-    'channelgrabber_cache_CG_Cache',
-//    'channelgrabber_codeception_CG_Codeception',
-    'channelgrabber_constants_CG',
-    'channelgrabber_etag_CG_ETag',
->>>>>>> a005ef3... CGIV-642 make module applyconfig env aware
     'channelgrabber_http_CG_Http',
     'channelgrabber_stdlib_CG_Stdlib',
-<<<<<<< HEAD
     'codeception_codeception_src_Codeception',
-=======
-    'channelgrabber_userpreference_CG_UserPreference',
-    'channelgrabber_validation_CG_Validation',
-    'channelgrabber_vnderror_CG_VndError',
-//    'codeception_codeception_src_Codeception',
->>>>>>> a005ef3... CGIV-642 make module applyconfig env aware
     'fabpot_goutte_Goutte',
     'guzzle_guzzle_src_Guzzle',
     'instaclick_php-webdriver_lib_WebDriver',
     'monolog_monolog_src_Monolog',
     'nocarrier_hal_src_Nocarrier',
     'phpspec_php-diff_lib_Diff',
-<<<<<<< HEAD
 //    'phpspec_phpspec_src_PhpSpec',
     'phpspec_prophecy_src_Prophecy',
     'psr_log_Psr_Log',
     'symfony_browser-kit_Symfony_Component_BrowserKit',
-=======
-    'phpspec_phpspec_src_PhpSpec',
-    'phpspec_prophecy_src_Prophecy',
-    'predis_predis_lib_Predis',
-    'psr_log_Psr_Log',
-    'slim_slim_Slim',
-    'symfony_browser-kit_Symfony_Component_BrowserKit',
-    'symfony_class-loader_Symfony_Component_ClassLoader',
-    'symfony_config_Symfony_Component_Config',
->>>>>>> a005ef3... CGIV-642 make module applyconfig env aware
     'symfony_console_Symfony_Component_Console',
     'symfony_css-selector_Symfony_Component_CssSelector',
     'symfony_dom-crawler_Symfony_Component_DomCrawler',
     'symfony_event-dispatcher_Symfony_Component_EventDispatcher',
-<<<<<<< HEAD
     'symfony_finder_Symfony_Component_Finder',
     'symfony_process_Symfony_Component_Process',
     'symfony_yaml_Symfony_Component_Yaml',
     'videlalvaro_php-amqplib_PhpAmqpLib',
-=======
-    'symfony_filesystem_Symfony_Component_Filesystem',
-    'symfony_finder_Symfony_Component_Finder',
-    'symfony_process_Symfony_Component_Process',
-    'symfony_yaml_Symfony_Component_Yaml',
->>>>>>> a005ef3... CGIV-642 make module applyconfig env aware
 //    'zendframework_zend-code_Zend_Code',
     'zendframework_zend-config_Zend_Config',
     'zendframework_zend-db_Zend_Db',
@@ -75,8 +39,4 @@ return array(
     'zendframework_zend-eventmanager_Zend',
     'zendframework_zend-servicemanager_Zend',
 //    'zendframework_zend-stdlib_Zend_Stdlib',
-<<<<<<< HEAD
-=======
-//    'zendframework_zend-validator_Zend_Validator',
->>>>>>> a005ef3... CGIV-642 make module applyconfig env aware
 );
