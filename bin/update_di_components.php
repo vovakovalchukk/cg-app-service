c6b6c2f4d07c496a53425ad688c1a0bb21b096ad
Sean Kenny <seank@channelgrabber.com>
2014-01-24 17:07:51 +0000

    CGIv-1421 vendor_components fixes

--- bin/update_di_components.php
+++ bin/update_di_components.php
@@ -5,15 +5,11 @@ define('RED', "\033[31m");
 define('WHITE', "\033[0m");
 define('COMPONENTS_FILE', 'config/di/vendor_components.php');
 
-if (!is_writable(COMPONENTS_FILE)) {
-	if(!file_exists(COMPONENTS_FILE)){
-		$empty = "<?php ". PHP_EOL . "return array();";
-		file_put_contents(COMPONENTS_FILE, $empty);
-	} else {
-    		echo RED . 'Cannot update DI components as ' . COMPONENTS_FILE . ' is not writeable. Check the file permissions or try sudo-ing' . WHITE . PHP_EOL;
-    		exit(1);
-	}
+if(!file_exists(COMPONENTS_FILE)) {
+	$empty = "<?php return array();";
+	file_put_contents(COMPONENTS_FILE, $empty);
 }
+
 
 require_once 'bootstrap.php';
 
@@ -75,8 +71,8 @@ foreach ($vendors as $vendor) {
         $componentName = implode('_', $componentNameParts);
         echo $componentName . '... ';
         $newComponents[$componentName] = true;
-
-        if (in_array($componentName, $currentComponents)) {
+        
+	if (in_array($componentName, $currentComponents)) {
             echo 'exists' . PHP_EOL;
             continue;
         }
