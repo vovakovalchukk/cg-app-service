9284e3632fe90275513926794ea8a6b369bee518
Adam <adamb@channelgrabber.com>
2014-02-10 12:15:38 +0000

    CGIV-1506 added vendor folder check for composer instlal

--- tools/post_deploy.sh
+++ tools/post_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -e
-COMPOSER_CACHE_DIR=/tmp/composer composer install --no-dev --no-interaction
+if [ ! -d "vendor/" ]; then
+  COMPOSER_CACHE_DIR=/tmp/composer composer install --no-dev --no-interaction
+fi
