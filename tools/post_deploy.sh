e5f7217b1dd9f435235a502b96e643586629583d
Adam <adamb@channelgrabber.com>
2014-02-11 08:34:59 +0000

    CGIV-1506 added debug command

--- tools/post_deploy.sh
+++ tools/post_deploy.sh
@@ -2,4 +2,6 @@
 set -e
 if [ ! -d "vendor/" ]; then
   COMPOSER_CACHE_DIR=/tmp/composer composer install --no-dev --no-interaction
+  touch /tmp/composer-exectued-on-test-deploy-script.txt
 fi
+touch /tmp/test-deploy-script-complete.txt
