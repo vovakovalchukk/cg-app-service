7dc97cd41bd254da9d13af6eb17649a977c603a5
Andu Aanei <andu@rhonda-ThinkPad-T520.(none)>
2014-03-12 14:57:59 +0000

    Fix one of the migrations.

--- phinx/migrations/20140225221200_order_date_fix.php
+++ phinx/migrations/20140225221200_order_date_fix.php
@@ -3,12 +3,21 @@ use Phinx\Migration\AbstractMigration;
 
 class OrderDateFix extends AbstractMigration
 {
-    public function change()
+    public function up()
     {
         $orderTable = $this->table('order');
         $orderTable->changeColumn('purchaseDate', 'datetime', array('null' => true));
         $orderTable->changeColumn('paymentDate', 'datetime', array('null' => true));
         $orderTable->changeColumn('printedDate', 'datetime', array('null' => true));
         $orderTable->changeColumn('dispatchDate', 'datetime', array('null' => true));
+    }
+
+    public function down()
+    {
+        $orderTable = $this->table('order');
+        $orderTable->changeColumn('purchaseDate', 'datetime', array('null' => false));
+        $orderTable->changeColumn('paymentDate', 'datetime', array('null' => false));
+        $orderTable->changeColumn('printedDate', 'datetime', array('null' => false));
+        $orderTable->changeColumn('dispatchDate', 'datetime', array('null' => false));
     }
 }
