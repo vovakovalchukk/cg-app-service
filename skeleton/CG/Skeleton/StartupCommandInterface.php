7c07b1b0cf0f144cfefe00f943c19a1b4051a6e2
Richard Heelin <richardh@channelgrabber.com>
2014-05-09 09:00:01 +0100

    CGIV-2046: Removed skeleton

--- skeleton/CG/Skeleton/StartupCommandInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace CG\Skeleton;
-
-use CG\Skeleton\DevelopmentEnvironment\Environment;
-
-interface StartupCommandInterface
-{
-    public function run(Arguments $arguments, Config $config, Environment $environment);
-}
