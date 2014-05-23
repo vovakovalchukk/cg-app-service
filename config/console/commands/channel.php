3279e760e19305541be7ff5cd5fac6d094b0821a
Sean Kenny <seank@channelgrabber.com>
2014-05-23 16:55:13 +0100

    CGIV-2181 Updated composer and corrected command

--- config/console/commands/channel.php
+++ config/console/commands/channel.php
@@ -6,19 +6,13 @@ return array(
     'channel:downloadOrders' => array(
         'command' => function (InputInterface $input) use ($di) {
             $channel = $input->getArgument('channel');
-            $getToTime = $input->getArgument('getToTime');
-
             $command = $di->get(OrderDownload::class);
-            $command->downloadOrders($channel, $getToTime);
+            $command->downloadOrders($channel);
         },
         'description' => 'Fetch all accounts for the provided channel then generate Gearman jobs to get orders from the respective channel.',
         'arguments' => array(
             'channel' => array(
                 'required' => true
-            ),
-            'getToTime' => array(
-                'required' => false,
-                'default' => 'now'
             )
         ),
         'options' => array(
