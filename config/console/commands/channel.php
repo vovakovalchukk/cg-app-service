f96afbd98b5d29aa00478f481b58789cbd82f343
Sean Kenny <seank@channelgrabber.com>
2014-05-27 15:39:04 +0100

    CGIV-2181 Readded to and from times

--- config/console/commands/channel.php
+++ config/console/commands/channel.php
@@ -6,13 +6,24 @@ return array(
     'channel:downloadOrders' => array(
         'command' => function (InputInterface $input) use ($di) {
             $channel = $input->getArgument('channel');
+            $from = $input->getArgument('from');
+            $to = $input->getArgument('to');
+
             $command = $di->get(OrderDownload::class);
-            $command->downloadOrders($channel);
+            $command->downloadOrders($channel, $from, $to);
         },
         'description' => 'Fetch all accounts for the provided channel then generate Gearman jobs to get orders from the respective channel.',
         'arguments' => array(
             'channel' => array(
                 'required' => true
+            ),
+            'from' => array(
+                'required' => false,
+                'default' => null
+            ),
+            'to' => array(
+                'required' => false,
+                'default' => null
             )
         ),
         'options' => array(
