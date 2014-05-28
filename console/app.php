39dcc3daa15b43bc927c9d9d030e6e28a254d7b8
Richard Heelin <richardh@channelgrabber.com>
2014-05-28 12:35:11 +0100

    CGIV-2259: Added logging framework to Gearman workers

--- console/app.php
+++ console/app.php
@@ -2,12 +2,14 @@
 chdir(dirname(__DIR__));
 
 use Cilex\Application as Cilex;
-use CG\Slim\Cilex\GenericCommand;
+use CG\Cilex\GenericCommand;
+use CG\Cilex\ServiceProvider\Logging;
 
 require_once 'application/bootstrap.php';
 $commands = require_once 'config/console/commands.php';
 
 $app = $di->get(Cilex::class, array("name" => ""));
+$app->register($di->get(Logging::class));
 foreach ($commands as $commandName => $command) {
     $app->command($di->newInstance(GenericCommand::class, array("commandName" => $commandName, "command" => $command)));
 }
