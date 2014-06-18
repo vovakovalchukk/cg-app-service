739d8d53a511ddef6cd0e545fa0ebb87b7ed35e5
Sean Kenny <seank@channelgrabber.com>
2014-06-18 14:01:58 +0100

    CGIV-2376 Added di config for shared itid service instance

---
+++ config/autoload/di.itid.global.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'di' => array(
+        'instance' => array(
+            ITIDService::class => array(
+                'shared' => true
+            )
+        )
+    )
+);
