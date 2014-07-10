4adf0101868f9caa8587f6c6d7660a8d26ff3ce5
Richard Heelin <richardh@channelgrabber.com>
2014-07-10 13:30:50 +0100

    CGIV-2587: Capistrano will now get version information from config file stored in remote repo instead of using local file which may not be the same as the branch version we're deploying

--- tools/capistrano/lib/capistrano/dsl/paths.rb
+++ tools/capistrano/lib/capistrano/dsl/paths.rb
@@ -1,15 +1,33 @@
 module Capistrano
     module DSL
         module Paths
-            def shared_path
-                version = JSON.parse(IO.read('../../config/autoload/version.json'))
-                paths = ['shared']
+            def version
+                version = fetch(:version)
+                return version unless version == nil
 
-                if version.is_a?(Hash) && version.has_key?('version')
-                    paths.unshift('version', "#{version['version']}")
+                path = fetch(:version_path, 'config/autoload/version.json')
+                json = JSON.parse(
+                    capture :git, :archive, "--remote=#{fetch(:repo_url)}", fetch(:branch), path, '|', :tar, '-Ox', path
+                )
+
+                version = ""
+                if json.is_a?(Hash) && json.has_key?('version')
+                    version = "#{json['version']}"
                 end
 
-                deploy_path.join(*paths)
+                set(:version, version)
+                return version
+            end
+
+            def shared_path
+                version = version()
+                paths = ['shared']
+
+                if version
+                    paths.unshift('version', version)
+                end
+
+                return deploy_path.join(*paths)
             end
         end
     end
