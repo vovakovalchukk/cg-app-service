a79683841c6e183504b53a335c8e4681fdc25d94
Paul Keelan <paulk@channelgrabber.com>
2014-08-15 14:24:18 +0100

    CGIV-2889: Added accountId to listing

---
+++ phinx/migrations/20140815140000_accountid_listing_migration.php
@@ -0,0 +1,13 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class AccountidListingMigration extends AbstractMigration
+{
+    public function change()
+    {
+        $listing = $this->table('listing');
+        $listing->addColumn('accountId', 'integer')
+            ->addIndex('accountId')
+            ->update();
+    }
+}
