67f186d27273c12c10afea709db66385661e9b9f
Andu <andua@channelgrabber.com>
2014-08-22 14:43:19 +0100

    CGIV-2925-Shipping-Accounts Add migration to change accountId and shippingService columns to accept NULL

---
+++ phinx/migrations/20140822140000_alias_null_migration.php
@@ -0,0 +1,21 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class AliasNullMigration extends AbstractMigration
+{
+    public function up()
+    {
+        $table = $this->table('alias');
+        $table->changeColumn('accountId', 'integer', ['null' => true])
+             ->changeColumn('shippingService', 'string', ['null' => true])
+             ->save();
+    }
+
+    public function down()
+    {
+        $table = $this->table('alias');
+        $table->changeColumn('accountId', 'integer', ['null' => true])
+             ->changeColumn('shippingService', 'string', ['null' => true])
+             ->save();
+    }
+}
