8f937ddc39037a8c35c420811673bc8ae9bc7f16
Andu <andua@channelgrabber.com>
2014-08-26 15:29:20 +0100

    CGIV-2925-Shipping-Accounts Change migration to have empty down method

--- phinx/migrations/20140822140000_alias_null_migration.php
+++ phinx/migrations/20140822140000_alias_null_migration.php
@@ -13,9 +13,6 @@ class AliasNullMigration extends AbstractMigration
 
     public function down()
     {
-        $table = $this->table('alias');
-        $table->changeColumn('accountId', 'integer', ['null' => false])
-             ->changeColumn('shippingService', 'string', ['null' => false])
-             ->save();
+
     }
 }
