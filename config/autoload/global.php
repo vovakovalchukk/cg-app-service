68d2e8ecd1ce3305c7e6e1fb69593df352d53fa5
Richard Heelin <richardh@channelgrabber.com>
2014-09-20 14:39:52 +0100

    CGIV-3323: Switched over to using the new cached runtime di definitions

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,62 +11,14 @@
  * file.
  */
 
-use Zend\Config\Config;
-use Zend\Di\Config as DiConfig;
-use Zend\Di\Di;
-use Zend\Di\InstanceManager;
-use CG\Zend\Stdlib\Di\Definition\RuntimeDefinition;
-use CG\Zend\Stdlib\Di\DefinitionList;
+use CG\ETag\Storage\Predis;
+use CG\ETag\StorageInterface;
 
 return array(
-    'service_manager' => array(
-        'factories' => array(
-            'Zend\Di\Di' => function($serviceManager) {
-                $configuration = $serviceManager->get('config');
-
-                $runtimeDefinition = new RuntimeDefinition(
-                    null,
-                    require dirname(dirname(__DIR__)) . '/vendor/composer/autoload_classmap.php'
-                );
-
-                $definitionList = new DefinitionList([$runtimeDefinition]);
-                $im = new InstanceManager();
-                $config = new DiConfig(isset($configuration['di']) ? $configuration['di'] : array());
-
-                $di = new Di($definitionList, $im, $config);
-
-                if (isset($configuration['db'], $configuration['db']['adapters'])) {
-                    foreach (array_keys($configuration['db']['adapters']) as $adapter) {
-                        $im->addAlias($adapter, 'Zend\Db\Adapter\Adapter');
-                        $im->addSharedInstance($serviceManager->get($adapter), $adapter);
-                    }
-                }
-
-                $im->addSharedInstance($di, 'Di');
-                $im->addSharedInstance($di, 'Zend\Di\Di');
-                $im->addSharedInstance($di->get('config', array('array' => $configuration)), 'config');
-                $im->addSharedInstance($di->get(Config::class, array('array' => $configuration)), 'app_config');
-
-                return $di;
-            }
-        ),
-        'shared' => array(
-            'Zend\Di\Di' => true
-        ),
-        'aliases' => array(
-            'Di' => 'Zend\Di\Di'
-        )
-    ),
     'di' => array(
         'instance' => array(
-            'aliases' => array(
-                'Di' => 'Zend\Di\Di',
-                'config' => Config::class,
-                'app_config' => Config::class
-            ),
             'preferences' => array(
-                'Zend\Di\LocatorInterface' => 'Zend\Di\Di',
-                'CG\ETag\StorageInterface' => 'CG\ETag\Storage\Predis',
+                StorageInterface::class => Predis::class,
             )
         )
     )
