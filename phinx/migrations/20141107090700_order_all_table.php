4d6fe5afb213a362be9a7a3e17eba3bca0c7d843
dangerous <dangerousdan@channelgrabber.com>
2014-11-13 13:15:55 +0000

    CGIV-4042 corrected phinx migration for new order table

--- phinx/migrations/20141107090700_order_all_table.php
+++ phinx/migrations/20141107090700_order_all_table.php
@@ -10,13 +10,18 @@ class OrderAllTable extends AbstractMigration
     public function up()
     {
         $this->duplicateTableStructure(static::OLD_TABLE_NAME, static::TABLE_NAME);
-        $table = $this->table(static::TABLE_NAME);
-        $table->addColumn('archived', 'boolean');
+        $table = $this->table(static::OLD_TABLE_NAME);
+        $table->addColumn('archived', 'boolean')
+              ->update();
     }
 
     public function down()
     {
         $this->dropTable(static::TABLE_NAME);
+
+        $table = $this->table(static::OLD_TABLE_NAME);
+        $table->removeColumn('archived')
+              ->update();
     }
 
     protected function duplicateTableStructure($oldTableName, $newTableName)
