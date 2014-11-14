1dbd437d822fb34bc580a29a30c34df97a3f1a77
Paul Keelan <paulk@channelgrabber.com>
2014-11-14 17:11:16 +0000

    CGIV-3865: Removed more Di

--- library/CG/InputValidation/Stock/Entity.php
+++ library/CG/InputValidation/Stock/Entity.php
@@ -5,17 +5,9 @@ use CG\Validation\Rules\IntegerValidator;
 use CG\Validation\RulesInterface;
 use Zend\Validator\GreaterThan;
 use Zend\Validator\StringLength;
-use Zend\Di\Di;
 
 class Entity implements RulesInterface
 {
-    protected $di;
-
-    public function __construct(Di $di)
-    {
-        $this->setDi($di);
-    }
-
     public function getRules()
     {
         return [
@@ -28,29 +20,16 @@ class Entity implements RulesInterface
                 'name'       => 'organisationUnitId',
                 'required'   => true,
                 'validators' => [
-                    $this->getDi()->newInstance(IntegerValidator::class, ['name' => 'organisationUnitId']),
-                    $this->getDi()->newInstance(GreaterThan::class, ['options' => ['min' => 1, 'inclusive' => true]])
+                    new IntegerValidator(['name' => 'organisationUnitId']),
+                    (new GreaterThan(['min' => 1, 'inclusive' => true]))
                         ->setMessages(['notGreaterThanInclusive' => 'organisationUnitId must be at least %min%'])
                 ]
             ],
             'sku' => [
                 'name' => 'sku',
                 'required' => true,
-                'validators' => [
-                    $this->getDi()->newInstance(StringLength::class, ['options' => ['min' => 1]])
-                ]
+                'validators' => [new StringLength(['min' => 1])]
             ]
         ];
-    }
-
-    public function setDi($di)
-    {
-        $this->di = $di;
-        return $this;
-    }
-
-    public function getDi()
-    {
-        return $this->di;
     }
 }
