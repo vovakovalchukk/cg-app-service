b99373a7f5e4cd17ab4512d738e766c551d9ea1e
Sean Kenny <sean.kenny@hotmail.com>
2014-11-17 11:13:43 +0000

    CGIV-4120 added empty migration

---
+++ phinx/migrations/20141117093610_migrate_products_to_root_ous.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MigrateProductsToRootOus extends AbstractMigration
+{
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+        // TODO
+        // Actually roll back something here
+        // TODO
+    }
+}
