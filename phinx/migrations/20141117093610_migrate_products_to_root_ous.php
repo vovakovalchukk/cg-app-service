9bd7a5b0a2dcc6b56c6c8f1b0ac80145a183d1fb
Sean Kenny <sean.kenny@hotmail.com>
2014-11-17 15:52:52 +0000

    CGIV-4120 foreach

--- phinx/migrations/20141117093610_migrate_products_to_root_ous.php
+++ phinx/migrations/20141117093610_migrate_products_to_root_ous.php
@@ -6,12 +6,15 @@ class MigrateProductsToRootOus extends AbstractMigration
 {
     public function up()
     {
+        $productIds = $this->query('SELECT `id`, `organisationUnitId` FROM `product`;');
+        foreach ($productIds as $productId) {
+            var_dump($productId);
+        }
+        throw new \Exception('Dont finish migration');
     }
 
     public function down()
     {
-        // TODO
-        // Actually roll back something here
-        // TODO
+        // Noop
     }
 }
