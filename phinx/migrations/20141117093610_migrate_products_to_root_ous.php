8af3c804288b53cbcd20e3a2ee78e60abb2de6ec
Sean Kenny <sean.kenny@hotmail.com>
2014-11-18 13:13:24 +0000

    CGIV-4120 migrations work correctly

--- phinx/migrations/20141117093610_migrate_products_to_root_ous.php
+++ phinx/migrations/20141117093610_migrate_products_to_root_ous.php
@@ -6,11 +6,25 @@ class MigrateProductsToRootOus extends AbstractMigration
 {
     public function up()
     {
-        $productIds = $this->query('SELECT `id`, `organisationUnitId` FROM `product`;');
-        foreach ($productIds as $productId) {
-            var_dump($productId);
+        $products = $this->query('SELECT `id`, `organisationUnitId` FROM `product`;');
+        foreach ($products as $product) {
+            $sql = <<<SQL
+UPDATE `product` SET `organisationUnitId` =
+(
+    SELECT IFNULL(
+        GROUP_CONCAT(parents.id ORDER BY parents.`left` DESC),
+        {$product['organisationUnitId']}
+    ) AS `organisationUnitId`
+    FROM `directory`.`organisationUnit` AS `ou`
+    LEFT JOIN `directory`.`organisationUnit` AS `parents`
+    ON `ou`.`left` > `parents`.`left` AND `ou`.`right` < `parents`.`right`
+    WHERE `ou`.`id` = {$product['organisationUnitId']}
+    GROUP BY `ou`.`id` HAVING ou.id
+    IS NOT NULL ORDER BY `parents`.`left` DESC
+) WHERE `id` = {$product['id']};
+SQL;
+            $this->execute($sql);
         }
-        throw new \Exception('Dont finish migration');
     }
 
     public function down()
