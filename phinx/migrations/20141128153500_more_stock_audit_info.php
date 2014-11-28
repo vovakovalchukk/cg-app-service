7fe320dd3213390f46fdfa075387e2408cdef510
Sean Kenny <sean.kenny@hotmail.com>
2014-11-28 15:38:00 +0000

    CGIV-4258 add empty migration

---
+++ phinx/migrations/20141128153500_more_stock_audit_info.php
@@ -0,0 +1,13 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class MoreStockAuditInfo extends AbstractMigration
+{
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+}
