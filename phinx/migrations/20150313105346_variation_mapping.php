b3f9c9658672f2411a4a2262cddeac865591a5a6
dangerous <dangerousdan@channelgrabber.com>
2015-03-19 16:36:09 +0000

    CGIV-5100 updated composer

--- phinx/migrations/20150313105346_variation_mapping.php
+++ phinx/migrations/20150313105346_variation_mapping.php
@@ -3,18 +3,42 @@ use Phinx\Migration\AbstractMigration;
 
 class VariationMapping extends AbstractMigration
 {
-    public function change()
+    const LISTING_ATTRIBUTE_MAPPING_TABLE = 'listingAttributeMapping';
+    const PRODUCT_ATTRIBUTE_TABLE = 'productAttribute';
+
+    public function up()
     {
-        $productAttribute = $this->table('listingAttributeMapping', ['id' => false, 'primary_key' => ['productAttributeId', 'listingId']]);
-        $productAttribute
+        $this->table(static::LISTING_ATTRIBUTE_MAPPING_TABLE)
             ->addColumn('productAttributeId', 'integer')
             ->addColumn('listingId', 'integer')
             ->addColumn('name', 'string')
             ->addForeignKey('productAttributeId', 'productAttribute', 'id',
-                ['delete' => 'CASCADE', 'update' => 'CASCADE'])
+                ['delete' => 'CASCADE', 'update' => 'NOACTION'])
             ->addForeignKey('listingId', 'listing', 'id',
-                ['delete' => 'CASCADE', 'update' => 'CASCADE'])
-            ->addIndex(['productAttributeId', 'name'])
+                ['delete' => 'CASCADE', 'update' => 'NOACTION'])
+            ->addIndex(['productAttributeId', 'listingId'], ['unique' => true])
             ->create();
+
+        $this->execute('SET FOREIGN_KEY_CHECKS=0');
+        $this->table(static::PRODUCT_ATTRIBUTE_TABLE)
+            ->removeIndex(['productId', 'name'])
+            ->addIndex(['productId', 'name'], ['type' => 'unique'])
+            ->update();
+
+        $this->execute('ALTER TABLE ' . static::PRODUCT_ATTRIBUTE_TABLE . ' CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin');
+        $this->execute('SET FOREIGN_KEY_CHECKS=1');
+    }
+
+    public function down()
+    {
+        $this->execute('SET FOREIGN_KEY_CHECKS=0');
+        $this->table(static::PRODUCT_ATTRIBUTE_TABLE)
+            ->removeIndex(['productId', 'name'])
+            ->addIndex(['productId', 'name'])
+            ->update();
+        $this->execute('SET FOREIGN_KEY_CHECKS=1');
+
+        $this->execute('ALTER TABLE ' . static::PRODUCT_ATTRIBUTE_TABLE . ' CONVERT TO CHARACTER SET utf8');
+        $this->dropTable(static::LISTING_ATTRIBUTE_MAPPING_TABLE);
     }
 }
