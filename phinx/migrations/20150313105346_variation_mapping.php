e64c6659cab8f0dc9c69663f4dd10f38c0cb8a75
dangerous <dangerousdan@channelgrabber.com>
2015-03-19 12:17:12 +0000

    CGIV-5100 renamed table

--- phinx/migrations/20150313105346_variation_mapping.php
+++ phinx/migrations/20150313105346_variation_mapping.php
@@ -5,14 +5,14 @@ class VariationMapping extends AbstractMigration
 {
     public function change()
     {
-        $productAttribute = $this->table('productAttributeMapping', ['id' => false, 'primary_key' => ['productId', 'accountId']]);
+        $productAttribute = $this->table('listingAttributeMapping', ['id' => false, 'primary_key' => ['productAttributeId', 'listingId']]);
         $productAttribute
-            ->addColumn('productId', 'integer')
-            ->addColumn('accountId', 'integer')
+            ->addColumn('productAttributeId', 'integer')
+            ->addColumn('listingId', 'integer')
             ->addColumn('name', 'string')
-            ->addForeignKey('productId', 'product', 'id',
+            ->addForeignKey('productAttributeId', 'productAttribute', 'id',
                 ['delete' => 'CASCADE', 'update' => 'NOACTION'])
-            ->addIndex(['productId', 'name'])
+            ->addIndex(['productAttributeId', 'name'])
             ->create();
     }
 }
