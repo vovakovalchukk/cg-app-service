fec580a4e52b88532bb5532ec4640fd9e48ab9a6
Andu <andua@channelgrabber.com>
2015-03-19 14:47:46 +0000

    CGIV-5165 Add id to variation mapping table

--- phinx/migrations/20150313105346_variation_mapping.php
+++ phinx/migrations/20150313105346_variation_mapping.php
@@ -5,14 +5,15 @@ class VariationMapping extends AbstractMigration
 {
     public function change()
     {
-        $productAttribute = $this->table('productAttributeMapping', ['id' => false, 'primary_key' => ['productId', 'accountId']]);
+        $productAttribute = $this->table('productAttributeMapping');
         $productAttribute
             ->addColumn('productId', 'integer')
             ->addColumn('name', 'string')
             ->addColumn('accountId', 'integer')
             ->addForeignKey('productId', 'product', 'id',
                 ['delete' => 'CASCADE', 'update' => 'NOACTION'])
-            ->addIndex(['productId', 'name'])
+            ->addIndex(['productId', 'accountId'], ['unique' => true])
+            ->addIndex(['name'])
             ->create();
     }
 }
