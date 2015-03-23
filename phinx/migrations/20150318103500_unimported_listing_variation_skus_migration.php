f48de0d5803f619200766147997bf8c40cd8302d
Paul Keelan <paulk@channelgrabber.com>
2015-03-23 15:46:34 +0000

    CGIV-5136: Modified unimported listing variation SKUs phinx migration to up/down

--- phinx/migrations/20150318103500_unimported_listing_variation_skus_migration.php
+++ phinx/migrations/20150318103500_unimported_listing_variation_skus_migration.php
@@ -3,14 +3,20 @@ use Phinx\Migration\AbstractMigration;
 
 class UnimportedListingVariationSkusMigration extends AbstractMigration
 {
-    public function change()
+    public function up()
     {
         $unimportedListingVariationSkusTable = $this->table(
             'unimportedListingVariationSkus',
             ['id' => false, 'primary_key' => ['unimportedListingId', 'variationSku']]
         );
-        $unimportedListingVariationSkusTable->addColumn('unimportedListingId', 'integer')
+        $unimportedListingVariationSkusTable
+            ->addColumn('unimportedListingId', 'integer')
             ->addColumn('variationSku', 'string')
             ->create();
+    }
+
+    public function down()
+    {
+        $this->table('unimportedListingVariationSkus')->drop();
     }
 }
