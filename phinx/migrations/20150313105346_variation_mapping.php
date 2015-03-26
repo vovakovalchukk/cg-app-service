3d87705b9b509e86a82da9ab4f5add7afbff9076
dangerous <dangerousdan@channelgrabber.com>
2015-03-26 12:24:16 +0000

    CGIV-5100 updated composer

--- phinx/migrations/20150313105346_variation_mapping.php
+++ phinx/migrations/20150313105346_variation_mapping.php
@@ -30,6 +30,14 @@ class VariationMapping extends AbstractMigration
 
         $this->execute('ALTER TABLE ' . static::PRODUCT_ATTRIBUTE_TABLE . ' CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin');
         $this->execute('SET FOREIGN_KEY_CHECKS=1');
+
+        $this->execute('INSERT IGNORE INTO variationAttributeMap (productId, productAttributeId, listingId, name)
+                        SELECT p.id as productId, pa.id as productAttributeId, plm.listingId, pa.name
+                        FROM product p
+                        INNER JOIN productToListingMap plm ON p.id = plm.productId
+                        INNER JOIN listing l ON plm.listingId = l.id
+                        INNER JOIN productAttribute pa ON p.id = pa.productId
+                        WHERE p.parentProductId = 0');
     }
 
     public function down()
