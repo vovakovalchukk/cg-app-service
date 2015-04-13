a12cd81be837add21d5c783c732dbe51ac22f82e
Sean Kenny <sean.kenny@hotmail.com>
2015-04-13 17:10:58 +0100

    CGIV-5323 fetch data instead of defaulting it

--- library/CG/Slim/Versioning/OrderEntity/Versioniser2.php
+++ library/CG/Slim/Versioning/OrderEntity/Versioniser2.php
@@ -4,14 +4,23 @@ namespace CG\Slim\Versioning\OrderEntity;
 use CG\Slim\Versioning\VersioniserInterface;
 use Nocarrier\Hal;
 use CG\Order\Shared\Entity as Order;
+use CG\Order\Service\Service;
+use CG\Stdlib\Exception\Runtime\NotFound;
 
 class Versioniser2 implements VersioniserInterface
 {
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->setService($service);
+    }
+
     public function upgradeRequest(array $params, Hal $request)
     {
         $data = $request->getData();
         if (!isset($data['fulfilmentChannel'])) {
-            $data['fulfilmentChannel'] = Order::DEFAULT_FULFILMENT_CHANNEL;
+            $data = $this->setFulfillmentChannelOnData($data);
         }
         $request->setData($data);
     }
@@ -21,5 +30,36 @@ class Versioniser2 implements VersioniserInterface
         $data = $response->getData();
         unset($data['fulfilmentChannel']);
         $response->setData($data);
+    }
+
+    protected function setFulfillmentChannelOnData(array $data)
+    {
+        if (!isset($data["id"])) {
+            $data["fulfilmentChannel"] = Order::DEFAULT_FULFILMENT_CHANNEL;
+            return $data;
+        }
+
+        try {
+            $order = $this->getService()->fetch($data["id"]);
+            $data["fulfilmentChannel"] = $order->getFulfilmentChannel();
+            return $data;
+        } catch (NotFound $e) {
+            $data["fulfilmentChannel"] = Order::DEFAULT_FULFILMENT_CHANNEL;
+            return $data;
+        }
+    }
+
+    public function setService(Service $service)
+    {
+        $this->service = $service;
+        return $this;
+    }
+
+    /**
+     * @return Service
+     */
+    public function getService()
+    {
+        return $this->service;
     }
 }
--- library/CG/Slim/Versioning/OrderItemEntity/Versioniser4.php
+++ library/CG/Slim/Versioning/OrderItemEntity/Versioniser4.php
@@ -3,15 +3,24 @@ namespace CG\Slim\Versioning\OrderItemEntity;
 
 use CG\Slim\Versioning\VersioniserInterface;
 use Nocarrier\Hal;
-use CG\Order\Item\Entity as Item;
+use CG\Order\Shared\Item\Entity as Item;
+use CG\Order\Service\Item\Service as Service;
+use CG\Stdlib\Exception\Runtime\NotFound;
 
 class Versioniser4 implements VersioniserInterface
 {
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->setService($service);
+    }
+
     public function upgradeRequest(array $params, Hal $request)
     {
         $data = $request->getData();
         if (!isset($data['isStockManaged'])) {
-            $data['isStockManaged'] = Item::DEFAULT_IS_STOCK_MANAGED;
+            $data = $this->setIsStockManagedOnData($data);
         }
         $request->setData($data);
     }
@@ -21,6 +30,37 @@ class Versioniser4 implements VersioniserInterface
         $data = $response->getData();
         unset($data['isStockManaged']);
         $response->setData($data);
+    }
+
+    protected function setIsStockManagedOnData(array $data)
+    {
+        if (!isset($data["id"])) {
+            $data["isStockManaged"] = Item::DEFAULT_IS_STOCK_MANAGED;
+            return $data;
+        }
+
+        try {
+            $item = $this->getService()->fetch($data["id"]);
+            $data["isStockManaged"] = $item->isStockManaged();
+            return $data;
+        } catch (NotFound $e) {
+            $data["isStockManaged"] = Item::DEFAULT_IS_STOCK_MANAGED;
+            return $data;
+        }
+    }
+
+    public function setService(Service $service)
+    {
+        $this->service = $service;
+        return $this;
+    }
+
+    /**
+     * @return Service
+     */
+    public function getService()
+    {
+        return $this->service;
     }
 }
 
