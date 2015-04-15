7bbffe68fff66b5598a0778ad24bee056b878a17
Paul Keelan <paulk@channelgrabber.com>
2015-04-15 15:04:34 +0100

    CGIV-5310: Modified the listing unique key migration to better check and delete any bad entities which are invalid to the new key

--- phinx/migrations/20150414162900_listing_unique_key.php
+++ phinx/migrations/20150414162900_listing_unique_key.php
@@ -5,10 +5,11 @@ class ListingUniqueKey extends AbstractMigration
 {
     public function up()
     {
-        $orderTable = $this->table('listing');
-        $orderTable
-            ->addIndex(['accountId', 'externalId'], ['unique' => true])
-            ->save();
+        $this->execute("DELETE * FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
+        $this->execute("SET foreign_key_checks = 0;");
+        $this->execute("ALTER IGNORE TABLE listing ADD UNIQUE INDEX accountIdExternalId (accountId, externalId);");
+        $this->execute("SET foreign_key_checks = 1;");
+        $this->execute("DELETE * FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
     }
 
     public function down()
