bd9b96d5f6980f5074c234a27d63152a067bd9ae
dangerous <dangerousdan@channelgrabber.com>
2015-04-15 16:48:21 +0100

    CGIV-5353 corrected migration

--- phinx/migrations/20150415144606_order_item_rename_to_stock_managed.php
+++ phinx/migrations/20150415144606_order_item_rename_to_stock_managed.php
@@ -7,11 +7,19 @@ class OrderItemRenameToStockManaged extends AbstractMigration
 {
     const TABLE_NAME = 'item';
 
-    public function change()
+    public function up()
     {
         $this->table(static::TABLE_NAME)
             ->removeColumn('isStockManaged')
             ->addColumn('stockManaged', 'boolean', ['default' => Item::DEFAULT_IS_STOCK_MANAGED])
+            ->update();
+    }
+
+    public function down()
+    {
+        $this->table(static::TABLE_NAME)
+            ->removeColumn('stockManaged')
+            ->addColumn('isStockManaged', 'boolean', ['default' => Item::DEFAULT_IS_STOCK_MANAGED])
             ->update();
     }
 }
