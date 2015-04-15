cbaab4ce020939b3cf9817a62099297a595e9935
Paul Keelan <paulk@channelgrabber.com>
2015-04-15 16:02:50 +0100

    CGIV-5310: Removed odd *'s from listing unique key phinx migration

--- phinx/migrations/20150414162900_listing_unique_key.php
+++ phinx/migrations/20150414162900_listing_unique_key.php
@@ -5,11 +5,11 @@ class ListingUniqueKey extends AbstractMigration
 {
     public function up()
     {
-        $this->execute("DELETE * FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
+        $this->execute("DELETE FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
         $this->execute("SET foreign_key_checks = 0;");
         $this->execute("ALTER IGNORE TABLE listing ADD UNIQUE INDEX accountIdExternalId (accountId, externalId);");
         $this->execute("SET foreign_key_checks = 1;");
-        $this->execute("DELETE * FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
+        $this->execute("DELETE FROM productToListingMap WHERE listingId NOT IN (SELECT id FROM listing);");
     }
 
     public function down()
