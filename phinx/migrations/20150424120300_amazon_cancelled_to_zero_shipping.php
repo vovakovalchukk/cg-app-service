d95ba7cb2b3cf18d06aa99f0565ef57410c966af
Paul Keelan <paulk@channelgrabber.com>
2015-04-24 12:23:00 +0100

    CGIV-5054: Removed an unused constant

--- phinx/migrations/20150424120300_amazon_cancelled_to_zero_shipping.php
+++ phinx/migrations/20150424120300_amazon_cancelled_to_zero_shipping.php
@@ -5,8 +5,6 @@ use CG\Order\Shared\Item\Entity as Item;
 
 class AmazonCancelledToZeroShipping extends AbstractMigration
 {
-    const TABLE_NAME = 'order';
-
     public function change()
     {
         $this->execute("UPDATE `order` SET shippingPrice=0 WHERE status='cancelled' && channel='amazon'");
