406c0048919d98c21dae3e710e59e7bbffdc8867
Aaron Baker <aaronb@channelgrabber.com>
2015-06-03 15:46:06 +0100

    CGIV-5703: added more integration tests for the Redis Sequential Number provider

--- tests/unit/CG/Stdlib/Test/SequentialNumber/Provider/RedisTest.php
+++ tests/unit/CG/Stdlib/Test/SequentialNumber/Provider/RedisTest.php
@@ -2,6 +2,7 @@
 namespace CG\Stdlib\Test\SequentialNumber\Provider;
 
 use CG\Stdlib\SequentialNumber\Provider\Redis;
+use CG\Stdlib\SequentialNumber\LockException;
 use PHPUnit_Framework_TestCase;
 use Predis\Client as Predis;
 
@@ -34,28 +35,118 @@ class RedisTest extends PHPUnit_Framework_TestCase
     {
         $sequenceName = static::SEQUENCE_NAME;
         $result = $this->provider->getNext($sequenceName);
-        $this->assertEquals(1, $result);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
     }
 
     /**
      * @group integration
      * @backupGlobals disabled
      */
-    public function testGetNextReturnsSequentialNumbers()
+    public function testGetNextThrowsExceptionIfLockNotReleased()
     {
         $sequenceName = static::SEQUENCE_NAME;
-        
-        $firstResult = $this->provider->getNext($sequenceName);
-        $this->assertEquals(1, $firstResult);
-        $this->provider->markUsed($sequenceName, $firstResult);
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        $this->setExpectedException(LockException::class);
+        $this->provider->getNext($sequenceName);
+    }
 
-        $secondResult = $this->provider->getNext($sequenceName);
-        $this->assertEquals(2, $secondResult);
-        $this->provider->markUsed($sequenceName, $secondResult);
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testMarkUsedSucceedsForCorrectNumber()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        $this->provider->markUsed($sequenceName, $result);
+        // An exception would be thrown if there was a problem
+        $this->assertTrue(true);
+    }
 
-        $thirdResult = $this->provider->getNext($sequenceName);
-        $this->assertEquals(3, $thirdResult);
-        $this->provider->markUsed($sequenceName, $thirdResult);
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testMarkUsedThrowsExceptionForIncorrectNumber()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        $this->setExpectedException(\UnexpectedValueException::class);
+        $this->provider->markUsed($sequenceName, $result+1);
+    }
+
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testGetNextReturnsSequentialNumbersWhenEachIsUsed()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $max = 5;
+        for ($count = 1; $count <= $max; $count++) {
+            $result = $this->provider->getNext($sequenceName);
+            $this->assertEquals($count, $result);
+            $this->provider->markUsed($sequenceName, $result);
+        }
+    }
+
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testLockExceptionCanBeOvercomeWithMarkUsed()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        try {
+            $this->provider->getNext($sequenceName);
+            $this->assertTrue(false, 'LockException should have been thrown');
+
+        } catch (LockException $e) {
+            $this->assertEquals(1, $e->getLockedNumber(), 'Locked number referenced in exception not as expected');
+            $this->provider->markUsed($sequenceName, $e->getLockedNumber());
+            $result = $this->provider->getNext($sequenceName);
+            $this->assertEquals(2, $result, 'Second sequential number not 2');
+        }
+    }
+
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testReleaseResetsNextNumber()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        $this->provider->release($sequenceName, $result);
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'Second attempt at first sequential number not 1');
+    }
+
+    /**
+     * @group integration
+     * @backupGlobals disabled
+     */
+    public function testLockExceptionCanBeOvercomeWithRelease()
+    {
+        $sequenceName = static::SEQUENCE_NAME;
+        $result = $this->provider->getNext($sequenceName);
+        $this->assertEquals(1, $result, 'First sequential number not 1');
+        try {
+            $this->provider->getNext($sequenceName);
+            $this->assertTrue(false, 'LockException should have been thrown');
+
+        } catch (LockException $e) {
+            $this->assertEquals(1, $e->getLockedNumber(), 'Locked number referenced in exception not as expected');
+            $this->provider->release($sequenceName, $e->getLockedNumber());
+            $result = $this->provider->getNext($sequenceName);
+            $this->assertEquals(1, $result, 'Second attempt at first sequential number not 1');
+        }
     }
 
     protected function resetForIntegrationTests($provider, $sequenceName)
