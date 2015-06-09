da98737c51388864e28f6a910adb1939b1a16915
Aaron Baker <aaronb@channelgrabber.com>
2015-06-09 17:12:10 +0100

    CGIV-5783: added command to fix stock levels for cancelled, dispatched, refunded orders.

--- config/console/commands/order.php
+++ config/console/commands/order.php
@@ -1,8 +1,10 @@
 <?php
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use CG\Order\Shared\Command\ApplyMissingStockAdjustmentsForCancDispRefOrders;
 use CG\Order\Shared\Command\CorrectStockOfItemsWithIncorrectStockManagedFlag;
 use CG\Order\Shared\Command\UpdateAllItemsTax;
+use DateTime;
 
 return [
     'order:updateAllItemsTax' => [
@@ -34,6 +36,29 @@ return [
             $dryRun = $input->getOption('dry-run');
             $command = $di->get(CorrectStockOfItemsWithIncorrectStockManagedFlag::class);
             $command($start, $dryRun);
+        }
+    ],
+    'order:applyMissingStockAdjustmentsForCancDispRefOrders' => [
+        'description' => "Identify Orders in a given period that seemingly did not apply stock adjustments when they were cancelled, dispatched or refunded and apply those adjustments",
+        'options' => [
+            'dry-run' => [
+                'description' => 'Dry run - will gather the data but not actually alter the Stock'
+            ]
+        ],
+        'arguments' => [
+            'start' => [
+                'required' => true
+            ],
+            'end' => [
+                'required' => true
+            ]
+        ],
+        'command' => function (InputInterface $input, OutputInterface $output) use ($di) {
+            $start = new DateTime($input->getArgument('start'));
+            $end = new DateTime($input->getArgument('end'));
+            $dryRun = $input->getOption('dry-run');
+            $command = $di->get(ApplyMissingStockAdjustmentsForCancDispRefOrders::class);
+            $command($start, $end, $dryRun);
         }
     ]
 ];
