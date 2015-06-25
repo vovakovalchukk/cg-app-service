b23289b82d132f43cbb65357106394560387750a
Sean Kenny <sean.kenny@hotmail.com>
2015-06-25 15:03:11 +0100

    CGIV-5853 add originalId column to order and orderLive tables

---
+++ phinx/migrations/20150625135900_add_original_order_id.php
@@ -0,0 +1,18 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class AddOriginalOrderId extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('order')
+            ->addColumn("originalId", "string", ["limit" => 120])
+            ->update();
+        $this->table('orderLive')
+            ->addColumn("originalId", "string", ["limit" => 120])
+            ->update();
+
+        $this->execute("UPDATE `order` SET originalId = id");
+        $this->execute("UPDATE `orderLive` SET originalId = id");
+    }
+}
