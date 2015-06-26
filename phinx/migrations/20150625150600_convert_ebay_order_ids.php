9d80f609579dd4a6ca66a64fe4904b595fba82a2
Sean Kenny <sean.kenny@hotmail.com>
2015-06-26 14:18:59 +0100

    CGIV-5853 add migration for applying new style ids everywhere

---
+++ phinx/migrations/20150625150600_convert_ebay_order_ids.php
@@ -0,0 +1,70 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class ConvertEbayOrderIds extends AbstractMigration
+{
+    public function up()
+    {
+        $orderIds = $this->fetchAll($this->getEbayOrderIdQuery());
+
+        $this->execute("START TRANSACTION;");
+
+        foreach ($orderIds as $orderId) {
+            $newId = $orderId["accountId"] . "-" . $orderId["ebayOrderId"];
+            $this->execute($this->getUpdate($newId, $orderId["originalId"]));
+        }
+
+        $this->execute("COMMIT;");
+    }
+
+    public function down()
+    {
+        // TODO
+        // Use originalId in cg_app.order table to revert the change
+    }
+
+    protected function getEbayOrderIdQuery()
+    {
+        return <<<SQL
+SELECT orderAdditional.id AS originalId, orderAdditional.ebayOrderId, `order`.accountId
+FROM cg_app.`order`
+RIGHT JOIN ebay.orderAdditional
+ON `order`.id=orderAdditional.id;
+SQL;
+    }
+
+    protected function getUpdate($newId, $originalId)
+    {
+        $sql = "";
+        $updateTemplate = $this->getUpdateOrderIdQuery();
+        foreach ($this->getAffectedTables() as $table => $column) {
+            $sql .= sprintf($updateTemplate, $table, $column, $newId, $column, $originalId);
+        }
+        return $sql;
+    }
+
+    protected function getAffectedTables()
+    {
+        return [
+            "ebay.orderAdditional" => "id",
+            "cg_app.`order`" => "id",
+            "cg_app.orderLive" => "id",
+            "cg_app.item" => "orderId",
+            "cg_app.orderTag" => "orderId",
+            "cg_app.note" => "orderId",
+            "cg_app.tracking" => "orderId",
+            "cg_app.alert" => "orderId",
+            "cg_app.cancel" => "orderId",
+            "cg_app.cancelItem" => "orderId",
+        ];
+    }
+
+    protected function getUpdateOrderIdQuery()
+    {
+        return <<<SQL
+UPDATE %s
+SET %s="%s"
+WHERE %s="%s";
+SQL;
+    }
+}
