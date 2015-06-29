bb364d82786e0c34e2a9b70b1186f8d8700d3271
Sean Kenny <sean.kenny@hotmail.com>
2015-06-29 15:17:13 +0100

    CGIV-5814add migration for adjustment log table

---
+++ phinx/migrations/20150629114900_stock_adjustment_log_table.php
@@ -0,0 +1,27 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class StockAdjustmentLogTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('stockLog')
+            ->addColumn('date', 'date', ['null' => true])
+            ->addColumn('time', 'time', ['null' => true])
+            ->addColumn('stid', 'string', ['null' => true])
+            ->addColumn('itid', 'string', ['null' => true])
+            ->addColumn('action', 'string', ['null' => true])
+            ->addColumn('organisationUnitId', 'integer', ['null' => true, 'signed' => false])
+            ->addColumn('accountId', 'integer', ['null' => true, 'signed' => false])
+            ->addColumn('listingId', 'integer', ['null' => true, 'signed' => false])
+            ->addColumn('productId', 'integer', ['null' => true, 'signed' => false])
+            ->addColumn('sku', 'string', ['null' => true])
+            ->addColumn('stockManagement', 'boolean', ['null' => true])
+            ->addColumn('type', 'string', ['null' => true])
+            ->addColumn('operator', 'string', ['null' => true])
+            ->addColumn('quantity', 'integer', ['null' => true])
+            ->addColumn('applied', 'boolean', ['null' => true, 'default' => false])
+            ->addIndex(['organisationUnitId', 'sku'])
+            ->create();
+    }
+}
