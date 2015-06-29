61fcfe34ddffa733cc9719176ac56a04b5ab8651
Richard Heelin <richardh@channelgrabber.com>
2015-06-29 14:12:19 +0100

    CGIV-5853: Fix adding of new column

--- phinx/migrations/20150625135900_add_original_order_id.php
+++ phinx/migrations/20150625135900_add_original_order_id.php
@@ -3,20 +3,34 @@ use Phinx\Migration\AbstractMigration;
 
 class AddOriginalOrderId extends AbstractMigration
 {
-    public function change()
+    public function up()
     {
-        $this->execute("START TRANSACTION;");
-        $this->table('order')
-            ->addColumn("originalId", "string", ["limit" => 120])
-            ->addIndex(["originalId"], ['unique' => true])
-            ->update();
-        $this->table('orderLive')
-            ->addColumn("originalId", "string", ["limit" => 120])
-            ->addIndex(["originalId"], ['unique' => true])
-            ->update();
+        $this->getAdapter()->beginTransaction();
 
-        $this->execute("UPDATE `order` SET originalId = id");
-        $this->execute("UPDATE `orderLive` SET originalId = id");
-        $this->execute("COMMIT;");
+        foreach ($this->getTables() as $table) {
+            $this->table($table)->addColumn('originalId', 'string', ['limit' => 120])->update();
+            $this->execute(sprintf('UPDATE `%s` SET `originalId` = `id`', $table));
+            $this->table($table)->addIndex(['originalId'], ['unique' => true])->update();
+        }
+
+        $this->getAdapter()->commitTransaction();
+    }
+
+    public function down()
+    {
+        $this->getAdapter()->beginTransaction();
+
+        foreach ($this->getTables() as $table) {
+            $this->table($table)->removeIndex(['originalId'], ['unique' => true])->update();
+            $this->execute(sprintf('UPDATE `%s` SET `id` = `originalId`', $table));
+            $this->table($table)->removeColumn('originalId')->update();
+        }
+
+        $this->getAdapter()->commitTransaction();
+    }
+
+    protected function getTables()
+    {
+        return ['order', 'orderLive'];
     }
 }
