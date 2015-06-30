123a80ff886384e9c805f89d90a73fd0b57e87e6
Sean Kenny <sean.kenny@hotmail.com>
2015-06-30 15:58:47 +0100

    CGIV-5812 correct itid type and add indexes

--- phinx/migrations/20150626091840_stock_log_table.php
+++ phinx/migrations/20150626091840_stock_log_table.php
@@ -8,7 +8,7 @@ class StockLogTable extends AbstractMigration
         $this->table('stockLog')
             ->addColumn('date', 'date', ['null' => true])
             ->addColumn('time', 'time', ['null' => true])
-            ->addColumn('itid', 'integer', ['null' => true])
+            ->addColumn('itid', 'string', ['null' => true])
             ->addColumn('organisationUnitId', 'integer', ['null' => true, 'signed' => false])
             ->addColumn('sku', 'string', ['null' => true])
             ->addColumn('stockId', 'integer', ['null' => true, 'signed' => false])
@@ -16,6 +16,11 @@ class StockLogTable extends AbstractMigration
             ->addColumn('allocatedQty', 'integer', ['null' => true])
             ->addColumn('onHandQty', 'integer', ['null' => true])
             ->addIndex(['organisationUnitId', 'sku'])
+            ->addIndex(['date', 'time'])
+            ->addIndex(['itid'])
+            ->addIndex(['stockId'])
+            ->addIndex(['locationId'])
+            ->addIndex(['sku'])
             ->create();
     }
 }
