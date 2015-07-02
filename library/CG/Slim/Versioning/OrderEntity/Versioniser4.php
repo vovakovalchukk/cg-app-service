16def15661fbffd53b1f875029a1ad82dc951366
Richard Heelin <richardh@channelgrabber.com>
2015-07-02 09:55:13 +0100

    CGIV-5883: Added new column emailDate to track when we emailed the customer for an order

---
+++ library/CG/Slim/Versioning/OrderEntity/Versioniser4.php
@@ -0,0 +1,56 @@
+<?php
+namespace CG\Slim\Versioning\OrderEntity;
+
+use CG\Order\Service\Service;
+use CG\Order\Shared\Entity as Order;
+use CG\Slim\Versioning\VersioniserInterface;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use Nocarrier\Hal;
+
+class Versioniser4 implements VersioniserInterface
+{
+    /**
+     * @var Service $service
+     */
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->setService($service);
+    }
+
+    public function upgradeRequest(array $params, Hal $request)
+    {
+        $data = $request->getData();
+        if (!isset($data['id']) || isset($data['emailDate'])) {
+            return;
+        }
+
+        try {
+            /**
+             * @var Order $order
+             */
+            $order = $this->service->fetch($data['id']);
+            $data['emailDate'] = $order->getEmailDate();
+            $request->setData($data);
+        } catch (NotFound $exception) {
+            // New order so there won't be a previously set emailDate
+        }
+    }
+
+    public function downgradeResponse(array $params, Hal $response, $requestedVersion)
+    {
+        $data = $response->getData();
+        unset($data['emailDate']);
+        $response->setData($data);
+    }
+
+    /**
+     * @return self
+     */
+    protected function setService(Service $service)
+    {
+        $this->service = $service;
+        return $this;
+    }
+} 
