9ae6aa5cd2bd671f56229656433a3ee2e587bbfa
Richard Heelin <richardh@channelgrabber.com>
2015-07-06 10:38:27 +0100

    Revert "Revert "Deployment of CGIV-5853""
    This reverts commit 7bb2e677e49cc61eba972178e73c992b46e38b09.

---
+++ phinx/migrations/20150625135900_add_original_order_id.php
@@ -0,0 +1,28 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class AddOriginalOrderId extends AbstractMigration
+{
+    public function up()
+    {
+        foreach ($this->getTables() as $table) {
+            $this->table($table)->addColumn('originalId', 'string', ['limit' => 120])->update();
+            $this->execute(sprintf('UPDATE `%s` SET `originalId` = `id`', $table));
+            $this->table($table)->addIndex(['originalId'], ['unique' => true])->update();
+        }
+    }
+
+    public function down()
+    {
+        foreach ($this->getTables() as $table) {
+            $this->table($table)->removeIndex(['originalId'], ['unique' => true])->update();
+            $this->execute(sprintf('UPDATE `%s` SET `id` = `originalId`', $table));
+            $this->table($table)->removeColumn('originalId')->update();
+        }
+    }
+
+    protected function getTables()
+    {
+        return ['order', 'orderLive'];
+    }
+}
