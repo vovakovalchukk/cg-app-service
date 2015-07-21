77ff34338c47e90c2fbb643843552e3dbbe37a71
Aaron Baker <aaronb@channelgrabber.com>
2015-07-21 14:23:00 +0100

    CGIV-5975: brought in command to audit all stock in the system

--- config/console/commands/stock.php
+++ config/console/commands/stock.php
@@ -1,6 +1,8 @@
 <?php
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use CG\Stock\Command\AuditAllStock;
 use CG\Stock\Command\ProcessAudit;
 use CG\Stock\Gearman\WorkerFunction\ProcessAudit as StockWorker;
 use CG\Stock\Gearman\WorkerFunction\ProcessAdjustmentAudit as AdjustmentWorker;
@@ -35,6 +37,20 @@ return [
                 'required' => false,
                 'default' => 100
             ]
+        ],
+        'options' => [
+
+        ]
+    ],
+    'stock:auditAll' => [
+        'command' => function (InputInterface $input, OutputInterface $output) use ($di) {
+            $output->writeln('Starting Audit All Stock command');
+            $command = $di->get(AuditAllStock::class);
+            $command();
+            $output->writeln('Done. Check the logs for details.');
+        },
+        'description' => 'Create an entry in the stockLog table for all stock in the system',
+        'arguments' => [
         ],
         'options' => [
 
