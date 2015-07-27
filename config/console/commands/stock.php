85c3fce3e59533b4e053a1e5f810f46a7fc0fcf7
Aaron Baker <aaronb@channelgrabber.com>
2015-07-27 15:06:18 +0100

    CGIV-6020: Added command to identify duplicated stock adjustments and zero any resulting negative stock levels.

--- config/console/commands/stock.php
+++ config/console/commands/stock.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use CG\Stock\Command\AuditAllStock;
+use CG\Stock\Command\ZeroNegativeStockForDuplicatedAdjustments;
 use CG\Stock\Command\ProcessAudit;
 use CG\Stock\Gearman\WorkerFunction\ProcessAudit as StockWorker;
 use CG\Stock\Gearman\WorkerFunction\ProcessAdjustmentAudit as AdjustmentWorker;
@@ -55,5 +56,20 @@ return [
         'options' => [
 
         ]
+    ],
+    'stock:zeroNegativeStockForDuplicatedAdjustments' => [
+        'command' => function (InputInterface $input, OutputInterface $output) use ($di) {
+            $dryRun = $input->getOption('dry-run');
+            $command = $di->get(ZeroNegativeStockForDuplicatedAdjustments::class, ['output' => $output]);
+            $command($dryRun);
+        },
+        'description' => 'Identify duplicated stock adjustments and zero any resulting negative stock levels',
+        'arguments' => [
+        ],
+        'options' => [
+            'dry-run' => [
+                'description' => 'Dry run - will gather the data but not actually alter the Stock'
+            ]
+        ],
     ]
 ];
