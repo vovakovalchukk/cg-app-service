e461b5ac0c65a0f4ffce75408d7a18a9777f6222
Richard Heelin <richardh@channelgrabber.com>
2015-08-17 14:43:50 +0100

    CGIV-6047: Create shell controllers for api endpoint

--- controllers/CG/Controllers/Settings/Api.php
+++ controllers/CG/Controllers/Settings/Api.php
@@ -1,9 +1,55 @@
 <?php
 namespace CG\Controllers\Settings;
 
-class Api
+use CG\Http\Exception\Exception4xx\NotFound as HttpNotFound;
+use CG\Http\StatusCode;
+use CG\Settings\Api\Service;
+use CG\Slim\ControllerTrait;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
+use NoCarrier\Hal;
+use Slim\Slim;
+
+class Api implements LoggerAwareInterface
 {
-    public function __construct()
+    use ControllerTrait, LogTrait;
+
+    public function __construct(Slim $app, Service $service)
     {
+        $this->setSlim($app)->setService($service);
     }
-} 
+
+    public function get($id)
+    {
+        try {
+            return $this->getService()->fetchAsHal($id);
+        } catch (NotFound $exception) {
+            throw new HttpNotFound(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+    }
+
+    public function put($id, Hal $hal)
+    {
+        $this->logDebug('Settings/Api/' . $id . ' was PUT');
+        return $this->getService()->saveHal($hal, ['id' => $id]);
+    }
+
+    public function delete($id)
+    {
+        try {
+            $this->getService()->removeById($id);
+            $this->getSlim()->response()->setStatus(StatusCode::NO_CONTENT);
+        } catch (NotFound $exception) {
+            throw new HttpNotFound(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+    }
+}
--- controllers/CG/Controllers/Settings/Api/Collection.php
+++ controllers/CG/Controllers/Settings/Api/Collection.php
@@ -1,9 +1,38 @@
 <?php
 namespace CG\Controllers\Settings\Api;
 
+use CG\Http\Exception\Exception4xx\NotFound as HttpNotFound;
+use CG\Settings\Api\Filter;
+use CG\Settings\Api\Service;
+use CG\Slim\ControllerTrait;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use Slim\Slim;
+
 class Collection
 {
-    public function __construct()
+    use ControllerTrait;
+
+    public function __construct(Slim $app, Service $service)
     {
+        $this->setSlim($app)->setService($service);
+    }
+
+    public function get()
+    {
+        try {
+            return $this->getService()->fetchCollectionAsHal(
+                new Filter(
+                    $this->getParams('limit'),
+                    $this->getParams('page'),
+                    $this->getParams('id') ?: []
+                )
+            );
+        } catch (NotFound $exception) {
+            throw new HttpNotFound(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
     }
 } 
