d8bd33ef62403528ab2c59fece97b36063101b62
Richard Heelin <richardh@channelgrabber.com>
2015-08-27 13:45:11 +0100

    CGIV-6136: Added support for marketplaces on listings and unimported listings table

---
+++ phinx/migrations/20150827132624_listing_marketplace_support.php
@@ -0,0 +1,44 @@
+<?php
+use CG\Amazon\Region\Urls;
+use Phinx\Migration\AbstractMigration;
+
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+class ListingMarketplaceSupport extends AbstractMigration
+{
+    protected function getTableNames()
+    {
+        return ['listing', 'unimportedListing'];
+    }
+
+    protected function getMarketplaces()
+    {
+        return (new Urls())->getUrls();
+    }
+
+    public function up()
+    {
+        foreach ($this->getTableNames() as $tableName) {
+            $this
+                ->table($tableName)
+                ->addColumn('marketplace', 'string', ['null' => true])
+                ->update();
+
+            foreach ($this->getMarketplaces() as $marketplace => $url) {
+                $this->execute(
+                    sprintf('UPDATE %s SET marketplace = "%s" WHERE url LIKE "%s%%"', $tableName, $marketplace, $url)
+                );
+            }
+        }
+    }
+
+    public function down()
+    {
+        foreach ($this->getTableNames() as $tableName) {
+            $this
+                ->table($tableName)
+                ->removeColumn('marketplace')
+                ->update();
+        }
+    }
+}
