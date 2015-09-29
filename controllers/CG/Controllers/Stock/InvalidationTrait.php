f65f6c4fbb0083ea2eb374645a509012d0613bd3
Richard Heelin <richardh@channelgrabber.com>
2015-09-29 12:18:20 +0100

    CGIV-6233: Updated the stock trait so that it returns the stock entity just like the stock location trait

--- controllers/CG/Controllers/Stock/InvalidationTrait.php
+++ controllers/CG/Controllers/Stock/InvalidationTrait.php
@@ -8,6 +8,9 @@ use Nocarrier\Hal;
 
 trait InvalidationTrait
 {
+    /**
+     * @return Stock
+     */
     protected function invalidateStockHal(Hal $stockHal)
     {
         return $this->invalidateStock(
@@ -15,9 +18,13 @@ trait InvalidationTrait
         );
     }
 
+    /**
+     * @return Stock
+     */
     protected function invalidateStock(Stock $stock)
     {
         $this->getInvalidator()->invalidateProductsForStock($stock);
+        return $stock;
     }
 
     /**
