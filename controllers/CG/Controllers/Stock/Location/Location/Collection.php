2f9270dcb1745c49d0074a15ab99c422e0a36c91
Richard Heelin <richardh@channelgrabber.com>
2015-09-29 11:38:33 +0100

    CGIV-6233: Ensure that we invalidate related stock and products when ever a stock location is created or modified

--- controllers/CG/Controllers/Stock/Location/Location/Collection.php
+++ controllers/CG/Controllers/Stock/Location/Location/Collection.php
@@ -1,22 +1,58 @@
 <?php
 namespace CG\Controllers\Stock\Location\Location;
 
-use CG\Stock\Location\Service;
-use CG\Slim\ControllerTrait;
+use CG\CGLib\Nginx\Cache\Invalidator\ProductStock as Invalidator;
+use CG\Controllers\Stock\Location\InvalidationTrait;
 use CG\Slim\Controller\Collection\GetTrait;
 use CG\Slim\Controller\Collection\PostTrait;
+use CG\Slim\ControllerTrait;
+use CG\Stock\Location\Mapper as StockLocationMapper;
+use CG\Stock\Location\Service;
+use CG\Stock\Service as StockService;
+use Exception;
+use Nocarrier\Hal;
 use Slim\Slim;
 use Zend\Di\Di;
 
 class Collection
 {
-    use ControllerTrait, GetTrait, PostTrait;
+    use ControllerTrait;
+    use GetTrait;
+    use PostTrait {
+        post as postTrait;
+    }
+    use InvalidationTrait;
 
-    public function __construct(Slim $app, Service $service, Di $di)
-    {
-        $this->setSlim($app)
+    protected $stockLocationMapper;
+    protected $stockService;
+    protected $invalidator;
+
+    public function __construct(
+        Slim $app,
+        Service $service,
+        Di $di,
+        StockLocationMapper $stockLocationMapper,
+        StockService $stockService,
+        Invalidator $invalidator
+    ) {
+        $this
+            ->setSlim($app)
             ->setService($service)
-            ->setDi($di);
+            ->setDi($di)
+            ->setStockLocationMapper($stockLocationMapper)
+            ->setStockService($stockService)
+            ->setInvalidator($invalidator);
+    }
+
+    public function post(Hal $hal)
+    {
+        $stockLocationHal = $this->postTrait($hal);
+        try {
+            $this->invalidateStockLocationHal($stockLocationHal);
+        } catch (Exception $exception) {
+            // No-op. Save succeeded, everything else is superfluous
+        }
+        return $stockLocationHal;
     }
 
     public function getData()
@@ -28,5 +64,55 @@ class Collection
             $this->getParams('locationId') ?: []
         );
     }
+
+    /**
+     * @return self
+     */
+    protected function setStockLocationMapper(StockLocationMapper $stockLocationMapper)
+    {
+        $this->stockLocationMapper = $stockLocationMapper;
+        return $this;
+    }
+
+    /**
+     * @return StockLocationMapper
+     */
+    protected function getStockLocationMapper()
+    {
+        return $this->stockLocationMapper;
+    }
+
+    /**
+     * @return self
+     */
+    protected function setStockService(StockService $stockService)
+    {
+        $this->stockService = $stockService;
+        return $this;
+    }
+
+    /**
+     * @return StockService
+     */
+    protected function getStockService()
+    {
+        return $this->stockService;
+    }
+
+    /**
+     * @return self
+     */
+    protected function setInvalidator(Invalidator $invalidator)
+    {
+        $this->invalidator = $invalidator;
+        return $this;
+    }
+
+    /**
+     * @return Invalidator
+     */
+    protected function getInvalidator()
+    {
+        return $this->invalidator;
+    }
 }
- 
