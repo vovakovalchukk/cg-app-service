cbc6146d8de385d5a39bdab626630a86f0942890
Aaron Baker <aaronb@channelgrabber.com>
2015-11-06 10:21:59 +0000

    CGIV-6477: moved CG\Stock\Location\Storage\Db & Cache from cg-stock as they're only used here

---
+++ library/CG/Stock/Location/Storage/Db.php
@@ -0,0 +1,152 @@
+<?php
+namespace CG\Stock\Location\Storage;
+
+use CG\Stock\Location\Collection as LocationCollection;
+use CG\Stock\Location\Entity as LocationEntity;
+use CG\Stock\Location\StorageInterface;
+use CG\Stdlib\CollectionInterface;
+use CG\Stdlib\Storage\Db\DbAbstract;
+use CG\Stdlib\Exception\Runtime\NotFound;
+use CG\Stdlib\Exception\Storage as StorageException;
+use Zend\Db\Sql\Exception\ExceptionInterface;
+
+class Db extends DbAbstract implements StorageInterface
+{
+    public function fetchCollectionByStockIds(array $stockIds)
+    {
+        try {
+            $select = $this->getSelect();
+            $query = [
+                'stockLocation.stockId' => $stockIds
+            ];
+            $select->where($query);
+
+            return $this->fetchCollection(
+                new LocationCollection($this->getEntityClass(), __FUNCTION__, compact('stockIds')),
+                $this->getReadSql(),
+                $select,
+                $this->getMapper()
+            );
+        } catch (ExceptionInterface $e) {
+            throw new StorageException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function fetchCollectionByPaginationAndFilters($limit, $page, array $stockId, array $locationId)
+    {
+        try {
+            $select = $this->getSelect();
+
+            $query = [];
+            if(!empty($stockId)) {
+                $query['stockLocation.stockId'] = $stockId;
+            }
+            if(!empty($locationId)) {
+                $query['stockLocation.locationId'] = $locationId;
+            }
+
+            $select->where($query);
+
+            if ($limit != 'all') {
+                $offset = ($page - 1) * $limit;
+                $select->limit($limit)
+                    ->offset($offset);
+            }
+
+            return $this->fetchPaginatedCollection(
+                new LocationCollection($this->getEntityClass(), __FUNCTION__, compact('limit', 'page', 'stockId', 'locationId')),
+                $this->getReadSql(),
+                $select,
+                $this->getMapper()
+            );
+        } catch (ExceptionInterface $e) {
+            throw new StorageException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+
+    public function remove($entity)
+    {
+        $delete = $this->getDelete()->where(array(
+            'locationId' => $entity->getLocationId(),
+            'stockId' => $entity->getStockId(),
+        ));
+        $this->getWriteSql()->prepareStatementForSqlObject($delete)->execute();
+    }
+
+    public function fetch($id)
+    {
+        return $this->fetchEntity(
+            $this->getReadSql(),
+            $this->getSelect()->where(LocationEntity::getStockAndLocationFromId($id)),
+            $this->getMapper()
+        );
+    }
+
+    protected function saveEntity($entity)
+    {
+        try {
+            $this->fetch($entity->getId());
+            $this->updateEntity($entity);
+        } catch (NotFound $ex) {
+            $this->insertEntity($entity);
+        }
+        return $entity;
+    }
+
+    protected function insertEntity($entity)
+    {
+        $insert = $this->getInsert()->values($this->toDbArray($entity));
+        $this->getWriteSql()->prepareStatementForSqlObject($insert)->execute();
+
+        $entity->setNewlyInserted(true);
+    }
+
+    protected function updateEntity($entity)
+    {
+        $update = $this->getUpdate()->set($this->toDbArray($entity))
+            ->where(array(
+                'locationId' => $entity->getLocationId(),
+                'stockId' => $entity->getStockId(),
+            ));
+        $this->getWriteSql()->prepareStatementForSqlObject($update)->execute();
+    }
+
+    public function saveCollection(CollectionInterface $collection)
+    {
+        foreach ($collection as $entity) {
+            $this->save($entity);
+        }
+    }
+
+    protected function toDbArray($entity)
+    {
+        $data = $entity->toArray();
+        unset($data['id']);
+        return $data;
+    }
+
+    protected function getSelect()
+    {
+        return $this->getReadSql()->select('stockLocation');
+    }
+
+    protected function getInsert()
+    {
+        return $this->getWriteSql()->insert('stockLocation');
+    }
+
+    protected function getUpdate()
+    {
+        return $this->getWriteSql()->update('stockLocation');
+    }
+
+    protected function getDelete()
+    {
+        return $this->getWriteSql()->delete('stockLocation');
+    }
+
+    public function getEntityClass()
+    {
+        return LocationEntity::class;
+    }
+}
