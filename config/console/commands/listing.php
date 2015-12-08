daaa34856899b0a6e2b17ad4318ca7d1a9ed5639
Aaron Baker <aaronb@channelgrabber.com>
2015-12-08 17:08:19 +0000

    CGIV-6595: Added a command to find any 'pending' Listings that have a non-pending sibling Listing (same product, same channel) and copies the correct status across to the former.

--- config/console/commands/listing.php
+++ config/console/commands/listing.php
@@ -1,13 +1,15 @@
 <?php
-use CG\Listing\Command\DeleteUnimportedListings as Command;
+use CG\Listing\Command\CorrectPendingListingsStatusFromSiblingListings as CorrectPendingListingsStatusFromSiblingListingsCommand;
+use CG\Listing\Command\DeleteUnimportedListings as DeleteUnimportedListingsCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 return [
     'listing:deleteListingsForChannel' => array(
         'command' => function (InputInterface $input) use ($di) {
             $channel = $input->getArgument('channel');
 
-            $command = $di->get(Command::class);
+            $command = $di->get(DeleteUnimportedListingsCommand::class);
             $command([$channel]);
         },
         'description' => 'Delete unimported listings for channel',
@@ -18,4 +20,20 @@ return [
         ],
         'options' => []
     ),
+    'listing:correctPendingListingsStatusFromSiblingListings' => [
+        'command' => function (InputInterface $input, OutputInterface $output) use ($di) {
+            $output->writeln('Starting correctPendingListingsStatusFromSiblingListings command');
+            $channel = $input->getArgument('channel');
+            $command = $di->get(CorrectPendingListingsStatusFromSiblingListingsCommand::class);
+            $affected = $command($channel);
+            $output->writeln('Done, ' . $affected . ' Listings were affected, see the logs for details');
+        },
+        'description' => 'Find pending listings for a given channel and, if possible, copy their correct status from a sibling Listing',
+        'arguments' => [
+            'channel' => [
+                'required' => true
+            ]
+        ],
+        'options' => []
+    ],
 ];
