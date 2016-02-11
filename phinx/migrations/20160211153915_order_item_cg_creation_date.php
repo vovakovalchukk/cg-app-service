7eea444b94b0b1505a697ea377b572b796fc5d8b
Richard Heelin <richardh@channelgrabber.com>
2016-02-11 16:17:53 +0000

    CGIV-6682: Added a phinx migration to add cg creation date to order items - uses percona online schema changes for speed

---
+++ phinx/migrations/20160211153915_order_item_cg_creation_date.php
@@ -0,0 +1,62 @@
+<?php
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * @property MysqlAdapter $adapter
+ */
+class OrderItemCgCreationDate extends AbstractMigration
+{
+    const TABLE = 'item';
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->onlineSchemaChange(static::TABLE, 'ADD COLUMN `cgCreationDate` DATETIME');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->onlineSchemaChange(static::TABLE, 'REMOVE COLUMN `cgCreationDate`');
+    }
+
+    protected function onlineSchemaChange($table, $alter)
+    {
+        $options = $this->adapter->getOptions();
+
+        $dsn = [
+            'h=' . addcslashes($options['host'], ','),
+            'u=' . addcslashes($options['user'], ','),
+            'p=' . addcslashes($options['pass'], ','),
+            'D=' . addcslashes($options['name'], ','),
+            't=' . addcslashes($table, ','),
+        ];
+
+        if (isset($options['port'])) {
+            $dsn[] = 'P=' . addcslashes($options['port'], ',');
+        }
+        if (isset($options['unix_socket'])) {
+            $dns[] = 'S=' . addcslashes($options['unix_socket'], ',');
+        }
+        if (isset($options['charset'])) {
+            $dns[] = 'A=' . addcslashes($options['charset'], ',');
+        }
+
+        $command = 'pt-online-schema-change';
+        $arguments = [
+            '--execute',
+            '--alter ' . $alter,
+            implode(',', $dsn),
+        ];
+
+        passthru($command . implode(' ', array_map('escapeshellarg', $arguments)), $return);
+        if ($return !== 0) {
+            throw new RuntimeException('Failed to update ' . $table);
+        }
+    }
+}
