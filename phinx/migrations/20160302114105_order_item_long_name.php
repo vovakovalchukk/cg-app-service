b6ae46bca3de47c85cff21f63915a56fc72956a7
Richard Heelin <richardh@channelgrabber.com>
2016-03-02 11:46:47 +0000

    CGIV-6752: Allow item names to be as long as required

---
+++ phinx/migrations/20160302114105_order_item_long_name.php
@@ -0,0 +1,63 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class OrderItemLongName extends AbstractMigration
+{
+    const TABLE = 'item';
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->onlineSchemaChange(static::TABLE, 'MODIFY COLUMN `itemName` LONGTEXT NOT NULL');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->onlineSchemaChange(static::TABLE, 'MODIFY COLUMN `itemName` VARCHAR(255) NOT NULL');
+    }
+
+    protected function onlineSchemaChange($table, $alter)
+    {
+        $options = $this->adapter->getOptions();
+        $config = require dirname(dirname(__DIR__)) . '/config/storage.local.php';
+        if (!isset($config[$options['name']])) {
+            throw new InvalidArgumentException('Root access is not configured for database ' . $options['name']);
+        }
+
+        $dsn = [
+            'h=' . addcslashes($config[$options['name']]['hostname'], ','),
+            'u=' . addcslashes($config[$options['name']]['username'], ','),
+            'p=' . addcslashes($config[$options['name']]['password'], ','),
+            'D=' . addcslashes($config[$options['name']]['database'], ','),
+            't=' . addcslashes($table, ','),
+        ];
+
+        if (isset($config[$options['name']]['port'])) {
+            $dsn[] = 'P=' . addcslashes($config[$options['name']]['port'], ',');
+        }
+        if (isset($config[$options['name']]['unix_socket'])) {
+            $dns[] = 'S=' . addcslashes($config[$options['name']]['unix_socket'], ',');
+        }
+        if (isset($config[$options['name']]['charset'])) {
+            $dns[] = 'A=' . addcslashes($config[$options['name']]['charset'], ',');
+        }
+
+        $command = 'pt-online-schema-change';
+        $arguments = [
+            '--execute',
+            '--alter ' . escapeshellarg($alter),
+            '--alter-foreign-keys-method ' . escapeshellarg('auto'),
+            escapeshellarg(implode(',', $dsn)),
+        ];
+
+        passthru($command . ' ' . implode(' ', $arguments), $return);
+        if ($return !== 0) {
+            throw new RuntimeException('Failed to update ' . $table);
+        }
+    }
+}
