64c9a70c151f603c08f7502bbb7f6548ac46bf65
Richard Heelin <richardh@channelgrabber.com>
2016-05-20 15:09:32 +0100

    CGIV-7001: Added support for patching the product detail entities

--- controllers/CG/Controllers/ProductDetail/ProductDetail.php
+++ controllers/CG/Controllers/ProductDetail/ProductDetail.php
@@ -4,6 +4,7 @@ namespace CG\Controllers\ProductDetail;
 use CG\Product\Detail\RestService;
 use CG\Slim\Controller\Entity\DeleteTrait;
 use CG\Slim\Controller\Entity\GetTrait;
+use CG\Slim\Controller\Entity\PatchTrait;
 use CG\Slim\Controller\Entity\PutTrait;
 use CG\Slim\ControllerTrait;
 use Slim\Slim;
@@ -11,7 +12,11 @@ use Zend\Di\Di;
 
 class ProductDetail
 {
-    use ControllerTrait, GetTrait, PutTrait, DeleteTrait;
+    use ControllerTrait;
+    use GetTrait;
+    use PutTrait;
+    use DeleteTrait;
+    use PatchTrait;
 
     public function __construct(Slim $app, RestService $service, Di $di)
     {
--- library/CG/Product/Detail/RestService.php
+++ library/CG/Product/Detail/RestService.php
@@ -1,12 +1,24 @@
 <?php
 namespace CG\Product\Detail;
 
-use CG\Product\Detail\Service;
+use CG\Slim\Patch\ServiceTrait as PatchTrait;
+use Zend\EventManager\GlobalEventManager as EventManager;
 
 class RestService extends Service
 {
+    use PatchTrait;
+
     const DEFAULT_LIMIT = 10;
     const DEFAULT_PAGE = 1;
+
+    /** @var EventManager $eventManager */
+    protected $eventManager;
+
+    public function __construct(EventManager $eventManager, StorageInterface $repository, Mapper $mapper)
+    {
+        parent::__construct($repository, $mapper);
+        $this->setEventManager($eventManager);
+    }
 
     public function fetchCollectionByFilterAsHal(Filter $filter)
     {
@@ -21,5 +33,23 @@ class RestService extends Service
         return $this->getMapper()->collectionToHal(
             $collection, "/productDetail", $filter->getLimit(), $filter->getPage(), $filter->toArray()
         );
+    }
+
+    /**
+     * @return self
+     */
+    protected function setEventManager(EventManager $eventManager)
+    {
+        $this->eventManager = $eventManager;
+        return $this;
+    }
+
+    /**
+     * Required by PatchTrait
+     * @return EventManager
+     */
+    protected function getEventManager()
+    {
+        return $this->eventManager;
     }
 }
