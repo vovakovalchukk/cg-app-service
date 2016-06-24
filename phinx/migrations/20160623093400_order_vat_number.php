3fb21307bee6d476dc8f376ed626fc91a54086b4
Aaron Baker <aaronb@channelgrabber.com>
2016-06-24 16:29:13 +0100

    CGIV-6455: changed Phinx migration that adds a vatNumber column to the orderInvoice table to use the 'pt-online-schema-change' command so as not to lock the table.

--- phinx/migrations/20160623093400_order_vat_number.php
+++ phinx/migrations/20160623093400_order_vat_number.php
@@ -1,23 +1,61 @@
 <?php
+use Phinx\Db\Adapter\MysqlAdapter;
 use Phinx\Migration\AbstractMigration;
 
+/**
+ * @property MysqlAdapter $adapter
+ */
 class OrderVatNumber extends AbstractMigration
 {
+    const TABLE = 'orderInvoice';
+
     public function up()
     {
-        $this
-            ->table('orderInvoice')
-            ->addColumn('vatNumber', 'string', ['null' => true])
-            ->changeColumn('invoiceNumber', 'integer', ['null' => true])
-            ->update();
+        $this->onlineSchemaChange(static::TABLE, 'ADD COLUMN `vatNumber` varchar(255) NULL, CHANGE COLUMN `invoiceNumber` `invoiceNumber` int(11) NULL');
     }
 
     public function down()
     {
-        $this
-            ->table('orderInvoice')
-            ->removeColumn('vatNumber')
-            ->changeColumn('invoiceNumber', 'integer', ['null' => false])
-            ->update();
+        $this->onlineSchemaChange(static::TABLE, 'DROP COLUMN `vatNumber`, CHANGE COLUMN `invoiceNumber` `invoiceNumber` int(11) NOT NULL');
+    }
+
+    protected function onlineSchemaChange($table, $alter)
+    {
+        $options = $this->adapter->getOptions();
+        $config = require dirname(dirname(__DIR__)) . '/config/storage.local.php';
+        if (!isset($config[$options['name']])) {
+            throw new InvalidArgumentException('Root access is not configured for database ' . $options['name']);
+        }
+
+        $dsn = [
+            'h=' . addcslashes($config[$options['name']]['hostname'], ','),
+            'u=' . addcslashes($config[$options['name']]['username'], ','),
+            'p=' . addcslashes($config[$options['name']]['password'], ','),
+            'D=' . addcslashes($config[$options['name']]['database'], ','),
+            't=' . addcslashes($table, ','),
+        ];
+
+        if (isset($config[$options['name']]['port'])) {
+            $dsn[] = 'P=' . addcslashes($config[$options['name']]['port'], ',');
+        }
+        if (isset($config[$options['name']]['unix_socket'])) {
+            $dns[] = 'S=' . addcslashes($config[$options['name']]['unix_socket'], ',');
+        }
+        if (isset($config[$options['name']]['charset'])) {
+            $dns[] = 'A=' . addcslashes($config[$options['name']]['charset'], ',');
+        }
+
+        $command = 'pt-online-schema-change';
+        $arguments = [
+            '--execute',
+            '--alter ' . escapeshellarg($alter),
+            '--alter-foreign-keys-method ' . escapeshellarg('auto'),
+            escapeshellarg(implode(',', $dsn)),
+        ];
+
+        passthru($command . ' ' . implode(' ', $arguments), $return);
+        if ($return !== 0) {
+            throw new RuntimeException('Failed to update ' . $table);
+        }
     }
 }
