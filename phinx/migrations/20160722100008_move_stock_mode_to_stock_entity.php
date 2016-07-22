294753415042b10977e99328f324fc1c14fe1078
Richard Heelin <richardh@channelgrabber.com>
2016-07-22 11:48:36 +0100

    CGIV-7266: Added a phinx migration to move the stock mode off the product entity and onto the stock entity - will require cache to be flushed otherwise we will get some weird results

---
+++ phinx/migrations/20160722100008_move_stock_mode_to_stock_entity.php
@@ -0,0 +1,103 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class MoveStockModeToStockEntity extends AbstractMigration
+{
+    const TABLE_PRODUCT = 'product';
+    const TABLE_STOCK = 'stock';
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this
+            ->addColumns(static::TABLE_STOCK)
+            ->migrateData(static::TABLE_PRODUCT, static::TABLE_STOCK)
+            ->removeColumns(static::TABLE_PRODUCT);
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this
+            ->addColumns(static::TABLE_PRODUCT)
+            ->migrateData(static::TABLE_STOCK, static::TABLE_PRODUCT)
+            ->updateParentProduct()
+            ->removeColumns(static::TABLE_STOCK);
+    }
+
+    /**
+     * @return self
+     */
+    protected function addColumns($table)
+    {
+        $this
+            ->table($table)
+            ->addColumn('stockMode', 'string', ['length' => 10, 'null' => true])
+            ->addColumn('stockLevel', 'integer', ['null' => true])
+            ->addIndex(['stockMode'])
+            ->update();
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    protected function migrateData($fromTable, $toTable)
+    {
+        $update = <<<SQL
+UPDATE `%s` moveTo
+JOIN (
+    SELECT `organisationUnitId`, `sku`, `stockMode`, `stockLevel`
+    FROM `%s`
+    WHERE `stockMode` IS NOT NULL OR `stockLevel` IS NOT NULL
+    ORDER BY %s
+) moveFrom ON moveTo.`organisationUnitId` = moveFrom.`organisationUnitId` AND moveTo.`sku` = moveFrom.`sku`
+SET moveTo.`stockMode` = moveFrom.`stockMode`, moveTo.`stockLevel` = moveFrom.`stockLevel`;
+SQL;
+
+        $orderBy = '`organisationUnitId`, `sku`';
+        if ($fromTable == static::TABLE_PRODUCT) {
+            $orderBy .= ', `parentProductId` DESC';
+        }
+        $this->execute(sprintf($update, $toTable, $fromTable, $orderBy));
+
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    protected function updateParentProduct()
+    {
+        $update = <<<SQL
+UPDATE `product` parent
+JOIN (
+    SELECT `parentProductId`, `stockMode`, `stockLevel`
+    FROM `product`
+    WHERE `parentProductId` > 0 AND (`stockMode` IS NOT NULL OR `stockLevel` IS NOT NULL)
+) variation ON parent.`id` = variation.`parentProductId`
+SET parent.`stockMode` = variation.`stockMode`, parent.`stockLevel` = variation.`stockLevel`;
+SQL;
+
+        $this->execute($update);
+        return $this;
+    }
+
+    /**
+     * @return self
+     */
+    protected function removeColumns($table)
+    {
+        $this
+            ->table($table)
+            ->removeIndex(['stockMode'])
+            ->removeColumn('stockMode')
+            ->removeColumn('stockLevel')
+            ->update();
+        return $this;
+    }
+}
