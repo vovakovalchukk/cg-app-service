7f4457b04cfe925d1d6c28dbe51b4a43943f984c
Richard Heelin <richardh@channelgrabber.com>
2016-07-25 09:28:04 +0100

    CGIV-7266: Updated the versioniser to take into account if having variations as links rather than been embedded. This ensures full backwards compatibility with the previous version.

--- library/CG/Slim/Versioning/ProductEntity/Versioniser8.php
+++ library/CG/Slim/Versioning/ProductEntity/Versioniser8.php
@@ -1,6 +1,7 @@
 <?php
 namespace CG\Slim\Versioning\ProductEntity;
 
+use CG\Product\Service\Service as ProductService;
 use CG\Slim\Versioning\VersioniserInterface;
 use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\Stock\Entity as Stock;
@@ -9,12 +10,14 @@ use Nocarrier\Hal;
 
 class Versioniser8 implements VersioniserInterface
 {
+    /** @var ProductService $productService */
+    protected $productService;
     /** @var StockService $stockService */
     protected $stockService;
 
-    public function __construct(StockService $stockService)
+    public function __construct(ProductService $productService, StockService $stockService)
     {
-        $this->setStockService($stockService);
+        $this->setProductService($productService)->setStockService($stockService);
     }
 
     public function upgradeRequest(array $params, Hal $request)
@@ -29,22 +32,35 @@ class Versioniser8 implements VersioniserInterface
         $data = $response->getData();
         try {
             $sku = $data['sku'];
-
             $variation = $response->getFirstResource('variation');
+            $variationLink = $response->getFirstLink('variation');
+
             if ($variation) {
                 // Parent Product - Use first variation sku
                 $sku = $variation->getData()['sku'];
+            } else if ($variationLink) {
+                $productId = substr($variationLink, 9);
+                $sku = $this->productService->fetch($productId)->getSku();
             }
 
             /** @var Stock $stock */
             $stock = $this->stockService->fetchBySku($sku, $data['organisationUnitId']);
             $data['stockMode'] = $stock->getStockMode();
-            $data['stockLevel'] = ($variation ? null : $stock->getStockLevel());
+            $data['stockLevel'] = ($variation || $variationLink ? null : $stock->getStockLevel());
         } catch (NotFound $exception) {
             // No matching stock means no stock mode
             $data['stockMode'] = $data['stockLevel'] = null;
         }
         $response->setData($data);
+    }
+
+    /**
+     * @return self
+     */
+    protected function setProductService(ProductService $productService)
+    {
+        $this->productService = $productService;
+        return $this;
     }
 
     /**
