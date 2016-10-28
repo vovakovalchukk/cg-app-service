67dd6c30752f0f555b07569333076bbdc3a1e419
Aaron Baker <aaronb@channelgrabber.com>
2016-10-28 09:03:14 +0100

    CGIV-7617: updated Phinx migration to create new orderSeuence table to add a trigger on the orde r table to automatically add new entries to the former. Not currently working, getting permission errors. W.I.P.

--- phinx/migrations/20161027144200_order_sequence.php
+++ phinx/migrations/20161027144200_order_sequence.php
@@ -11,14 +11,19 @@ class OrderSequence extends AbstractMigration
             ->addIndex(['orderId'], ['unique' => true])
             ->create();
 
+        // Trigger an insert when we insert into the order table
+        $this->execute(
+            'CREATE TRIGGER orderInsert AFTER INSERT ON `order` FOR EACH ROW '
+            . 'INSERT INTO `orderSequence` (`orderId`) VALUES (new.id)'
+        );
+
         // Backfill
-        $sql = 'INSERT INTO `orderSequence` (`orderId`) '
-            . 'SELECT `id` FROM `order`';
-        $this->execute($sql);
+        $this->execute('INSERT INTO `orderSequence` (`orderId`) SELECT `id` FROM `order`');
     }
 
     public function down()
     {
+        $this->execute('DROP TRIGGER orderInsert');
         $this->table('orderSequence')->drop();
     }
 }
