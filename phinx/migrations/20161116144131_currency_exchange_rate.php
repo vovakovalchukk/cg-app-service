e22da1601d385247a472347f88a60b7452930b8e
Jack Monk <jack.monk@channelgrabber.com>
2016-11-16 15:13:40 +0000

    CGIV-7659 Removed the ID column creation as thats already taken care of

--- phinx/migrations/20161116144131_currency_exchange_rate.php
+++ phinx/migrations/20161116144131_currency_exchange_rate.php
@@ -6,7 +6,6 @@ class CurrencyExchangeRate extends AbstractMigration
     public function change()
     {
         $this->table('exchangeRate', ['collation' => 'utf8_general_ci'])
-            ->addColumn('id', 'integer', ['signed' => false])
             ->addColumn('datetime', 'datetime')
             ->addColumn('currencyCode', 'string')
             ->addColumn('baseCurrencyCode', 'string')
