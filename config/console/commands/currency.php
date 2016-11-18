aeb515082c52374ec9ca81eb5a65a904bafd032f
Jack Monk <jack.monk@channelgrabber.com>
2016-11-18 16:47:48 +0000

    CGIV-7659 Removed the date calculation as we will now check for the date in the fetch function and supply the latest date

--- config/console/commands/currency.php
+++ config/console/commands/currency.php
@@ -7,7 +7,6 @@ return [
     'currency:fetchExchangeRates' => [
         'command' => function () use ($di) {
             $command = $di->get(ExchangeRateService::class);
-            $endOfDay = date('Y-m-d H:i:s');
             $command->fetchAllExchangeRates($endOfDay);
         },
         'description' => "Fetch end-of-day exchange rates for all currencies from Open Exchange Rates",
