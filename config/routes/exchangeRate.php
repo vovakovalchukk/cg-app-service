6938f10bb99fb30dc3e66e44a3622f03eedfcf02
Jack Monk <jack.monk@channelgrabber.com>
2016-11-18 15:27:52 +0000

    CGIV-7659 Removed the route that took a datetime as a get param as this is not the correct usag eof the routes

--- config/routes/exchangeRate.php
+++ config/routes/exchangeRate.php
@@ -19,21 +19,5 @@ return [
             'filterRules' => ExchangeRateCollectionValidation::class,
             'dataRules' => ExchangeRateEntityValidation::class
         ]
-    ],
-    '/exchangeRate/:datetime' => [
-        'controllers' => function($datetime) use ($di, $app) {
-            $method = $app->request()->getMethod();
-            $controller = $di->get(ExchangeRateCollection::class);
-            $app->view()->set(
-                'RestResponse',
-                $controller->$method($datetime, $app->request()->getBody())
-            );
-        },
-        'via' => ['GET', 'POST', 'OPTIONS'],
-        'name' => 'ExchangeRateCollectionByDatetime',
-        'validation' => [
-            'filterRules' => ExchangeRateCollectionValidation::class,
-            'dataRules' => ExchangeRateEntityValidation::class
-        ]
     ]
 ];
