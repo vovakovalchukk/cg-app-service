192b2d2fc724777db42640071d3f0af3c5d60709
Richard Heelin <richardh@channelgrabber.com>
2016-12-15 15:48:49 +0000

    CGIV-7796: Updated services.live to php 7.1

--- tools/capistrano/config/deploy/live.rb
+++ tools/capistrano/config/deploy/live.rb
@@ -1,5 +1,5 @@
 set :stage, :live
 
 server 'www-data@82.145.48.2', :roles => [:app, :php71]
-server 'www-data@109.169.46.68', :roles => [:app, :php5]
+server 'www-data@109.169.46.68', :roles => [:app, :php71]
 server 'www-data@109.169.50.137', :roles => [:app, :php5]
