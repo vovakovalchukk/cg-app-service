ebf2312a5c2edd4498aecac5710f5cf3e83fd8ad
Richard Heelin <richardh@channelgrabber.com>
2017-01-24 10:27:47 +0000

    CGIV-7897: Added an index for OU so that we can improve the speed of lookups even further

--- phinx/migrations/20170123155233_order_tag_efficiency.php
+++ phinx/migrations/20170123155233_order_tag_efficiency.php
@@ -13,6 +13,7 @@ class OrderTagEfficiency extends AbstractMigration
             ->table('orderTags', ['id' => false, 'primary_key' => ['orderTag', 'organisationUnitId'], 'collation' => 'utf8_general_ci'])
             ->addColumn('orderTag', 'string', ['length' => 120])
             ->addColumn('organisationUnitId', 'integer', ['length' => 10, 'signed' => false])
+            ->addIndex(['organisationUnitId'])
             ->create();
 
         $this->getAdapter()->execute('INSERT INTO orderTags SELECT DISTINCT `orderTag`, `organisationUnitId` FROM orderTag');
