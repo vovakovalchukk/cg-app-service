4b5c1c285831d31fd5991bc35778a6eaad6b7578
Richard Heelin <richardh@channelgrabber.com>
2017-01-24 10:21:08 +0000

    CGIV-7897: Added a second table for order tags to allow us to be able to quickly get all tags for an OU without having to trawl through all orders that have that tag assigned.

---
+++ phinx/migrations/20170123155233_order_tag_efficiency.php
@@ -0,0 +1,28 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Table\ForeignKey;
+
+class OrderTagEfficiency extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this
+            ->table('orderTags', ['id' => false, 'primary_key' => ['orderTag', 'organisationUnitId'], 'collation' => 'utf8_general_ci'])
+            ->addColumn('orderTag', 'string', ['length' => 120])
+            ->addColumn('organisationUnitId', 'integer', ['length' => 10, 'signed' => false])
+            ->create();
+
+        $this->getAdapter()->execute('INSERT INTO orderTags SELECT DISTINCT `orderTag`, `organisationUnitId` FROM orderTag');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->table('orderTags')->drop();
+    }
+}
