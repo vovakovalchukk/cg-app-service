f3ec3b94786bca4b47428d513414bcd5ebee155c
Aaron Baker <aaronb@channelgrabber.com>
2017-01-26 09:09:50 +0000

    CGIV-7895: fixed Invoice Settings Versioniser3 to add / remove *all* of the fields added in that version

--- library/CG/Slim/Versioning/InvoiceSettingsEntity/Versioniser3.php
+++ library/CG/Slim/Versioning/InvoiceSettingsEntity/Versioniser3.php
@@ -22,7 +22,9 @@ class Versioniser3 implements VersioniserInterface
     public function upgradeRequest(array $params, Hal $request)
     {
         $data = $request->getData();
-        if (!isset($data['id']) || isset($data['emailSendAs'])) {
+        if (!isset($data['id']) ||
+            isset($data['emailSendAs'], $data['autoEmailAllowed'], $data['emailVerified'], $data['emailVerificationStatus'], $data['emailBcc'], $data['copyRequired'])
+        ) {
             return;
         }
 
@@ -32,16 +34,55 @@ class Versioniser3 implements VersioniserInterface
              */
             $invoiceSetting = $this->invoiceSettingsService->fetch($data['id']);
             $data['emailSendAs'] = $invoiceSetting->getEmailSendAs();
+            $data['autoEmailAllowed'] = $invoiceSetting->isAutoEmailAllowed();
+            $data['emailVerified'] = $invoiceSetting->isEmailVerified();
+            $data['emailVerificationStatus'] = $invoiceSetting->getEmailVerificationStatus();
+            $data['emailBcc'] = $invoiceSetting->getEmailBcc();
+            $data['copyRequired'] = $invoiceSetting->isCopyRequired();
+            if (isset($data['tradingCompanies']) && !empty($data['tradingCompanies'])) {
+                $this->upgradeTradingCompanies($data);
+            }
+
             $request->setData($data);
         } catch (NotFound $exception) {
             // New setting so there won't be a previously set email
         }
     }
 
+    protected function upgradeTradingCompanies(array &$data, InvoiceSetting $invoiceSetting)
+    {
+        $currentTradingCompanies = $invoiceSetting->getTradingCompanies();
+        $newTradingCompanies = $data['tradingCompanies'];
+        $data['tradingCompanies'] = [];
+        foreach ($newTradingCompanies as $id => $assignedInvoice) {
+            if (is_array($assignedInvoice)) {
+                $data['tradingCompanies'][$id] = $assignedInvoice;
+
+            } elseif (isset($currentTradingCompanies[$id])) {
+                $data['tradingCompanies'][$id] = $currentTradingCompanies[$id];
+                $data['tradingCompanies'][$id]['assignedInvoice'] = $assignedInvoice;
+
+            } else {
+                $data['tradingCompanies'][$id] = [
+                    'id' => $id,
+                    'assignedInvoice' => $assignedInvoice,
+                    'emailSendAs' => null,
+                    'emailVerified' => false,
+                    'emailVerificationStatus' => null,
+                ];
+            }
+        }
+    }
+
     public function downgradeResponse(array $params, Hal $response, $requestedVersion)
     {
         $data = $response->getData();
-        unset($data['emailSendAs']);
+        unset($data['emailSendAs'], $data['autoEmailAllowed'], $data['emailVerified'], $data['emailVerificationStatus'], $data['emailBcc'], $data['copyRequired']);
+        $tradingCompanies = $data['tradingCompanies'];
+        $data['tradingCompanies'] = [];
+        foreach ($tradingCompanies as $id => $settings) {
+            $data['tradingCompanies'][$id] = $settings['assignedInvoice'];
+        }
         $response->setData($data);
     }
 
