0c274ad68a29996607ae33aa7ca0335d3dc1002d
Jack Monk <jack.monk@channelgrabber.com>
2017-03-03 09:11:30 +0000

    HOTFIX: Updated the sub route of /orderItem/:orderItemId/images which needs to be kept in sync with /orderItem/:orderItemId when updating - added comment to notify future updates

--- config/routes/routes.php
+++ config/routes/routes.php
@@ -356,7 +356,7 @@ return array(
         'via' => array('GET', 'PUT', 'OPTIONS', 'DELETE', 'PATCH'),
         'name' => 'OrderItemEntity',
         'validation' => array("dataRules" => ItemEntityValidationRules::class, "filterRules" => null, "flatten" => false),
-        'version' => new Version(1, 10),
+        'version' => new Version(1, 10),//Updating this requires an update to /orderItem/:orderItemId/images route also (Line 380)
         'eTag' => [
             'mapperClass' => ItemMapper::class,
             'entityClass' => ItemEntity::class,
@@ -377,7 +377,7 @@ return array(
         'via' => ['PUT', 'OPTIONS'],
         'name' => 'OrderItemImages',
         'validation' => ['dataRules' => ItemImagesValidationRules::class, 'filterRules' => null, 'flatten' => false],
-        'version' => new Version(1, 9),
+        'version' => new Version(1, 10),
         'eTag' => [
             'mapperClass' => ItemMapper::class,
             'entityClass' => ItemEntity::class,
