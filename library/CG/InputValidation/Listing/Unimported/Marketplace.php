520ed70ad4c1c068c9129beb3137bb5fdb1fc4b2
Jack Monk <jack.monk@channelgrabber.com>
2017-04-03 16:06:41 +0100

    CGIV-7903 Added validators for the new fields on the UnimportListingMarketplace entity

--- library/CG/InputValidation/Listing/Unimported/Marketplace.php
+++ library/CG/InputValidation/Listing/Unimported/Marketplace.php
@@ -4,6 +4,7 @@ namespace CG\InputValidation\Listing\Unimported;
 use CG\Validation\Rules\ArrayOfIntegersValidator;
 use CG\Validation\Rules\IntegerValidator;
 use CG\Validation\RulesInterface;
+use Zend\Validator\StringLength;
 
 class Marketplace implements RulesInterface
 {
@@ -15,6 +16,18 @@ class Marketplace implements RulesInterface
                 'required'   => false,
                 'validators' => [
                     new ArrayOfIntegersValidator(new IntegerValidator(), 'organisationUnitId')
+                ]
+            ],
+            'channel' => [
+                'name'       => 'channel',
+                'required'   => false,
+                'validators' => [new StringLength(['min' => 1])]
+            ],
+            'accountId' => [
+                'name'       => 'accountId',
+                'required'   => false,
+                'validators' => [
+                    new ArrayOfIntegersValidator(new IntegerValidator(), 'accountId')
                 ]
             ],
         ];
