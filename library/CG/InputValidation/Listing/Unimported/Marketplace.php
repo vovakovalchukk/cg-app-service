f4426149616bc663fcfeabcdac20d847f375df8c
Richard Heelin <richardh@channelgrabber.com>
2017-04-19 13:16:18 +0100

    CGIV-7903: We don't support filtering by channel

--- library/CG/InputValidation/Listing/Unimported/Marketplace.php
+++ library/CG/InputValidation/Listing/Unimported/Marketplace.php
@@ -4,7 +4,6 @@ namespace CG\InputValidation\Listing\Unimported;
 use CG\Validation\Rules\ArrayOfIntegersValidator;
 use CG\Validation\Rules\IntegerValidator;
 use CG\Validation\RulesInterface;
-use Zend\Validator\StringLength;
 
 class Marketplace implements RulesInterface
 {
@@ -17,11 +16,6 @@ class Marketplace implements RulesInterface
                 'validators' => [
                     new ArrayOfIntegersValidator(new IntegerValidator(), 'organisationUnitId')
                 ]
-            ],
-            'channel' => [
-                'name'       => 'channel',
-                'required'   => false,
-                'validators' => [new StringLength(['min' => 1])]
             ],
             'accountId' => [
                 'name'       => 'accountId',
