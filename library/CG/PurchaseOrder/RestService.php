de01cd8cf9c41ae01e58a13e23d58cec5ef9951b
Aaron Baker <aaronb@channelgrabber.com>
2017-05-17 16:44:00 +0100

    CGIV-8310: the PO RestService now embeds the Items on each Entity.

--- library/CG/PurchaseOrder/RestService.php
+++ library/CG/PurchaseOrder/RestService.php
@@ -1,6 +1,9 @@
 <?php
 namespace CG\PurchaseOrder;
 
+use CG\PurchaseOrder\Item\Filter as ItemFilter;
+use CG\PurchaseOrder\Item\Service as ItemService;
+use CG\Stdlib\Exception\Runtime\NotFound;
 use Zend\EventManager\GlobalEventManager as EventManager;
 
 class RestService extends Service
@@ -10,16 +13,22 @@ class RestService extends Service
 
     /** @var EventManager $eventManager */
     protected $eventManager;
+    /** @var ItemService */
+    protected $itemService;
 
-    public function __construct(EventManager $eventManager, StorageInterface $repository, Mapper $mapper)
-    {
+    public function __construct(
+        EventManager $eventManager,
+        StorageInterface $repository,
+        Mapper $mapper,
+        ItemService $itemService
+    ) {
         parent::__construct($repository, $mapper);
         $this->eventManager = $eventManager;
+        $this->itemService = $itemService;
     }
 
     public function fetchCollectionByFilterAsHal(Filter $filter)
     {
-
         if (!$filter->getLimit()) {
             $filter->setLimit(static::DEFAULT_LIMIT);
         }
@@ -28,9 +37,53 @@ class RestService extends Service
         }
 
         $collection = $this->fetchCollectionByFilter($filter);
+        $this->fetchAndAddEmbedsToCollection($collection);
         return $this->getMapper()->collectionToHal(
             $collection, "/purchaseOrder", $filter->getLimit(), $filter->getPage(), $filter->toArray()
         );
+    }
+
+    public function fetchAsHal($id)
+    {
+        $entity = $this->fetch($id);
+        $this->fetchAndAddEmbedsToEntity($entity);
+
+        return $this->getMapper()->toHal($entity);
+    }
+
+    protected function fetchAndAddEmbedsToCollection(Collection $collection)
+    {
+        $items = $this->fetchItemsByPurchaseOrderIds($collection->getIds());
+        if (!$items) {
+            return;
+        }
+        foreach ($collection as $purchaseOrder)
+        {
+            $itemsForPO = $items->getBy('purchaseOrderId', $purchaseOrder->getId());
+            $purchaseOrder->setItems($itemsForPO);
+        }
+    }
+
+    protected function fetchItemsByPurchaseOrderIds(array $ids)
+    {
+        $filter = (new ItemFilter())
+            ->setLimit('all')
+            ->setPage(1)
+            ->setPurchaseOrderId($ids);
+        try {
+            return $this->itemService->fetchCollectionByFilter($filter);
+        } catch (NotFound $e) {
+            return null;
+        }
+    }
+
+    protected function fetchAndAddEmbedsToEntity(Entity $entity)
+    {
+        $items = $this->fetchItemsByPurchaseOrderIds([$entity->getId()]);
+        if (!$items) {
+            return;
+        }
+        $entity->setItems($items);
     }
 
     /**
