80afe33ef06ac75cd3d8f43cabbe6b3218e072d5
Richard Heelin <richardh@channelgrabber.com>
2017-06-01 15:35:35 +0100

    CGIV-8255: Silly copy/paste error

--- library/CG/Stock/Location/Storage/LinkedReplacer.php
+++ library/CG/Stock/Location/Storage/LinkedReplacer.php
@@ -235,7 +235,7 @@ class LinkedReplacer implements StorageInterface, LoggerAwareInterface
         }
 
         return $this->productLinkStorage->fetchCollectionByFilter(
-            (new ProductLinkFilter(1, 1))->setOuIdProductSku($ouSkuMap)
+            (new ProductLinkFilter('all', 1))->setOuIdProductSku($ouSkuMap)
         );
     }
 
