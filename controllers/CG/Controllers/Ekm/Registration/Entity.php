9b961f6f624d0cfd0dfc88a75c2530db24cd755e
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-09-26 17:13:23 +0100

    CGIV-8906 - Fix CG\Controllers\Ekm\Registration\Entity

--- controllers/CG/Controllers/Ekm/Registration/Entity.php
+++ controllers/CG/Controllers/Ekm/Registration/Entity.php
@@ -1,61 +1,22 @@
 <?php
 namespace CG\Controllers\Ekm\Registration;
 
-use CG\Validation\Rules\IntegerValidator;
-use CG\Validation\RulesInterface;
-use Zend\Validator\Date;
-use Zend\Validator\StringLength;
+use CG\Ekm\Registration\Service;
+use CG\Slim\Controller\Entity\DeleteTrait;
+use CG\Slim\Controller\Entity\GetTrait;
+use CG\Slim\Controller\Entity\PutTrait;
+use CG\Slim\ControllerTrait;
+use Slim\Slim;
+use Zend\Di\Di;
 
-class Entity implements RulesInterface
+class Entity
 {
-    public function getRules()
+    use ControllerTrait, GetTrait, PutTrait, DeleteTrait;
+
+    public function __construct(Slim $app, Service $service, Di $di)
     {
-        return [
-            'id' => [
-                'name'       => 'id',
-                'required'   => false,
-                'validators' => []
-            ],
-            'ekmUsername' => [
-                'name' => 'ekmUsername',
-                'required' => true,
-                'validators' => [new StringLength(['min' => 1])]
-            ],
-            'json' => [
-                'name' => 'json',
-                'required' => true,
-                'validators' => [new StringLength(['min' => 1])]
-            ],
-            'referrer' => [
-                'name' => 'referrer',
-                'required' => true,
-                'validators' => [new StringLength(['min' => 1])]
-            ],
-            'application' => [
-                'name' => 'application',
-                'required' => true,
-                'validators' => [new StringLength(['min' => 1])]
-            ],
-            'token' => [
-                'name' => 'token',
-                'required' => false,
-                'validators' => [new StringLength(['min' => 1])]
-            ],
-            'organisationUnitId' => [
-                'name'       => 'organisationUnitId',
-                'required'   => false,
-                'validators' => [new IntegerValidator(['name' => 'organisationUnitId'])]
-            ],
-            'createdDate' => [
-                'name' => 'createdDate',
-                'required' => true,
-                'validators' => [new Date(['format' => 'Y-m-d H:i:s'])]
-            ],
-            'completedDate' => [
-                'name' => 'completedDate',
-                'required' => false,
-                'validators' => [new Date(['format' => 'Y-m-d H:i:s'])]
-            ]
-        ];
+        $this->setSlim($app)
+            ->setService($service)
+            ->setDi($di);
     }
 }
