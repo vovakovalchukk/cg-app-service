3341ffac88965c3f41265132b718d99ed904b4d8
Richard Heelin <richardh@channelgrabber.com>
2017-09-28 14:02:24 +0100

    CGIV-8914: Call the adhoc function from the migration so that we start to update orders asap

--- phinx/migrations/20170928093952_order_exchange_rates.php
+++ phinx/migrations/20170928093952_order_exchange_rates.php
@@ -3,6 +3,9 @@ use Phinx\Migration\AbstractOnlineSchemaChange;
 
 class OrderExchangeRates extends AbstractOnlineSchemaChange
 {
+    const CILEX_LOCATION = '/../../console/app.php';
+    const CILEX_CMD = 'ad-hoc:updateOrderExchangeRates --quiet';
+
     /**
      * Migrate Up.
      */
@@ -13,6 +16,11 @@ class OrderExchangeRates extends AbstractOnlineSchemaChange
                 $table,
                 'ADD COLUMN `exchangeRate` DECIMAL(12, 4), ADD COLUMN `exchangeRateCurrencyCode` VARCHAR(255)'
             );
+        }
+
+        passthru('php ' . __DIR__ . static::CILEX_LOCATION . ' ' . static::CILEX_CMD, $exitCode);
+        if ($exitCode !== 0) {
+            throw new \RuntimeException('Failed to generate jobs to update orders exchange rates');
         }
     }
 
