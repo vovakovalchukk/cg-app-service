2ce504bb101b1554ffe3ad082fdab848ca1c392d
Eric Mugerwa <eric.mugerwa@channelgrabber.com>
2017-09-29 15:52:09 +0100

    CGIV-8903 - Use registration service to generate job

--- library/CG/Ekm/Registration/Service/Service.php
+++ library/CG/Ekm/Registration/Service/Service.php
@@ -1,7 +1,6 @@
 <?php
 namespace CG\Ekm\Registration\Service;
 
-use CG\Ekm\Gearman\Generator\Registration\Action\Register as RegisterActionGenerator;
 use CG\Ekm\Registration\Mapper;
 use CG\Ekm\Registration\Service as BaseService;
 use CG\Ekm\Registration\StorageInterface;
@@ -9,30 +8,18 @@ use CG\Stdlib\Sites;
 
 class Service extends BaseService
 {
-
-    /** @var  RegisterActionGenerator $registerActionGenerator */
-    protected $registerActionGenerator;
-
     public function __construct(
         StorageInterface $repository,
         Mapper $mapper,
-        Sites $sites,
-        RegisterActionGenerator $registerActionGenerator
+        Sites $sites
     ) {
         parent::__construct($repository, $mapper, $sites);
-        $this->registerActionGenerator = $registerActionGenerator;
     }
 
     public function save($entity)
     {
         $entity = parent::save($entity);
-        $this->createRegistrationJob($entity->getEkmUsername(), $entity->getToken());
+        $this->createEkmRegistrationGearmanJob($entity->getEkmUsername(), $entity->getToken());
         return $entity;
-    }
-
-    protected function createRegistrationJob(string $ekmUsername, string $token): void
-    {
-        $this->registerActionGenerator->__invoke($ekmUsername, $token);
-        return;
     }
 }
