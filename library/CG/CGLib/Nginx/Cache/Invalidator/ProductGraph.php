eda15f9be31011f305e09b4070a4d5b9ae90f491
Richard Heelin <richardh@channelgrabber.com>
2017-11-01 09:02:47 +0000

    CGIV-8959: Ensure that we invalidate product links as well as graphs as their expanded stock map will need updating

--- library/CG/CGLib/Nginx/Cache/Invalidator/ProductGraph.php
+++ library/CG/CGLib/Nginx/Cache/Invalidator/ProductGraph.php
@@ -6,13 +6,17 @@ use CG\CGLib\Nginx\Cache\AbstractInvalidator;
 class ProductGraph extends AbstractInvalidator
 {
     const TYPE_GRAPH = 'graph';
+    const TYPE_LINK = 'link';
+
     protected $uriTypeMap = [
         self::TYPE_GRAPH => '/productGraph',
+        self::TYPE_LINK => '/productLink',
     ];
 
     public function invalidate($id)
     {
         $this->purgeResource(static::TYPE_GRAPH, $id);
+        $this->purgeResource(static::TYPE_LINK, $id);
     }
 
     protected function getUriForType($type)
