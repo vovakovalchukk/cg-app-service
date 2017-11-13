b1e5643c6b80cd5f009f8ccf08c0d9523c637b7a
Richard Heelin <richardh@channelgrabber.com>
2017-11-13 10:29:09 +0000

    CGIV-8981: Renamed variable to make it clearer that it is storing

--- library/CG/Stock/Location/Storage/LinkedReplacer.php
+++ library/CG/Stock/Location/Storage/LinkedReplacer.php
@@ -318,7 +318,7 @@ class LinkedReplacer implements StorageInterface, LoggerAwareInterface
         array $linkedStockLocationsByLocationOuAndSku,
         array $stockLocationsIds
     ): LinkedLocation {
-        $productLinkLeafedStockLocations = $this->buildProductLinkLeafedStockLocations(
+        $productLinkLeafStockLocations = $this->getProductLinkLeafStockLocations(
             $stock,
             $stockLocation,
             $productLinkLeaf,
@@ -327,7 +327,7 @@ class LinkedReplacer implements StorageInterface, LoggerAwareInterface
         );
 
         $productQuantifiedStockLocations = $this->getQuantifiedStockLocations(
-            $productLinkLeafedStockLocations,
+            $productLinkLeafStockLocations,
             $productLinkLeaf->getStockSkuMap(),
             $stockLocationsIds
         );
@@ -361,40 +361,38 @@ class LinkedReplacer implements StorageInterface, LoggerAwareInterface
     /**
      * @return Collection
      */
-    protected function buildProductLinkLeafedStockLocations(
+    protected function getProductLinkLeafStockLocations(
         Stock $stock,
         StockLocation $stockLocation,
         ProductLinkLeaf $productLinkLeaf,
         array $linkedStockLocationsByLocationOuAndSku,
         array &$stockLocationsIds
     ): Collection {
-        $productLinkLeafedStockLocations = new Collection(StockLocation::class, __FUNCTION__, ['id' => [$stockLocation->getId()]]);
-
+        $productLinkLeafStockLocations = new Collection(StockLocation::class, __FUNCTION__, ['id' => [$stockLocation->getId()]]);
         foreach ($productLinkLeaf->getStockSkuMap() as $sku => $qty) {
-            $productLinkLeafedLocationKey = $this->generateStockLocationKey($stock, $stockLocation, $sku);
-            if (!isset($linkedStockLocationsByLocationOuAndSku[$productLinkLeafedLocationKey])) {
+            $productLinkLeafStockLocationKey = $this->generateStockLocationKey($stock, $stockLocation, $sku);
+            if (!isset($linkedStockLocationsByLocationOuAndSku[$productLinkLeafStockLocationKey])) {
                 continue;
             }
 
-            $productLinkLeafedStockLocation = $linkedStockLocationsByLocationOuAndSku[$productLinkLeafedLocationKey];
+            $productLinkLeafStockLocation = $linkedStockLocationsByLocationOuAndSku[$productLinkLeafStockLocationKey];
             try {
-                if (isset($stockLocationsIds[$productLinkLeafedStockLocation->getId()])) {
+                if (isset($stockLocationsIds[$productLinkLeafStockLocation->getId()])) {
                     throw new RecursionException(
                         sprintf(
                             'Already fetched stock location with id %s for linked stock location %d',
-                            $productLinkLeafedStockLocation->getId(),
+                            $productLinkLeafStockLocation->getId(),
                             $stockLocation->getId()
                         )
                     );
                 }
-                $stockLocationsIds[$productLinkLeafedStockLocation->getId()] = $sku;
-                $productLinkLeafedStockLocations->attach($productLinkLeafedStockLocation);
+                $stockLocationsIds[$productLinkLeafStockLocation->getId()] = $sku;
+                $productLinkLeafStockLocations->attach($productLinkLeafStockLocation);
             } catch (RecursionException $exception) {
-                $this->logCriticalException($exception, static::LOG_MSG_RECURSIVE_FETCH, ['id' => $productLinkLeafedStockLocation->getId(), 'sku' => $productLinkLeafedStockLocation->getSku()], static::LOG_CODE_RECURSIVE_FETCH, ['ou' => $productLinkLeafedStockLocation->getOrganisationUnitId()]);
+                $this->logCriticalException($exception, static::LOG_MSG_RECURSIVE_FETCH, ['id' => $productLinkLeafStockLocation->getId(), 'sku' => $productLinkLeafStockLocation->getSku()], static::LOG_CODE_RECURSIVE_FETCH, ['ou' => $productLinkLeafStockLocation->getOrganisationUnitId()]);
             }
         }
-
-        return $productLinkLeafedStockLocations;
+        return $productLinkLeafStockLocations;
     }
 
     /**
