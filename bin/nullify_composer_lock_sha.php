0c1d24b6d22f09b08dcf085f57b8e2f2a947efea
James Moorhouse <jamesm@channelgrabber.com>
2017-11-14 10:31:50 +0000

    CGIV-8271 new script to nullify lockfile shas to make the bitbucket pipeline work 60% of the time, every time

---
+++ bin/nullify_composer_lock_sha.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+$composerLockFilePath = dirname(__DIR__) . '/composer.lock';
+$composerLockFile = json_decode(file_get_contents($composerLockFilePath), true);
+
+foreach($composerLockFile['packages'] as $package) {
+    $package['dist']['shasum'] = null;
+}
+
+foreach($composerLockFile['packages-dev'] as $package) {
+    $package['dist']['shasum'] = null;
+}
+
+file_put_contents($composerLockFilePath, json_encode($composerLockFile, JSON_PRETTY_PRINT));
