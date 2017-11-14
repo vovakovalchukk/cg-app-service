9252c048c73bbf2876e3c4aac96b16ac8b92ee9f
James Moorhouse <jamesm@channelgrabber.com>
2017-11-14 10:55:27 +0000

    CGIV-8271 trying to update things by reference

--- bin/nullify_composer_lock_sha.php
+++ bin/nullify_composer_lock_sha.php
@@ -1,14 +1,15 @@
 #!/usr/bin/env php
+
 <?php
 
 $composerLockFilePath = dirname(__DIR__) . '/composer.lock';
 $composerLockFile = json_decode(file_get_contents($composerLockFilePath), true);
 
-foreach($composerLockFile['packages'] as $package) {
+foreach($composerLockFile['packages'] as &$package) {
     $package['dist']['shasum'] = null;
 }
 
-foreach($composerLockFile['packages-dev'] as $package) {
+foreach($composerLockFile['packages-dev'] as &$package) {
     $package['dist']['shasum'] = null;
 }
 
