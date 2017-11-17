97d068bbd02353a883d719065ff720cc9cd8801e
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2017-11-17 12:02:11 +0000

    CGIV-9008: Imported storage adapters from cg-template library

---
+++ library/CG/Template/Storage/Cache.php
@@ -0,0 +1,42 @@
+<?php
+namespace CG\Template\Storage;
+
+use CG\Cache\CacheAbstract;
+use CG\Cache\Storage\CollectionTrait;
+use CG\Cache\Storage\FetchTrait;
+use CG\Cache\Storage\RemoveByFieldTrait;
+use CG\Cache\Storage\RemoveTrait;
+use CG\Cache\Storage\SaveTrait;
+use CG\Cache\Strategy\CollectionInterface as CollectionStrategy;
+use CG\Cache\Strategy\EntityInterface as EntityStrategy;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
+use CG\Template\Collection;
+use CG\Template\Mapper;
+use CG\Template\StorageInterface;
+
+class Cache extends CacheAbstract implements StorageInterface, LoggerAwareInterface
+{
+    use CollectionTrait;
+    use SaveTrait;
+    use RemoveTrait;
+    use RemoveByFieldTrait;
+    use FetchTrait;
+    use LogTrait;
+
+    public function __construct(Mapper $mapper, EntityStrategy $entityStrategy, CollectionStrategy $collectionStrategy)
+    {
+        parent::__construct($mapper, $entityStrategy, $collectionStrategy);
+    }
+
+    public function fetchCollectionByPagination($limit, $page, array $id, array $organisationUnitId, array $type)
+    {
+        $collection = new Collection($this->getEntityClass(), __FUNCTION__, compact('limit', 'page', 'id', 'organisationUnitId', 'type'));
+        return $this->fetchCollection($collection);
+    }
+
+    public function getCollectionClass()
+    {
+        return Collection::class;
+    }
+}
