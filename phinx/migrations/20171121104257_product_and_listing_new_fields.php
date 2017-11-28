5f335b99c0168b5cc1a2b8bf73586a910f8acbe9
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2017-11-28 15:04:39 +0000

    CGIV-9018: update the new product details adn listing fields and use an online schema change for migration

--- phinx/migrations/20171121104257_product_and_listing_new_fields.php
+++ phinx/migrations/20171121104257_product_and_listing_new_fields.php
@@ -1,24 +1,51 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
+use Phinx\Migration\AbstractOnlineSchemaChange;
 
-class ProductAndListingNewFields extends AbstractMigration
+class ProductAndListingNewFields extends AbstractOnlineSchemaChange
 {
-    public function change()
-    {
-        $this->table('product')
-            ->addColumn('description', 'string', ['length' => 4096, 'null' => true, 'after' => 'name'])
-            ->addColumn('price', 'decimal', ['precision' => 10, 'scale' => 4, 'null' => true, 'after' => 'description'])
-            ->addColumn('cost', 'decimal', ['precision' => 10, 'scale' => 4, 'null' => true, 'after' => 'price'])
-            ->addColumn('condition', 'string', ['length' => 40, 'null' => true, 'after' => 'cost'])
-            ->update();
+    const TABLE_PRODUCT_DETAIL = 'productDetail';
+    const TABLE_LISTING = 'listing';
 
-        $this->table('listing')
-            ->addColumn('name', 'string', ['length' => 255, 'null' => true, 'after' => 'accountId'])
-            ->addColumn('description', 'string', ['length' => 4096, 'null' => true, 'after' => 'name'])
-            ->addColumn('price', 'decimal', ['precision' => 10, 'scale' => 4, 'null' => true, 'after' => 'description'])
-            ->addColumn('cost', 'decimal', ['precision' => 10, 'scale' => 4, 'null' => true, 'after' => 'price'])
-            ->addColumn('condition', 'string', ['length' => 40, 'null' => true, 'after' => 'cost'])
-            ->update();
+    public function up()
+    {
+        $productDetailAlter = 'ADD COLUMN `description` MEDIUMTEXT AFTER `sku`,'
+            . 'ADD COLUMN `ean` VARCHAR(13) AFTER `description`,'
+            . 'ADD COLUMN `brand` VARCHAR(100) AFTER `ean`,'
+            . 'ADD COLUMN `mpn` VARCHAR(40) AFTER `brand`,'
+            . 'ADD COLUMN `asin` VARCHAR(13) AFTER `mpn`,'
+            . 'ADD COLUMN `price` DECIMAL(10,4) AFTER `asin`,'
+            . 'ADD COLUMN `cost` DECIMAL(10,4) AFTER `price`,'
+            . 'ADD COLUMN `condition` VARCHAR(40) AFTER `cost`';
+
+        $listingAlter = 'ADD COLUMN `name` VARCHAR(255) AFTER `accountId`,'
+            . 'ADD COLUMN `description` MEDIUMTEXT AFTER `name`,'
+            . 'ADD COLUMN `price` DECIMAL(10,4) AFTER `description`,'
+            . 'ADD COLUMN `cost` DECIMAL(10,4) AFTER `price`,'
+            . 'ADD COLUMN `condition` VARCHAR(40) AFTER `cost`';
+
+        $this->onlineSchemaChange(static::TABLE_PRODUCT_DETAIL, $productDetailAlter);
+        $this->onlineSchemaChange(static::TABLE_LISTING, $listingAlter);
+    }
+
+    public function down()
+    {
+        $productDetailAlter = 'DROP COLUMN `description`,'
+            . 'DROP COLUMN `ean`,'
+            . 'DROP COLUMN `brand`,'
+            . 'DROP COLUMN `mpn`,'
+            . 'DROP COLUMN `asin`,'
+            . 'DROP COLUMN `price`,'
+            . 'DROP COLUMN `cost`,'
+            . 'DROP COLUMN `condition`';
+
+        $listingAlter = 'DROP COLUMN `name`,'
+            . 'DROP COLUMN `description`,'
+            . 'DROP COLUMN `price`,'
+            . 'DROP COLUMN `cost`,'
+            . 'DROP COLUMN `condition`';
+
+        $this->onlineSchemaChange(static::TABLE_PRODUCT_DETAIL, $productDetailAlter);
+        $this->onlineSchemaChange(static::TABLE_LISTING, $listingAlter);
     }
 }
