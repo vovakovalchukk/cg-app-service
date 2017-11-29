e7e442061ad27cd1063eea086b9b45cb63c841df
Richard Heelin <richardh@channelgrabber.com>
2017-11-29 09:33:05 +0000

    CGIV-9041: Ensure that what ever type of stock location we attempt to cache, it is always stored as the base entity class.

--- config/autoload/di.cgcache.global.php
+++ config/autoload/di.cgcache.global.php
@@ -1,6 +1,11 @@
 <?php
 use CG\Cache\InvalidationHandler;
+use CG\Cache\KeyGenerator\Redis as KeyGenerator;
 use CG\Order\Shared\Shipping\Method\Entity as ShippingMethod;
+use CG\Stock\Location\Entity as StockLocation;
+use CG\Stock\Location\LinkedLocation as LinkedStockLocation;
+use CG\Stock\Location\QuantifiedLocation as QuantifiedStockLocation;
+use CG\Stock\Location\TypedEntity as TypedStockLocation;
 
 return [
     'di' => [
@@ -10,6 +15,15 @@ return [
                     'validateCollectionChance' => [
                         InvalidationHandler::VALIDATE_COLLECTION_CHANCE_DEFAULT_KEY => 50,
                         ShippingMethod::class => 0,
+                    ],
+                ],
+            ],
+            KeyGenerator::class => [
+                'parameters' => [
+                    'classMap' => [
+                        TypedStockLocation::class => StockLocation::class,
+                        QuantifiedStockLocation::class => StockLocation::class,
+                        LinkedStockLocation::class => StockLocation::class,
                     ],
                 ],
             ],
