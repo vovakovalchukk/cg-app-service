fee6162b6f4786aa8aece9abc023ab6e7bae68be
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2017-11-30 10:09:57 +0000

    CGIV-9005: Allow for long data in preferences

--- phinx/migrations/20171117120600_create_user_preference_table.php
+++ phinx/migrations/20171117120600_create_user_preference_table.php
@@ -1,12 +1,13 @@
 <?php
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter as Adapter;
 
 class CreateUserPreferenceTable extends AbstractMigration
 {
     public function change()
     {
         $this->table('userPreference', ['collation' => 'utf8_general_ci'])
-            ->addColumn('preference', 'string')
+            ->addColumn('preference', 'string', ['limit' => Adapter::TEXT_LONG])
             ->addColumn('mongoId', 'string')
             ->addIndex(['mongoId'], ['unique' => true])
             ->create();
