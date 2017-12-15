69b194cba401a52bdcbdc739dbaeaf7582c4ac74
Richard Heelin <richardh@channelgrabber.com>
2017-12-15 14:32:32 +0000

    Revert "CGIV-9080: Removed services2 from capistrano live deployment config as decommissioned"
    This reverts commit 999453a8f4c595b4fa69c6f9b9da00721b9206f0.

--- tools/capistrano/config/deploy/live.rb
+++ tools/capistrano/config/deploy/live.rb
@@ -1,6 +1,7 @@
 set :stage, :live
 
 server 'www-data@82.145.48.2', :roles => [:app, :php71]
+server 'www-data@109.169.46.68', :roles => [:app, :php71]
 server 'www-data@94.130.132.23', :roles => [:app, :php71]
 server 'www-data@148.251.1.112', :roles => [:app, :php71]
 server 'www-data@94.130.165.40', :roles => [:app, :php71]
