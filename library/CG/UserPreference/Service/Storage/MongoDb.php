34932f38844a98be1bac28e62c1b75ff10efcdf1
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2018-01-31 14:27:20 +0000

    CGIV-9211: Prevent saving to mongodb to facilitate easier migration

--- library/CG/UserPreference/Service/Storage/MongoDb.php
+++ library/CG/UserPreference/Service/Storage/MongoDb.php
@@ -27,16 +27,7 @@ class MongoDb implements StorageInterface
 
     public function save($entity)
     {
-        try {
-            $userPreference = $this->getMapper()->toMongoArray($entity);
-            $save = $this->getMongoCollection()->save($userPreference);
-            if (!$save["updatedExisting"]) {
-                $entity->setNewlyInserted(true);
-            }
-            return $entity;
-        } catch(\MongoException $e) {
-            throw new StorageException($e->getMessage(), $e->getCode(), $e);
-        }
+        return;
     }
 
     public function remove($entity)
