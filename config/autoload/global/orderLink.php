b3f2ef1223dbf160ad36721c221ea726cfa5e1e4
Richard Heelin <richardh@channelgrabber.com>
2018-02-20 15:47:32 +0000

    TAC-12: Removed cache storage for order links as they have a very low hit ratio and/or request rate

--- config/autoload/global/orderLink.php
+++ config/autoload/global/orderLink.php
@@ -1,20 +1,12 @@
 <?php
 
-use CG\Order\Service\OrderLink\Storage\Cache as OrderLinkCacheStorage;
 use CG\Order\Service\OrderLink\Storage\Db as OrderLinkDbStorage;
 use CG\Order\Shared\OrderLink\Mapper as OrderLinkMapper;
-use CG\Order\Shared\OrderLink\Repository as OrderLinkRepository;
 use CG\Order\Shared\OrderLink\StorageInterface as OrderLinkStorage;
 
 return [
     'di' => [
         'instance' => [
-            OrderLinkRepository::class => [
-                'parameter' => [
-                    'storage' => OrderLinkCacheStorage::class,
-                    'repository' => OrderLinkDbStorage::class
-                ]
-            ],
             OrderLinkDbStorage::class => [
                 'parameter' => [
                     'readSql' => 'ReadSql',
@@ -24,7 +16,7 @@ return [
                 ]
             ],
             'preferences' => [
-                OrderLinkStorage::class => OrderLinkRepository::class,
+                OrderLinkStorage::class => OrderLinkDbStorage::class,
             ]
         ]
     ]
