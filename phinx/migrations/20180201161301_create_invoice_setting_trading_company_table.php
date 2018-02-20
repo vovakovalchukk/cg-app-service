ceff207490ed0024a6732f2ab34241dbe9e45350
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2018-02-20 14:50:03 +0000

    IN-39: Ensure all required table fields exist

--- phinx/migrations/20180201161301_create_invoice_setting_trading_company_table.php
+++ phinx/migrations/20180201161301_create_invoice_setting_trading_company_table.php
@@ -6,11 +6,13 @@ class CreateInvoiceSettingTradingCompanyTable extends AbstractMigration
 {
     public function change()
     {
-        $this->table('invoiceSettingTraduingCompany', ['collation' => 'utf8_general_ci'])
+        $this->table('invoiceSettingTradingCompany', ['collation' => 'utf8_general_ci'])
             ->addColumn('assignedInvoice', 'string', ['null' => true])
             ->addColumn('emailSendAs', 'string', ['null' => true])
             ->addColumn('emailVerified', 'boolean')
             ->addColumn('emailVerificationStatus', 'string', ['null' => true])
-            ->update();
+            ->addColumn('invoiceSettingId', 'integer', ['null' => false])
+            ->addColumn('mongoId', 'string', ['null' => true])
+            ->create();
     }
 }
