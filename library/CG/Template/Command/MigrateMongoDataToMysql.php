92e45d33d9b7bbe9fc1828a90b0f24f087ccc96a
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2018-02-21 10:34:34 +0000

    IN-40: Ensure invoice mappings have mysql IDs rather than mongo

--- library/CG/Template/Command/MigrateMongoDataToMysql.php
+++ library/CG/Template/Command/MigrateMongoDataToMysql.php
@@ -2,28 +2,35 @@
 
 namespace CG\Template\Command;
 
+use CG\Settings\InvoiceMapping\Repository as InvoiceMappingRepository;
+use CG\Settings\InvoiceMapping\Filter as InvoiceMappingFilter;
 use CG\Template\Storage\Db as MySQLStorage;
-use CG\Template\Storage\MongoDb as MongoDbStorage;
+use CG\Template\Repository as TemplateRepository;
 use CG\Stdlib\Exception\Runtime\NotFound as NotFoundException;
 
 class MigrateMongoDataToMysql
 {
     protected $db;
-    /** @var MongoDbStorage */
-    protected $mongoDb;
+    /** @var TemplateRepository */
+    protected $templateRepository;
+    /** @var InvoiceMappingRepository */
+    protected $invoiceMappingRepository;
 
     public function __construct(
         MySQLStorage $db,
-        MongoDbStorage $mongoDb
-    ) {
+        TemplateRepository $templateRepository,
+        InvoiceMappingRepository $invoiceMappingRepository
+    )
+    {
         $this->db = $db;
-        $this->mongoDb = $mongoDb;
+        $this->templateRepository = $templateRepository;
+        $this->invoiceMappingRepository = $invoiceMappingRepository;
     }
 
     public function __invoke()
     {
         $collection = $this->migrate();
-
+        $this->reindexInvoiceMappings();
         return count($collection);
     }
 
@@ -34,8 +41,8 @@ class MigrateMongoDataToMysql
         do {
             try {
 
-                $collection = $this->mongoDb
-                    ->fetchCollectionByPagination(100, $page, [], [], []);
+                $collection = $this->templateRepository
+                    ->fetchCollectionByPagination(50, $page, [], [], []);
                 array_merge($entityArray, $collection->toArray());
                 $page++;
             } catch (NotFoundException $unused) {
@@ -48,5 +55,33 @@ class MigrateMongoDataToMysql
         }
 
         return $entityArray;
+    }
+
+    protected function reindexInvoiceMappings()
+    {
+        $invoices = [];
+        $invoiceMappingFilter = new InvoiceMappingFilter();
+        $invoiceMappingFilter->setLimit(50);
+        $page = 1;
+        do {
+            $invoiceMappingFilter->setPage($page);
+            $page++;
+            try {
+                $invoiceMappings = $this->invoiceMappingRepository->fetchCollectionByFilter($invoiceMappingFilter);
+            } catch (NotFoundException $notFound) {
+                break;
+            }
+            /** @var \CG\Settings\InvoiceMapping\Entity $invoiceMapping */
+            foreach ($invoiceMappings as $invoiceMapping) {
+                if (isset($invoices[$invoiceMapping->getInvoiceId()])) {
+                    $invoice = $invoices[$invoiceMapping->getInvoiceId()];
+                } else {
+                    $invoice = $this->templateRepository->fetch($invoiceMapping->getInvoiceId());
+                    $invoices[$invoiceMapping->getInvoiceId()] = $invoice;
+                }
+                $invoiceMapping->setInvoiceId($invoice->getId());
+                $this->invoiceMappingRepository->save($invoiceMapping);
+            }
+        } while (true);
     }
 }
