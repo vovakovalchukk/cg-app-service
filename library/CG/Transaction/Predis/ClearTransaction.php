50b43d3bd5479e784940c19d213c4e2511ea9a8f
Richard Heelin <richardh@channelgrabber.com>
2018-02-28 08:21:57 +0000

    CGIV-9257: Helps if you get the logic right

--- library/CG/Transaction/Predis/ClearTransaction.php
+++ library/CG/Transaction/Predis/ClearTransaction.php
@@ -13,12 +13,11 @@ class ClearTransaction extends ScriptedCommand
     public function getScript()
     {
         return <<<SCRIPT
-if redis.call('EXISTS', KEYS[1]) then
+if !redis.call('EXISTS', KEYS[1]) then
     return redis.call('DEL', KEYS[2])
 else
     return 0
 end
 SCRIPT;
-
     }
 }
