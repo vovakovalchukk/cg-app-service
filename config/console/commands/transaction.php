532d722038ca015eec0029a1ea3a39bea59ee429
Richard Heelin <richardh@channelgrabber.com>
2018-02-28 08:56:38 +0000

    CGIV-9257: Switched the exception to a more fitting one

--- config/console/commands/transaction.php
+++ config/console/commands/transaction.php
@@ -1,6 +1,6 @@
 <?php
 use CG\Transaction\Command\Cleanup;
-use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zend\Di\Di;
@@ -13,7 +13,7 @@ return [
             $command = $di->newInstance(Cleanup::class);
             $chunkSize = $input->getArgument('chunkSize');
             if (!preg_match('/^[1-9][0-9]*$/', $chunkSize)) {
-                throw new RuntimeException('Argument "chunkSize" should be an positive integer');
+                throw new InvalidArgumentException('Argument "chunkSize" should be an positive integer');
             }
             $command($output, $chunkSize);
         },
