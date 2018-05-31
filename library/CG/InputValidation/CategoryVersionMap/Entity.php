ddf9de9e104c8fb75d5efe07df2dbfdc288c1973
Mark Duffield <mark.duffield@channelgrabber.com>
2018-05-31 13:33:47 +0100

    LIS-216: Adds input validation for CategoryVersionMap api

---
+++ library/CG/InputValidation/CategoryVersionMap/Entity.php
@@ -0,0 +1,24 @@
+<?php
+namespace CG\InputValidation\CategoryVersionMap;
+
+use CG\Validation\Rules\IntegerValidator;
+use CG\Validation\RulesInterface;
+use Zend\Validator\GreaterThan;
+
+class Entity implements RulesInterface
+{
+    public function getRules()
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'required' => false,
+                'validators' => [
+                    new IntegerValidator(['name' => 'id']),
+                    (new GreaterThan(['min' => 1, 'inclusive' => true]))
+                        ->setMessages(['notGreaterThanInclusive' => 'id must be at least %min%'])
+                ]
+            ],
+        ];
+    }
+}
