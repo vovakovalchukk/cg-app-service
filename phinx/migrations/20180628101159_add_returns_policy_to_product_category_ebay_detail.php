52e1d56398564efd72169abee4d0b952ad21c3e6
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-07-02 15:08:43 +0100

    TAC-127: Save the seller policies as string in the ebay category detail table

--- phinx/migrations/20180628101159_add_returns_policy_to_product_category_ebay_detail.php
+++ phinx/migrations/20180628101159_add_returns_policy_to_product_category_ebay_detail.php
@@ -1,13 +1,14 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class AddReturnsPolicyToProductCategoryEbayDetail extends AbstractMigration
 {
     public function change()
     {
         $this->table('productCategoryEbayDetail')
-            ->addColumn('returnPolicy', 'integer', ['null' => true])
+            ->addColumn('returnPolicy', 'string', ['limit' => MysqlAdapter::TEXT_TINY, 'null' => true])
             ->save();
     }
 }
--- phinx/migrations/20180629131124_add_shipping_and_payments_policy_to_product_category_ebay_detail.php
+++ phinx/migrations/20180629131124_add_shipping_and_payments_policy_to_product_category_ebay_detail.php
@@ -1,14 +1,15 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class AddShippingAndPaymentsPolicyToProductCategoryEbayDetail extends AbstractMigration
 {
     public function change()
     {
         $this->table('productCategoryEbayDetail')
-            ->addColumn('shippingPolicy', 'integer', ['null' => true])
-            ->addColumn('paymentPolicy', 'integer', ['null' => true])
+            ->addColumn('shippingPolicy', 'string', ['limit' => MysqlAdapter::TEXT_TINY, 'null' => true])
+            ->addColumn('paymentPolicy', 'string', ['limit' => MysqlAdapter::TEXT_TINY, 'null' => true])
             ->save();
     }
 }
