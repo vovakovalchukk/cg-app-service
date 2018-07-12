3920fc851f04c8e664b3b34c072c0d9e32a25a14
Micheal Wilkinson <micheal.wilkinson@channelgrabber.com>
2018-07-12 13:26:17 +0100

    IN-51: Remove MongoMigrationRepository references

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -491,7 +491,6 @@ $config = array(
                 'StockLocationApiService' => StockLocationService::class,
                 'ExchangeRateRepositoryPrimary' => ExchangeRateRepository::class,
                 'ExchangeRateRepositorySecondary' => ExchangeRateRepository::class,
-                'TemplateMongoMigrationRepository' => TemplateRepository::class,
                 'InvoiceSettingsMongoMigrationRepository' => InvoiceSettingsRepository::class,
                 'UserPreferenceMongoMigrationRepository' => UserPreferenceRepository::class,
                 'PersistentApiSettingsRepository' => ApiSettingsRepository::class,
@@ -926,16 +925,9 @@ $config = array(
             TemplateRepository::class => [
                 'parameter' => [
                     'storage' => TemplateCacheStorage::class,
-                    'repository' => 'TemplateMongoMigrationRepository',
+                    'repository' => TemplateDbStorage::class,
                 ],
             ],
-            'TemplateMongoMigrationRepository' => [
-                'parameter' => [
-                    'storage' => TemplateDbStorage::class,
-                    'repository' => TemplateMongoDbStorage::class,
-                ],
-            ],
-
             TemplateDbStorage::class => array(
                 'parameter' => array(
                     'readSql' => 'ReadSql',
