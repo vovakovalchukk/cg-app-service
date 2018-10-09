9f28ef8782a2f07551960466940c41e873c2e692
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-10-09 13:14:37 +0100

    CGIV-9871: Start tideways profiling using our new profiling library

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 use CG\Log\FatalErrorHandler;
 use Slim\Slim;
+use CG\Profiling\Profiler;
+
 define('PROJECT_ROOT', dirname(__DIR__));
 define('DS', DIRECTORY_SEPARATOR);
+
+Profiler::startProfiling('cg_app', [], [
+    Profiler::MODE_BASIC => 70,
+    Profiler::MODE_PROFILE => 29,
+    Profiler::MODE_TRACE => 1
+]);
 
 //Autoloading
 require_once dirname(__DIR__) . DS .'vendor' . DS .'autoload.php';
