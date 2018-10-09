987e90a528d609cf091c7bae768b032805535234
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2018-10-09 15:24:33 +0100

    CGIV-9871: Fix starting the profiling from bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,16 +1,11 @@
 <?php
+
 use CG\Log\FatalErrorHandler;
-use Slim\Slim;
 use CG\Profiling\Profiler;
+use Slim\Slim;
 
 define('PROJECT_ROOT', dirname(__DIR__));
 define('DS', DIRECTORY_SEPARATOR);
-
-Profiler::startProfiling(APPLICATION, [], [
-    Profiler::MODE_BASIC => 70,
-    Profiler::MODE_PROFILE => 29,
-    Profiler::MODE_TRACE => 1
-]);
 
 //Autoloading
 require_once dirname(__DIR__) . DS .'vendor' . DS .'autoload.php';
@@ -23,6 +18,12 @@ $components = array(
 foreach ($components as $component) {
     require_once __DIR__ . DS . $component . DS .'bootstrap.php';
 }
+
+Profiler::startProfiling('cg_app_dev', [], [
+    Profiler::MODE_BASIC => 70,
+    Profiler::MODE_PROFILE => 29,
+    Profiler::MODE_TRACE => 1
+]);
 
 $di = $serviceManager->get('Di');
 $app = $di->get(Slim::class);
