6f8fb2955ec34421bfe5dd75b4ce802d52c44347
Aaron Baker <aaron.baker@channelgrabber.com>
2018-10-24 09:55:25 +0100

    TAC-223: fix to InvoiceMapping Versioniser1 to correctly look up any existing data for the new emailSubject and emailTemplate lines if none is passed in

--- library/CG/Slim/Versioning/InvoiceMappingEntity/Versioniser1.php
+++ library/CG/Slim/Versioning/InvoiceMappingEntity/Versioniser1.php
@@ -1,17 +1,35 @@
 <?php
 namespace CG\Slim\Versioning\InvoiceMappingEntity;
 
+use CG\Settings\InvoiceMapping\Service;
 use CG\Slim\Versioning\VersioniserInterface;
 use Nocarrier\Hal;
 
 class Versioniser1 implements VersioniserInterface
 {
+    /** @var Service */
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
     public function upgradeRequest(array $params, Hal $request)
     {
         $data = $request->getData();
-        $data['emailSubject'] = $data['emailSubject'] ?? null;
-        $data['emailTemplate'] = $data['emailTemplate'] ?? null;
-        $request->setData($data);
+        if (!isset($data['id']) || isset($data['emailSubject'], $data['emailTemplate'])) {
+            return;
+        }
+
+        try {
+            $invoiceMapping = $this->service->fetch($data['id']);
+            $data['emailSubject'] = $data['emailSubject'] ?? $invoiceMapping->getEmailSubject();
+            $data['emailTemplate'] = $data['emailTemplate'] ?? $invoiceMapping->getEmailTemplate();
+            $request->setData($data);
+        } catch (NotFound $exception) {
+            // New mapping so there won't be a previously set email
+        }
     }
 
     public function downgradeResponse(array $params, Hal $response, $requestedVersion)
