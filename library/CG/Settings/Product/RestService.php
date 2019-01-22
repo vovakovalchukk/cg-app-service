3bad6b8d063329ae44650269dbafdd72f471a2a0
Aaron Baker <aaron.baker@channelgrabber.com>
2019-01-22 16:51:56 +0000

    TAC-326: added some logging when the Settings\Product::includePurchaseOrdersInAvailable flag has changed

--- library/CG/Settings/Product/RestService.php
+++ library/CG/Settings/Product/RestService.php
@@ -3,11 +3,18 @@ namespace CG\Settings\Product;
 
 use CG\Settings\Product\Entity;
 use CG\Settings\Product\Service;
+use CG\Stdlib\Log\LoggerAwareInterface;
+use CG\Stdlib\Log\LogTrait;
 use CG\Stock\Gearman\Workload\UpdateIncludePurchaseOrderFromDefault as UpdateIncludePurchaseOrderFromDefaultWorkload;
 use GearmanClient;
 
-class RestService extends Service
+class RestService extends Service implements LoggerAwareInterface
 {
+    use LogTrait;
+
+    const LOG_CODE = 'SettingsProductRestService';
+    const LOG_INCLUDE_PO_CHANGED = 'The includePurchaseOrdersInAvailable flag for OU %d has changed to %s, will create jobs to update Stock';
+
     /** @var GearmanClient */
     protected $gearmanClient;
 
@@ -33,6 +40,7 @@ class RestService extends Service
         if ($existingEntity->isIncludePurchaseOrdersInAvailable() == $newEntity->isIncludePurchaseOrdersInAvailable()) {
             return;
         }
+        $this->logDebug(static::LOG_INCLUDE_PO_CHANGED, ['ou' => $newEntity->getOrganisationUnitId(), ($newEntity->isIncludePurchaseOrdersInAvailable() ? 'true' : 'false')], [static::LOG_CODE, 'IncludePoChanged']);
         $workload = new UpdateIncludePurchaseOrderFromDefaultWorkload($newEntity->getOrganisationUnitId(), $newEntity->isIncludePurchaseOrdersInAvailable());
         $this->gearmanClient->doBackground(UpdateIncludePurchaseOrderFromDefaultWorkload::FUNCTION_NAME, serialize($workload));
     }
