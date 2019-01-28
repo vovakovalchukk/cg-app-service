216d5be3fa9f87ec1b11a81ba2e462ec2be36cd1
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2019-01-28 10:04:39 +0000

    TAC-325: Create a phinx migration that created the low stock threshold columns in both the product settings and stock tables

---
+++ phinx/migrations/20190125114404_add_low_stock_threshold.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Migration\AbstractOnlineSchemaChange;
+
+class AddLowStockThreshold extends AbstractOnlineSchemaChange
+{
+    public function up()
+    {
+        $alter = [
+            'ADD COLUMN `lowStockThresholdOn` TINYINT(1) NULL',
+            'ADD COLUMN `lowStockThresholdValue` INT(10) NULL'
+        ];
+
+        $this->onlineSchemaChange('productSettings', implode(', ', $alter), 200);
+        $this->onlineSchemaChange('stock', implode(', ', $alter), 200);
+    }
+
+    public function down()
+    {
+        $alter = [
+            'DROP COLUMN `lowStockThresholdOn` TINYINT(1) NULL',
+            'DROP COLUMN `lowStockThresholdValue` INT(10) NULL'
+        ];
+
+        $this->onlineSchemaChange('productSettings', implode(', ', $alter), 200);
+        $this->onlineSchemaChange('stock', implode(', ', $alter), 200);
+    }
+}
