9c004d7970d50f591e93d8ed1e0c4310ee8ed0c2
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2019-02-11 12:07:50 +0000

    TAC-325: Generate low stock default changed jobs when the default low stock threhsold changes

--- library/CG/Settings/Product/RestService.php
+++ library/CG/Settings/Product/RestService.php
@@ -1,10 +1,9 @@
 <?php
 namespace CG\Settings\Product;
 
-use CG\Settings\Product\Entity;
-use CG\Settings\Product\Service;
 use CG\Stdlib\Log\LoggerAwareInterface;
 use CG\Stdlib\Log\LogTrait;
+use CG\Stock\Gearman\Generator\UpdateLowStockThresholdFromDefault as UpdateLowStockThresholdFromDefaultGenerator;
 use CG\Stock\Gearman\Workload\UpdateIncludePurchaseOrderFromDefault as UpdateIncludePurchaseOrderFromDefaultWorkload;
 use GearmanClient;
 
@@ -14,14 +13,22 @@ class RestService extends Service implements LoggerAwareInterface
 
     const LOG_CODE = 'SettingsProductRestService';
     const LOG_INCLUDE_PO_CHANGED = 'The includePurchaseOrdersInAvailable flag for OU %d has changed to %s, will create jobs to update Stock';
+    const LOG_INCLUDE_LOW_STOCK_THRESHOLD_CHANGED = 'The low stock threshold flag for OU %d has changed to %s, will create jobs to update Stock';
 
     /** @var GearmanClient */
     protected $gearmanClient;
+    /** @var UpdateLowStockThresholdFromDefaultGenerator */
+    protected $updateLowStockThresholdFromDefaultGenerator;
 
-    public function __construct(StorageInterface $repository, Mapper $mapper, GearmanClient $gearmanClient)
-    {
+    public function __construct(
+        StorageInterface $repository,
+        Mapper $mapper,
+        GearmanClient $gearmanClient,
+        UpdateLowStockThresholdFromDefaultGenerator $updateLowStockThresholdFromDefaultGenerator
+    ) {
         parent::__construct($repository, $mapper);
         $this->gearmanClient = $gearmanClient;
+        $this->updateLowStockThresholdFromDefaultGenerator = $updateLowStockThresholdFromDefaultGenerator;
     }
 
     /**
@@ -32,6 +39,7 @@ class RestService extends Service implements LoggerAwareInterface
         $existingEntity = $this->fetch($entity->getId());
         $hal = parent::save($entity);
         $this->handleIncludePurchaseOrdersInAvailableChange($existingEntity, $entity);
+        $this->handleLowStockThresholdChange($existingEntity, $entity);
         return $hal;
     }
 
@@ -43,5 +51,14 @@ class RestService extends Service implements LoggerAwareInterface
         $this->logDebug(static::LOG_INCLUDE_PO_CHANGED, ['ou' => $newEntity->getOrganisationUnitId(), ($newEntity->isIncludePurchaseOrdersInAvailable() ? 'true' : 'false')], [static::LOG_CODE, 'IncludePoChanged']);
         $workload = new UpdateIncludePurchaseOrderFromDefaultWorkload($newEntity->getOrganisationUnitId(), $newEntity->isIncludePurchaseOrdersInAvailable());
         $this->gearmanClient->doBackground(UpdateIncludePurchaseOrderFromDefaultWorkload::FUNCTION_NAME, serialize($workload));
+    }
+
+    protected function handleLowStockThresholdChange(Entity $existingEntity, Entity $newEntity): void
+    {
+        if ($existingEntity->isLowStockThresholdOn() == $newEntity->isLowStockThresholdOn()) {
+            return;
+        }
+        $this->logDebug(static::LOG_INCLUDE_LOW_STOCK_THRESHOLD_CHANGED, ['ou' => $newEntity->getOrganisationUnitId(), ($newEntity->isLowStockThresholdOn() ? 'true' : 'false')], [static::LOG_CODE, 'LowStockThresholdChanged']);
+        $this->updateLowStockThresholdFromDefaultGenerator->generateJob($newEntity->getOrganisationUnitId());
     }
 }
