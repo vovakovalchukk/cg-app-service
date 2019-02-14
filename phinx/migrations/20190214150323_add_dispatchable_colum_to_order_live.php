cfc23ba3125cce67359bedb59ef41f5258d0c28a
markduffield <duffield.mark@gmail.com>
2019-02-14 15:05:29 +0000

    TAC-328: Breaks out migrations into separate files so we can run individually

--- phinx/migrations/20190214150323_add_dispatchable_colum_to_order_live.php
+++ phinx/migrations/20190214150323_add_dispatchable_colum_to_order_live.php
@@ -2,22 +2,18 @@
 
 use Phinx\Migration\AbstractOnlineSchemaChange;
 
-class AddDispatchableColumnToOrderAndOrderItem extends AbstractOnlineSchemaChange
+class AddDispatchableColumnToOrderLive extends AbstractOnlineSchemaChange
 {
 
     public function up()
     {
         $alter = 'ADD COLUMN dispatchable TINYINT(1) NOT NULL DEFAULT 0';
-        $this->onlineSchemaChange('order', $alter);
         $this->onlineSchemaChange('orderLive', $alter);
-        $this->onlineSchemaChange('item', $alter);
     }
 
     public function down()
     {
         $alter = 'DROP COLUMN dispatchable';
-        $this->onlineSchemaChange('order', $alter);
         $this->onlineSchemaChange('orderLive', $alter);
-        $this->onlineSchemaChange('item', $alter);
     }
 }
