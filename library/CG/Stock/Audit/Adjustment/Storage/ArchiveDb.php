58d510a7b20efd64b7119b0995200160e949468a
Richard Heelin <richardh@channelgrabber.com>
2019-07-02 15:42:02 +0100

    IN-393: Updated the audit adjustment archive db to use an `Archive` table

--- library/CG/Stock/Audit/Adjustment/Storage/ArchiveDb.php
+++ library/CG/Stock/Audit/Adjustment/Storage/ArchiveDb.php
@@ -6,11 +6,12 @@ use CG\Stock\Audit\Adjustment\MigrationTimer;
 
 class ArchiveDb extends Db
 {
+    const TABLE = parent::TABLE . 'Archive';
     const LOG_CODE = parent::LOG_CODE . '::Archive';
 
     public function saveCollection(CollectionInterface $collection, MigrationTimer $migrationTimer = null)
     {
-        $timer = $migrationTimer->getUploadTimer();
+        $timer = $migrationTimer ? $migrationTimer->getUploadTimer() : function() {};
         try {
             return parent::saveCollection($collection);
         } finally {
