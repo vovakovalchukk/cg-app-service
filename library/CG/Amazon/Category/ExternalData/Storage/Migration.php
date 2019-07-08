e3474923ce818f315e61488a2a3c3c3591354cc9
Dominik Gajewski <dominikg@channelgrabber.com>
2019-07-08 11:57:18 +0100

    CGIV-10736 Migration looks the same as repository class beside save mehod so it is necessary to override it

--- library/CG/Amazon/Category/ExternalData/Storage/Migration.php
+++ library/CG/Amazon/Category/ExternalData/Storage/Migration.php
@@ -2,23 +2,13 @@
 namespace CG\Amazon\Category\ExternalData\Storage;
 
 use CG\Amazon\Category\ExternalData\Data;
+use CG\Amazon\Category\ExternalData\Repository;
 use CG\Amazon\Category\ExternalData\StorageInterface;
 
-class Migration implements StorageInterface
+class Migration extends Repository implements StorageInterface
 {
-
-    public function fetch(int $categoryId): Data
-    {
-        // TODO: Implement fetch() method.
-    }
-
     public function save(int $categoryId, Data $data): void
     {
-        // TODO: Implement save() method.
-    }
-
-    public function remove(int $categoryId): void
-    {
-        // TODO: Implement remove() method.
+        $this->storage->save($categoryId, $data);
     }
 }
