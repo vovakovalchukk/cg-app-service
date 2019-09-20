e14018e5e60779fee486684372271045cc57cef8
Dominik Gajewski <dominikg@channelgrabber.com>
2019-09-20 13:13:47 +0100

    CGIV-10481 added phinx migration to let same categories but from different tree be saved in db

---
+++ phinx/migrations/20190920104941_category_index_update.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Migration\AbstractOnlineSchemaChange;
+
+class CategoryIndexUpdate extends AbstractOnlineSchemaChange
+{
+    private const TABLE = 'category';
+
+    public function up()
+    {
+        $alter = 'DROP INDEX `ExternalIdChannelMarketplaceVersion`, ADD CONSTRAINT `ExternalIdParentIdChannelMarketplaceVersion` UNIQUE KEY (`externalId`, `parentId`, `channel`, `marketplace`, `version`)';
+        $this->onlineSchemaChange(static::TABLE, $alter);
+    }
+
+    public function down()
+    {
+        $alter = 'DROP INDEX `ExternalIdParentIdChannelMarketplaceVersion`, ADD CONSTRAINT `ExternalIdChannelMarketplaceVersion` UNIQUE KEY (`externalId`, `channel`, `marketplace`, `version`)';
+        $this->onlineSchemaChange(static::TABLE, $alter);
+    }
+
+    protected function getAdditionalArguments(): array
+    {
+        return ['--nocheck-unique-key-change'];
+    }
+}
