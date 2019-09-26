bf6717b89b03c0f73091450110b364a84ddb4d28
Aaron Baker <aaron.baker@channelgrabber.com>
2019-09-26 09:37:38 +0100

    MIG-38: fix to /settings/invoice DB layer to correctly group the entity rows together before mapping.
    Because each entity can have multiple trading company settings we can get multiple rows per entity.
    Fetching of individual entities was handling this but not collections so we never got any trading company settings
    from collections.

--- library/CG/Settings/Invoice/Service/Storage/Db.php
+++ library/CG/Settings/Invoice/Service/Storage/Db.php
@@ -7,12 +7,14 @@ use CG\Settings\Invoice\Shared\Entity as InvoiceEntity;
 use CG\Settings\Invoice\Shared\Filter as InvoiceFilter;
 use CG\Settings\Invoice\Shared\StorageInterface;
 use CG\Stdlib\Coerce\Id\IntegerTrait as CoerceIntegerIdTrait;
+use CG\Stdlib\CollectionInterface;
 use CG\Stdlib\Exception\Runtime\NotFound;
 use CG\Stdlib\Exception\Storage as StorageException;
 use CG\Stdlib\Mapper\FromArrayInterface as ArrayMapper;
 use CG\Stdlib\PaginatedCollection as InvoiceCollection;
 use CG\Stdlib\Storage\Db\DbAbstract;
 use InvalidArgumentException;
+use Zend\Db\Adapter\Driver\ResultInterface;
 use Zend\Db\Sql\Exception\ExceptionInterface;
 use Zend\Db\Sql\Expression;
 use Zend\Db\Sql\Predicate\Predicate;
@@ -75,6 +77,34 @@ class Db extends DbAbstract implements StorageInterface
                 $exception
             );
         }
+    }
+
+    protected function fetchCollection(CollectionInterface $collection, ZendSql $sql, ZendSelect $select, ArrayMapper $arrayMapper, $expected = false)
+    {
+        $results = $sql->prepareStatementForSqlObject($select)->execute();
+        if ($results->count() == 0 || ($expected !== false && $results->count() != $expected)) {
+            throw new NotFound();
+        }
+
+        $entityData = $this->groupRowsByEntity($results);
+        foreach ($entityData as $rows) {
+            $collection->attach($arrayMapper->fromArray($rows));
+        }
+
+        return $collection;
+    }
+
+    protected function groupRowsByEntity(ResultInterface $results): array
+    {
+        $entityData = [];
+        foreach ($results as $row) {
+            $id = $row['id'];
+            if (!isset($entityData[$id])) {
+                $entityData[$id] = [];
+            }
+            $entityData[$id][] = $row;
+        }
+        return $entityData;
     }
 
     protected function saveEntity($entity)
