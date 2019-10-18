f26e12911a117b18cb4c796c198328139860f4d5
Dominik Gajewski <dominikg@channelgrabber.com>
2019-10-18 16:54:32 +0100

    CGIV-10996 added ebay config to create job in proper gearman and updated composer

--- config/autoload/global/ebay.php
+++ config/autoload/global/ebay.php
@@ -1,10 +1,16 @@
 <?php
 use CG\Ebay\ListingImport as EbayListingImport;
+use CG\Ebay\Gearman\Generator\RemoveItemRedisKey as RemoveItemRedisKeyGenerator;
 
 return [
     'di' => [
         'instance' => [
             EbayListingImport::class => [
+                'parameters' => [
+                    'gearmanClient' => 'ebayGearmanClient'
+                ]
+            ],
+            RemoveItemRedisKeyGenerator::class => [
                 'parameters' => [
                     'gearmanClient' => 'ebayGearmanClient'
                 ]
