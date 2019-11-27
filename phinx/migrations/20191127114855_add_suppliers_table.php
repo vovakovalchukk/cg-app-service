a363da9275cfcc24819b91b8b91508f19d6fb8b5
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2019-11-27 13:12:28 +0000

    TAC-562: Fixed the phinx migration for creating the supplier table and added DI config for the new supplier classes

--- phinx/migrations/20191127114855_add_suppliers_table.php
+++ phinx/migrations/20191127114855_add_suppliers_table.php
@@ -4,33 +4,12 @@ use Phinx\Migration\AbstractMigration;
 
 class AddSuppliersTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
     public function change()
     {
-        $this->table('suppliers')
-            ->addColumn('organisationUnitId', 'int')
+        $this->table('supplier')
+            ->addColumn('organisationUnitId', 'integer')
             ->addColumn('name', 'string')
             ->addIndex(['organisationUnitId', 'name'], ['unique' => true])
-            ->update();
+            ->create();
     }
 }
