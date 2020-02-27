1d35c6e44d3373048072f698340c2aa147073dad
Oliver Stephenson <oliver@channelgrabber.com>
2020-02-27 15:05:24 +0000

    CGIV-11280: moved clearstaletransaction lua script to app from predis lib, and increased verbosity in the script

---
+++ lua/clearstaletransaction.lua
@@ -0,0 +1,38 @@
+local function deleteTxAction()
+    return redis.call('DEL', KEYS[2])
+end
+
+local function deleteTransaction()
+    return redis.call('DEL', KEYS[1])
+end
+
+local function getTransactionTimestamp()
+    return redis.call('GET', KEYS[1]) or 0
+end
+
+local function txActionOlderThanTransaction(txActionTimestamp, transactionTimestamp)
+    return transactionTimestamp > txActionTimestamp
+end
+
+local function transactionIsTooOld(transactionTimestamp, cutoffTimestamp)
+    return transactionTimestamp <= cutoffTimestamp
+end
+
+local function getOutputStatus(currentStatus, newStatus)
+    return currentStatus or newStatus
+end
+
+local txActionTimestamp = tonumber(ARGV[1])
+local cutoffTimestamp = tonumber(ARGV[2])
+local transactionTimestamp = getTransactionTimestamp()
+local status = 0
+
+if txActionOlderThanTransaction(txActionTimestamp, transactionTimestamp) then
+    status = getOutputStatus(status, deleteTxAction())
+end
+
+if transactionIsTooOld(transactionTimestamp, cutoffTimestamp) then
+    status = getOutputStatus(status, deleteTransaction())
+end
+
+return status
