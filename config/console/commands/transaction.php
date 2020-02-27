934f1751f81b0441848239d6c77f6be7a131ac7c
Oliver Stephenson <oliver@channelgrabber.com>
2020-02-27 15:18:27 +0000

    CGIV-11280: removed unused code from command config

--- config/console/commands/transaction.php
+++ config/console/commands/transaction.php
@@ -1,7 +1,6 @@
 <?php
-use CG\Stdlib\DateTime;
+
 use CG\Transaction\Command\Cleanup;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zend\Di\Di;
@@ -12,10 +11,6 @@ return [
         'command' => function (InputInterface $input, OutputInterface $output) use ($di) {
             /** @var Cleanup $command */
             $command = $di->newInstance(Cleanup::class);
-            $timeThreshold = $input->getArgument('timeThreshold');
-            if (!is_null($chunkSize) && (new DateTime($timeThreshold)) > (new DateTime('now'))) {
-                throw new InvalidArgumentException('timeThreshold can\'t be in the future');
-            }
             $command($input, $output);
         },
         'description' => 'Cleanup any old transactions',
