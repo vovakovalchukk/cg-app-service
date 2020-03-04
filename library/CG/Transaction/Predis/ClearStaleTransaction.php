ef42f92772f6482636d3d5f80d2fba91fe2cb302
Oliver Stephenson <oliver@channelgrabber.com>
2020-03-04 13:00:35 +0000

    CGIV-11280: overrode LUA_FILE_DIRECTORY const as its in the app-level lua folder rather than the channelgrabber/predis lib

--- library/CG/Transaction/Predis/ClearStaleTransaction.php
+++ library/CG/Transaction/Predis/ClearStaleTransaction.php
@@ -5,6 +5,8 @@ use CG\Predis\Command\CachedScriptedCommandAbstract;
 
 class ClearStaleTransaction extends CachedScriptedCommandAbstract
 {
+    protected const LUA_FILE_DIRECTORY = PROJECT_ROOT . '/lua/';
+
     protected function getKeysCount()
     {
         return 2;
