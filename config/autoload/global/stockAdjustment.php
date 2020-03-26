361123738d021324be3c9e7c79a73bace2d69aa6
Aaron Baker <aaron.baker@channelgrabber.com>
2020-03-26 11:55:59 +0000

    CGIV-11321: composer update and DI config to utilise the new stock adjustment queuing mechanism

---
+++ config/autoload/global/stockAdjustment.php
@@ -0,0 +1,25 @@
+<?php
+
+use CG\CGLib\Adjustment\Queuer as StockAdjustmentQueuer;
+use CG\Redis\Queue as RedisQueue;
+
+return [
+    'di' => [
+        'instance' => [
+            'alias' => [
+                'StockAdjustmentRedisQueue' => RedisQueue::class,
+            ],
+            StockAdjustmentQueuer::class => [
+                'parameters' => [
+                    'predisClient' => 'reliable_redis',
+                    'queue' => 'StockAdjustmentRedisQueue',
+                ]
+            ],
+            'StockAdjustmentRedisQueue' => [
+                'parameters' => [
+                    'predis' => 'reliable_redis',
+                ]
+            ]
+        ]
+    ]
+];
