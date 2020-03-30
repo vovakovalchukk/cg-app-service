4b5afd99105893c2e71fd4e0d32bd00b4beb56f0
Oliver Stephenson <oliver@channelgrabber.com>
2020-03-30 14:49:27 +0100

    TAC-607: added missing DI config, as cg-cache had to be updated

--- config/autoload/global/lockingStampedePrevention.php
+++ config/autoload/global/lockingStampedePrevention.php
@@ -1,6 +1,10 @@
 <?php
 
+use CG\Cache\StampedePrevention\Caching\Strategy\Runtime as RuntimeCachingStrategy;
+use CG\Cache\StampedePrevention\Caching\StrategyInterface as CachingStrategyInterface;
 use CG\Cache\StampedePrevention\Locking as LockingStampedePrevention;
+use CG\Cache\StampedePrevention\Locking\Strategy\Standard as StandardLockingStrategy;
+use CG\Cache\StampedePrevention\Locking\StrategyInterface as LockingStrategyInterface;
 
 return [
     'di' => [
@@ -18,6 +22,8 @@ return [
             ],
             'preferences' => [
                 LockingStampedePrevention::class => 'LockingStampedePreventionForAllSalesAccounts',
+                LockingStrategyInterface::class => StandardLockingStrategy::class,
+                CachingStrategyInterface::class => RuntimeCachingStrategy::class,
             ]
         ]
     ]
