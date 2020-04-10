35efdaccdce28f7d41b8aa34f94ef106b9379f1a
Eduard Busuioc <eduard.busuioc@channelgrabber.com>
2020-04-10 17:40:44 +0100

    TAC-605: Added an after order save class that handles callbacks for different job generations when a new or existing order is saved

---
+++ library/CG/Order/Service/Callback/AfterOrderSave.php
@@ -0,0 +1,69 @@
+<?php
+namespace CG\Order\Service\Callback;
+
+use CG\Amazon\Gearman\Generator\UploadInvoiceForOrder;
+use CG\Order\Callback\Service\AfterOrderSaveInterface;
+use CG\Order\Client\Gearman\Generator\AutoEmailInvoice as AutoEmailInvoiceGenerator;
+use CG\Order\Client\Gearman\Generator\CalculateOrderWeight as CalculateOrderWeightGenerator;
+use CG\Order\Client\Gearman\Generator\LinkMatchingOrders as LinkMatchingOrdersGenerator;
+use CG\Order\Client\Gearman\Generator\SaveOrderShippingMethod as SaveOrderShippingMethodGenerator;
+use CG\Order\Client\Gearman\Generator\UpdateExchangeRate;
+use CG\Order\Client\Gearman\Generator\UpdateOrderCount as UpdateOrderCountGenerator;
+use CG\Order\Shared\Entity as Order;
+
+class AfterOrderSave implements AfterOrderSaveInterface
+{
+    /** @var UploadInvoiceForOrder */
+    protected $uploadInvoiceForOrder;
+    /** @var AutoEmailInvoiceGenerator */
+    protected $autoEmailInvoiceGenerator;
+    /** @var CalculateOrderWeightGenerator */
+    protected $calculateOrderWeightGenerator;
+    /** @var SaveOrderShippingMethodGenerator */
+    protected $saveOrderShippingMethodGenerator;
+    /** @var UpdateOrderCountGenerator */
+    protected $updateOrderCountGenerator;
+    /** @var LinkMatchingOrdersGenerator */
+    protected $linkMatchingOrdersGenerator;
+    /** @var UpdateExchangeRate */
+    protected $exchangeRateUpdater;
+
+    public function __construct(
+        UploadInvoiceForOrder $uploadInvoiceForOrder,
+        AutoEmailInvoiceGenerator $autoEmailInvoiceGenerator,
+        CalculateOrderWeightGenerator $calculateOrderWeightGenerator,
+        SaveOrderShippingMethodGenerator $saveOrderShippingMethodGenerator,
+        UpdateOrderCountGenerator $updateOrderCountGenerator,
+        LinkMatchingOrdersGenerator $linkMatchingOrdersGenerator,
+        UpdateExchangeRate $exchangeRateUpdater
+    ) {
+        $this->uploadInvoiceForOrder = $uploadInvoiceForOrder;
+        $this->autoEmailInvoiceGenerator = $autoEmailInvoiceGenerator;
+        $this->calculateOrderWeightGenerator = $calculateOrderWeightGenerator;
+        $this->saveOrderShippingMethodGenerator = $saveOrderShippingMethodGenerator;
+        $this->updateOrderCountGenerator = $updateOrderCountGenerator;
+        $this->linkMatchingOrdersGenerator = $linkMatchingOrdersGenerator;
+        $this->exchangeRateUpdater = $exchangeRateUpdater;
+    }
+
+    public function triggerCallbacksForExistingOrder(Order $order): void
+    {
+        $this->triggerCallbacksForOrder($order);
+    }
+
+    public function triggerCallbacksForNewOrder(Order $order): void
+    {
+        $this->calculateOrderWeightGenerator->generateJobForOrder($order);
+        $this->triggerCallbacksForOrder($order);
+    }
+
+    protected function triggerCallbacksForOrder(Order $order): void
+    {
+        $this->autoEmailInvoiceGenerator->createJobForOrder($order);
+        $this->uploadInvoiceForOrder->generateJobForOrder($order);
+        $this->saveOrderShippingMethodGenerator->createJob($order);
+        $this->updateOrderCountGenerator->createJob($order);
+        $this->linkMatchingOrdersGenerator->generateForOrder($order);
+        $this->exchangeRateUpdater->addJobForOrder($order);
+    }
+}
