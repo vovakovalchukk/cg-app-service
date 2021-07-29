2f30c3f2034cde68a033efbb954bd19ceca23bae
Oliver Stephenson <oliver@channelgrabber.com>
2021-07-29 10:50:56 +0100

    CGIV-12121: added DI config for stockAdjustmentLogRelated tables

--- config/autoload/global/stockAudit.php
+++ config/autoload/global/stockAudit.php
@@ -1,8 +1,12 @@
 <?php
+
 use CG\Locking\Service as LockingService;
 use CG\Stock\Audit\Adjustment\Mapper as AdjustmentMapper;
-use CG\Stock\Audit\Adjustment\Storage\Db as AdjustmentDbStorage;
+use CG\Stock\Audit\Adjustment\Related\Mapper as AdjustmentRelatedMapper;
+use CG\Stock\Audit\Adjustment\Related\Storage\ArchiveDb as AdjustmentRelatedArchiveDbStorage;
+use CG\Stock\Audit\Adjustment\Related\Storage\Db as AdjustmentRelatedDbStorage;
 use CG\Stock\Audit\Adjustment\Storage\ArchiveDb as AdjustmentArchiveDbStorage;
+use CG\Stock\Audit\Adjustment\Storage\Db as AdjustmentDbStorage;
 use CG\Stock\Command\MigrateStockAuditAdjustments;
 
 return [
@@ -33,6 +37,22 @@ return [
                     'fastReadSql' => 'channelgrabber-stockauditSql',
                     'writeSql' => 'channelgrabber-stockauditSql',
                     'mapper' => AdjustmentMapper::class,
+                ],
+            ],
+            AdjustmentRelatedDbStorage::class => [
+                'parameters' => [
+                    'readSql' => 'cg_appReadSql',
+                    'fastReadSql' => 'cg_appFastReadSql',
+                    'writeSql' => 'cg_appWriteSql',
+                    'mapper' => AdjustmentRelatedMapper::class,
+                ],
+            ],
+            AdjustmentRelatedArchiveDbStorage::class => [
+                'parameters' => [
+                    'readSql' => 'channelgrabber-stockauditSql',
+                    'fastReadSql' => 'channelgrabber-stockauditSql',
+                    'writeSql' => 'channelgrabber-stockauditSql',
+                    'mapper' => AdjustmentRelatedMapper::class,
                 ],
             ],
             'LockingServiceStockAdjustmentMigration' => [
