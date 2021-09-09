eca6a97ec12a31890fc1efff14b667a41c28413f
Dominik Gajewski <dominikg@channelgrabber.com>
2021-09-09 14:51:33 +0100

    CGIV-12143 added Feed and FeedMessage di config

--- config/autoload/global/amazon.php
+++ config/autoload/global/amazon.php
@@ -1,12 +1,59 @@
 <?php
 use CG\Amazon\ListingImport as AmazonListingImport;
 
+use CG\Amazon\Feed\Mapper as FeedMapper;
+use CG\Amazon\Feed\Message\Mapper as FeedMessageMapper;
+use CG\Amazon\Feed\Message\Repository as FeedMessageRepository;
+use CG\Amazon\Feed\Message\Service as FeedMessageService;
+use CG\Amazon\Feed\Message\Storage\Db as FeedMessageDb;
+use CG\Amazon\Feed\Message\Storage\Cache as FeedMessageCache;
+use CG\Amazon\Feed\Message\StorageInterface as FeedMessageStorage;
+use CG\Amazon\Feed\Storage\Db as FeedDb;
+use CG\Amazon\Feed\StorageInterface as FeedStorage;
+use CG\Amazon\Feed\Service as FeedService;
+
 return [
     'di' => [
+        'preferences' => [
+            FeedMessageStorage::class => FeedMessageRepository::class,
+            FeedStorage::class => FeedDb::class,
+        ],
         'instance' => [
             AmazonListingImport::class => [
                 'parameters' => [
                     'gearmanClient' => 'amazonGearmanClient'
+                ]
+            ],
+            FeedMessageDb::class => [
+                'parameters' => [
+                    'readSql' => 'amazonReadCGSql',
+                    'fastReadSql' => 'amazonFastReadCGSql',
+                    'writeSql' => 'amazonWriteCGSql',
+                    'mapper' => FeedMessageMapper::class,
+                ]
+            ],
+            FeedMessageRepository::class => [
+                'parameters' => [
+                    'storage' => FeedMessageCache::class,
+                    'repository' => FeedMessageDb::class
+                ]
+            ],
+            FeedMessageService::class => [
+                'parameters' => [
+                    'storage' => FeedMessageRepository::class,
+                ]
+            ],
+            FeedDb::class => [
+                'parameter' => [
+                    'readSql' => 'amazonReadCGSql',
+                    'fastReadSql' => 'amazonFastReadCGSql',
+                    'writeSql' => 'amazonWriteCGSql',
+                    'mapper' => FeedMapper::class,
+                ]
+            ],
+            FeedService::class => [
+                'parameters' => [
+                    'storage' => FeedDb::class,
                 ]
             ]
         ],
