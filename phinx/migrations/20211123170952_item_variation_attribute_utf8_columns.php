a2c5ada353bc29d913767f019cf407746d834960
Oliver Stephenson <oliver@channelgrabber.com>
2021-11-25 09:44:09 +0000

    CGIV-12214: added columns to migrate

--- phinx/migrations/20211123170952_item_variation_attribute_utf8_columns.php
+++ phinx/migrations/20211123170952_item_variation_attribute_utf8_columns.php
@@ -6,7 +6,16 @@ class ItemVariationAttributeUtf8Columns extends AbstractOnlineSchemaChange
 {
     protected const TABLE = 'itemVariationAttribute';
     protected const COLUMNS = [
-
+        [
+            'name',
+            'varchar(255)',
+            'utf8mb4_0900_ai_ci',
+        ],
+        [
+            'value',
+            'varchar(255)',
+            'utf8mb4_0900_ai_ci',
+        ],
     ];
 
     public function up()
